8d8e24df8116666798653dc2dd7374525539372d
Carlos Santisteban <zuiko21@gmail.com>
2016-03-03 12:43:42 +0100

    start working on shell!

---
+++ forge/shell/monitor.c
@@ -0,0 +1,291 @@
+/* Monitor shell for minimOS (simple version) 0.5b1
+ * last modified 2016-03-02
+ * (c) 2016 Carlos J. Santisteban
+ * */
+ 
+#include <stdio.h>
+
+/* Function prototypes */
+void getNextChar(void);		// get next valid character in input
+void getListChar(void);		// get next valid character in opcode list
+void hex2byte(void);		// convert a pair of hex ciphers into byte (calls hex2nibble)
+void checkEnd(void);		// check whether there's no more in the input line
+void fetchValue(int bytes);	// advance to the next n bytes, convert into tmp[0...1]
+void disassemble(void);		// disassemble for one opcode
+void opcodePrint(void);		// prints instruction and hexdump
+void scanLabel(void);		// looks for a label in symbol table
+void init(void);			// initialize variables
+void confUndf(void);		// check whether there are undefined symbols, (i) if OK by user
+
+/* Global variables */
+//	int rom = 0xC000;	// start of emulated ROM
+	unsigned char ram[65536];
+
+	FILE* arch;
+
+	unsigned char _A, i, j, c, x, value;	// value after hex2byte (A), b after GetNextChar (A), i loops (X)
+//	unsigned char adrm, udfd, sal;
+	unsigned char lines;						// number of lines #u to display
+	unsigned char areg, xreg, yreg, psr, sp;	// 6502 registers
+	char buf[80];								// input buffer
+	int ptr, dir, count, tmp[3], scan, os, oldscan, cursor, si;	// several pointers
+	int siz;	// temporarily for opcode list loading, later #n
+
+	char undef=0, empty=0;	// *** uncomment if no symbolic assembler is used ***
+
+/* Main programme */
+int main(void) {
+
+
+// start up things
+	init();
+	printf("minimOS 0.5b1 Monitor shell\n");
+	printf("(c)2016 Carlos J. Santisteban\n");
+	do {							// main loop
+		printf("%04x> ", ptr);		// prompt
+		fgets(buf, 80, stdin);		// read input line
+		scan=0;
+		while(buf[scan++]!='\n' && scan<80);	// get terminating newline
+		buf[--scan]=0;				// terminate instead
+		// might call assembler module from here
+		scan=0;			// reset indexes
+		oldscan=0;
+		cursor=0;
+		tmp[2] = 0;		// reset opcode to be detected
+		cursor--;		// undo first advance
+		getNextChar();	// get first valid character in input
+// check for pseudo-opcodes
+		if (_A=='.') {			// pseudo-opcode
+			getNextChar();		// get the command itself
+			switch(_A) {		// identify command
+				case 'A':		// set accumulator as $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					areg = _A;	// store byte
+					break;
+				case 'B':		// store byte $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					ram[ptr++] = _A;		// store byte
+					break;
+				case 'C':		// call $aaaa*
+					if (undef) {		// some pending definition
+						printf("***UNDEFINED LABELS***\n");
+					} else {
+						fetchValue(2);			// get next two bytes in tmp[]
+						dir = tmp[1] + 256*tmp[0];	// compute address
+						printf("[JSR $%04x]\n", dir);
+					}
+					break;
+				case 'E':		// dump #u lines from $aaaa
+					fetchValue(2);			// get next two bytes in tmp[]
+					dir = tmp[1] + 256*tmp[0];	// compute base address
+					for (i=0; i<lines; i++) {	// will show #u lines
+						printf("%04x [ ", dir);	// start address
+						for (j=0; j<8; j++) {	// eight bytes per line
+							printf("%02x ", ram[dir++]);	// show one byte
+						}
+						printf("] ");			// end hex dump, now in ascii
+						for (j=8;j>0;j--) {		// rescan downwards
+							c = ram[dir-j];		// get byte again
+							if (c<' ' || c>'~') {	// non-printable
+								printf("·");	// interpunct as substitute
+							} else {
+								printf("%c", c);	// place ascii
+							}
+						}
+						printf("\n");			// new line
+					}
+					break;
+				case 'F':		// force cold boot
+					printf("\n---[forced cold boot]---\n");
+					return 0;	// go away!
+					break;
+				case 'G':		// set SP as $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					sp = _A;	// store byte
+					break;
+				case 'H':		// show command list
+					printf("\nCommand list:\n");
+					// ...
+					break;
+				case 'J':		// jump to $aaaa*
+					if (undef) {		// some pending definition
+						printf("***UNDEFINED LABELS***\n");
+					} else {
+						fetchValue(2);			// get next two bytes in tmp[]
+						dir = tmp[1] + 256*tmp[0];	// compute address
+						printf("[JMP $%04x]\n", dir);
+					}
+					break;
+				case 'M':		// copy #n bytes from current to $aaaa
+					fetchValue(2);			// get next two bytes in tmp[]
+					dir = tmp[1] + 256*tmp[0];			// compute destination address
+					oldscan = dir;						// save initial value
+					scan = ptr;							// copy source address
+					if (dir<scan) {			// may copy forward
+						for (count=0; count<siz; count++) {	// copy #n bytes
+							ram[dir++] = ram[scan++];		// copy from current to destination
+						}
+					} else {
+						for (count=siz-1; count>=0; count--) {	// copy #n bytes BACKWARDS
+							ram[dir+count] = ram[scan+count];	// copy from current to destination
+						}						
+					}
+					printf("---$%04x bytes copied at $%04x---\n", siz, oldscan);	// show results
+					break;
+				case 'N':		// set #n as $dddd
+					fetchValue(2);			// get next two bytes in tmp[]
+					siz = tmp[1] + 256*tmp[0];	// compute number of bytes #n
+					break;
+				case 'O':		// set current address as $aaaa
+					fetchValue(2);			// get next two bytes in tmp[]
+					ptr = tmp[1] + 256*tmp[0];	// compute new address
+					break;
+				case 'P':		// set PSR as $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					psr = _A;	// store byte
+					break;
+				case 'Q':		// quit or poweroff*
+					confUndf();		// check whether something pending
+					if (_A) {
+						return 0;	// go away!
+					}
+					break;
+				case 'R':		// reboot*
+					if (undef) {		// some pending definition
+						printf("***UNDEFINED LABELS***\n");
+					} else {
+						printf("\n---[system restart]---\n");
+						init();			// apparent reset
+						continue;
+					}
+					break;
+				case 'S':		// store immediate raw string until EOL
+					cursor++;	// skip the 'S'!
+					while(c=buf[cursor++]) {	// get raw char until terminator
+						ram[ptr++]=c;			// store in place
+					}
+					// does NOT store the termination byte, use cursor++ if desired
+					break;
+				case 'T':
+					break;
+				case 'U':		// set #u as $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					lines = _A;	// will show #u lines
+					break;
+				case 'V':		// view registers
+					printf("\nPC:  A: X: Y: S: NV·bDIZC\n");
+					printf("%04x %02x %02x %02x %02x ", ptr, areg, xreg, yreg, sp);
+					for (i=128;i;i/=2) {	// binary backwards loop
+						if (i==32) {
+							i=16;			// skip unused bit
+							printf("·");
+						}
+						if (psr&i) {		// check that bit
+							printf("1");	// asserted
+						} else {
+							printf("0");	// negated
+						}
+					}
+					printf("\n");
+					break;
+				case 'W':		// store word $dddd
+					fetchValue(2);			// get next two bytes in tmp[]
+					ram[ptr++] = tmp[1];	// store LSB
+					ram[ptr++] = tmp[0];	// store MSB
+					break;
+				case 'X':		// set X as $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					xreg = _A;	// store byte
+					break;
+				case 'Y':		// set Y as $dd
+					getNextChar();	// advance to operand
+					hex2byte();		// convert to number
+					yreg = _A;	// store byte
+					break;
+				case 'Z':		// poweroff or suspend
+					printf("\n---suspend---\n\n");
+					break;
+				default:
+					printf("***Bad command***\n");
+			}
+			continue;			// command ended, ask for another
+		} else printf("***Missing module***\n");	
+	} while (-1);
+
+	return 0;
+}
+
+/* Function definitions */
+void getNextChar(void) {							// get next valid character in input
+			do {
+				cursor++;							// advance by default
+			} while (buf[cursor]==' ' || buf[cursor]=='$');	// skip spaces AND RADIX in input
+			_A = buf[cursor];						// get char
+			if (_A>='a' && _A<='z')		_A &= 223;	// all uppercase
+}
+
+
+void hex2byte(void) {			// convert a pair of hex ciphers into byte (calls hex2nibble)
+	int loop;
+
+	value = 0;
+	for (loop=0; loop<2; loop++) {			// two hex per byte
+		_A -= '0';	// convert to number
+		if (_A>=0 && _A<10) {			// already a valid number
+			// nothing to do???
+		} else {					// check whether alpha
+			if (_A>='A'-'0' && _A<='F'-'0') {		// valid hex cipher
+				_A -= 'A'-'0'-10;				// add alpha cipher
+			} else {
+				//***ERROR, not valid cipher***
+				cursor--;	// try to reprocess this char
+				break;
+			}
+		}
+		// _A is 0...15 unless wrong cipher (will not reach this anyway)
+		value *= 16;		// older value was MSN
+		value += _A;			// add LSN
+		getNextChar();		// go for next hex
+	}
+}
+
+void checkEnd(void) {			// check whether there's no more in the input line
+	if (x==128) {			// opcode ended at list, but...
+		while (buf[cursor]==' ' || buf[cursor]=='$')		cursor++;	// skip spaces in input
+		if (buf[cursor]) {	// some chars remain in input, not this one
+			oldscan=scan;	// point to start
+			tmp[2]++;		// try next opcode
+			cursor = 0;		// reset index
+			x = 0;			// don't exit yet!
+		}
+	}
+}
+
+void fetchValue(int bytes) {	// advance to the next n bytes, convert into tmp[0...1]
+	getNextChar();				// advance to operand
+
+		for(i=0;i<bytes;i++) {		// do number of bytes
+			hex2byte();				// get whole byte
+			tmp[i] = value;			// store converted value
+		}
+		count = i;					// size as supplied
+}
+
+
+void init(void) {		// initialize variables
+	ptr = 0x400;			// set initial address
+	siz = 0;				// copy/transfer size
+	lines = 4;				// lines to disassemble/display
+
+}
+
+void confUndf(void) {		// check whether there are undefined symbols, (b) if OK by user
+		_A = 1;		// OK to proceed
+	
+}
---
+++ forge/shell/monitor.s
@@ -0,0 +1,455 @@
+/* Monitor shell for minimOS (simple version) 0.5b1
+ * last modified 2016-03-03
+ * (c) 2016 Carlos J. Santisteban
+ * */
+
+#ifndef	KERNEL
+#include "../../OS/options.h"
+#include "../../OS/macros.h"
+#include "../../OS/abi.h"
+.zero
+#include "../../OS/zeropage.h"
+.bss
+#include "../../OS/firmware/firmware.h"
+#include "../../OS/sysvars.h"
+.text
+user_sram	= $0400
+#endif
+
+; *** constant definitions ***
+#define	BUFSIZ		20
+#define	CR			13
+#define	BS			9
+#define	BEL			7
+
+; *** include minimOS headers and some other stuff ***
+-shell:
+; *** declare zeropage variables ***
+	ptr		= uz		; current address pointer
+	siz		= ptr+2		; number of bytes to copy or transfer ('n')
+	lines	= siz+2		; lines to dump ('u')
+	_a		= lines+1	; A register
+	_x		= _a+1		; X register
+	_y		= _x+1		; Y register
+	_psr	= _y+1		; status register
+	_sp		= _psr+1	; stack pointer
+	cursor	= _sp+1		; storage for X offset
+	buffer	= cursor+1		; storage for input line (BUFSIZ chars)
+	tmp		= buffer+BUFSIZ	; temporary storage
+	iodev	= tmp+2		; standard I/O ** minimOS specific **
+
+	__last	= iodev+1	; *** just for easier size check ***
+
+; *** initialise the monitor ***
+; ** minimOS specific stuff **
+	LDA #__last-uz		; zeropage space needed
+; check whether has enough zeropage space
+#ifdef	SAFE
+	CMP z_used			; check available zeropage space
+	BCC go_mon			; enough space
+	BEQ go_mon			; just enough!
+		_ERR(FULL)			; not enough memory otherwise (rare)
+go_mon:
+#endif
+	STA z_used			; set needed ZP space as required by minimOS
+	_STZA zpar			; no screen size required
+	_STZA zpar+1		; neither MSB
+	LDY #<title			; LSB of window title
+	LDA #>title			; MSB of window title
+	STY zaddr3			; set parameter
+	STA zaddr3+1
+	_KERNEL(OPEN_W)		; ask for a character I/O device
+	BCC open_mon		; no errors
+		RTS					; abort otherwise!
+open_mon:
+	STY iodev			; store device!!!
+; ** end of minimOS specific stuff
+
+; global variables
+	LDA #>user_sram		; initial address
+	LDY #<user_sram
+	STY ptr				; store LSB
+	STA ptr+1			; and MSB
+	LDA #4				; standard number of lines
+	STA lines			; set variable
+	_STZA siz			; clear copy/transfer size
+	LDA #>splash		; address of splash message
+	LDY #<splash
+	JSR prnStr			; print the string!
+
+; *** begin things ***
+main_loop:
+; might print current address in hex!!!
+		LDA #>prompt		; address of prompt string
+		LDY #<prompt
+		JSR prnStr			; print the string!
+		JSR getLine			; input a line
+		LDX #$FF			; GetNextChar will advance it!
+		JSR getNextChar		; get first character on string
+;		CMP #'.'			; command introducer (not used nor accepted if monitor only)
+;			BNE not_mcmd		; not a monitor command
+		STX cursor			; save cursor!
+		CMP #'Z'+1			; past last command?
+			BCS bad_cmd			; unrecognised
+		SEC
+		SBC #'A'			; first available command
+			BCC bad_cmd			; cannot be lower
+		ASL					; times two to make it index
+		TAX					; use as index
+		JSR call_mcmd		; call monitor command
+		_BRA main_loop		; continue forever
+;not_mcmd:
+;	LDA #>err_mmod		; address of error message
+;	LDY #<err_mmod
+;	_BRA d_error		; display error
+bad_cmd:
+	LDA #>err_bad		; address of error message
+	LDY #<err_bad
+d_error:
+	JSR prnStr			; display error
+	_BRA main_loop		; continue
+
+; *** command routines, named as per pointer table ***
+set_A:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDY tmp				; converted byte
+	STY _a				; set accumulator
+	RTS
+
+store_byte:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDA tmp				; converted byte
+	_STAY(ptr)			; set accumulator
+	INC ptr				; advance pointer
+	BNE sb_end			; all done if no wrap
+		INC ptr+1			; increase MSB otherwise
+sb_end:
+	RTS
+
+call_address:
+	JSR fetch_word		; get operand address
+; now ignoring operand errors!
+; restore stack pointer, this is somehow dangerous, to say the least...
+	LDX _sp				; get stored value
+	TXS					; set new pointer...
+; SP restored
+	JSR do_call			; set regs and jump!
+	JMP main_loop		; hopefully context is OK
+
+jump_address:
+	JSR fetch_word		; get operand address
+; now ignoring operand errors!
+; restore stack pointer, this is somehow dangerous, to say the least...
+	LDX _sp				; get stored value
+	TXS					; set new pointer...
+; SP restored
+; restore registers and jump
+do_call:
+	LDX _x				; retrieve registers
+	LDY _y
+	LDA _psr			; status is different
+	PHA					; will be set via PLP
+	LDA _a				; lastly retrieve accumulator
+	PLP					; restore status
+	JMP (tmp)			; go! might return somewhere else
+
+examine:
+
+set_SP:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDY tmp				; converted byte
+	STY _sp				; set stack pointer
+	RTS
+
+help:
+	LDA #>help_str		; help string
+	LDY #<help_str
+	JMP prnStr			; print it, and return to main loop
+
+	_BRA do_call		; set regs and jump! no return!
+
+move:
+set_count:
+	JSR fetch_word		; get operand word
+	LDY tmp				; copy LSB
+	LDA tmp+1			; and MSB
+	STY siz				; into destination variable
+	STA siz+1
+	RTS
+
+origin:
+set_PSR:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDY tmp				; converted byte
+	STY _psr			; set status
+	RTS
+
+quit:
+; will not check any pending issues
+	PLA					; discard main loop return address
+	PLA
+	RTS					; exit to minimOS
+
+store_str:
+	TXA					; get cursor
+	TAY					; use as offset
+sstr_l:
+		INY					; skip the S and increase
+		LDA buffer, Y		; get raw character
+		STA (ptr), Y		; store in place
+		BEQ sstr_end		; until terminator, will be stored anyway
+		CMP #CR				; newline also accepted, just in case
+		BNE sstr_l			; contine string
+sstr_end:
+	RTS
+
+set_lines:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDY tmp				; converted byte
+	STY lines			; set number of lines
+	RTS
+
+view_regs:
+store_word:
+	JSR fetch_word		; get operand word
+	LDA tmp				; get LSB
+	_STAY(ptr)			; store in memory
+	INC ptr				; next byte
+	BNE sw_nw			; no wrap
+		INC ptr+1			; otherwise increment pointer MSB
+sw_nw:
+	LDA tmp+1			; same for MSB
+	_STAY(ptr)
+	INC ptr				; next byte
+	BNE sw_end			; no wrap
+		INC ptr+1			; otherwise increment pointer MSB
+sw_end:
+	RTS
+
+set_X:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDY tmp				; converted byte
+	STY _x				; set register
+	RTS
+
+set_Y:
+	LDX cursor			; retrieve index
+	JSR getNextChar		; go to operand
+	JSR hex2byte		; convert value
+	LDY tmp				; converted byte
+	STY _y				; set register
+	RTS
+
+force:
+	LDY #PW_COLD		; cold boot request ** minimOS specific **
+	_BRA fw_shut		; call firmware
+
+reboot:
+	LDY #PW_WARM		; warm boot request ** minimOS specific **
+	_BRA fw_shut		; call firmware
+
+poweroff:
+	LDY #PW_OFF			; poweroff request ** minimOS specific **
+fw_shut:
+	_KERNEL(SHUTDOWN)
+
+unrecognised:
+	PLA					; discard main loop return address
+	PLA
+	JMP bad_cmd			; show error message and continue
+
+; *** useful routines ***
+; * print a NULL-terminated string pointed by $AAYY *
+prnStr:
+	STA zaddr3+1		; store MSB
+	STY zaddr3			; LSB
+	LDY iodev			; standard device
+	_KERNEL(STRING)		; print it!
+; currently ignoring any errors...
+	RTS
+
+; * get input line from device at fixed-address buffer *
+; minimOS should have one of these in API...
+getLine:
+	LDX #0				; reset pointer
+	STX tmp				; set variable
+gl_l:
+		LDY iodev			; use device
+		_KERNEL(CIN)		; get one character
+			BCS gl_l			; wait until something
+		LDY iodev			; use device again
+		_KERNEL(COUT)		; echo!!! hope parameter stays ok
+; ignoring possible I/O error...
+		LDA zpar			; get received
+		LDX tmp				; retrieve index
+		CMP #CR				; hit CR?
+			BEQ gl_cr			; all done then
+		CMP #BS				; is it backspace?
+			BEQ gl_bs			; delete then
+		STA buffer, X		; store into buffer
+		INX					; next
+		CPX #BUFSIZ			; overflow?
+			BCS gl_off			; complain if so
+		STX tmp				; update index
+		_BRA gl_l			; and continue
+gl_bs:
+	CPX #0				; already empty?
+		BEQ gl_off			; complain
+	DEX					; backoff
+	STX tmp				; update index
+	_BRA gl_l			; continue input
+gl_off:
+	LDA #BEL			; console sound?
+	STA zpar			; store parameter
+	LDY iodev			; use device
+	_KERNEL(COUT)		; complain!
+	_BRA gl_l			; continue
+gl_cr:
+	_STZA buffer, X		; terminate string
+	RTS					; and all done!
+
+; * get clean character from buffer in A, cursor at X *
+getNextChar:
+	INX					; advance!
+	LDA buffer, X		; get raw character
+	CMP #' '			; white space?
+		BEQ getNextChar		; skip it!
+	CMP #'$'			; ignored radix?
+		BEQ getNextChar		; skip it!
+	CMP #'a'			; not lowercase?
+		BCC gn_ok			; all done!
+	CMP #'z'+1			; still within lowercase?
+		BCS gn_ok			; otherwise do not correct!
+	AND #%11011111		; remove bit 5 to uppercase
+gn_ok:
+	RTS
+
+; * convert two hex ciphers into byte@tmp, A is current char, X is cursor *
+hex2byte:
+	LDY #0				; reset loop counter
+	STY tmp				; also reset value
+h2b_l:
+		SEC					; prepare
+		SBC #'0'			; convert to value
+			BCC h2b_err			; below number!
+		CMP #10				; already OK?
+		BCC h2b_num			; do not shift letter value
+			CMP #'A'-'0'		; should be a letter then
+				BCC h2b_err			; not!
+			CMP #'F'-'0'+1		; but no more than F
+				BCS h2b_err			; bad letter
+			SBC #'A'-'0'-9		; convert from hex (had CLC before!)
+h2b_num:
+		ASL tmp				; older value times 16
+		ASL tmp
+		ASL tmp
+		ASL tmp
+		CLC
+		ADC tmp				; add computed nibble
+		STA tmp				; and store full byte
+		JSR getNextChar		; go for next hex cipher
+		INY					; loop counter
+		CPY #2				; two ciphers per byte
+		BNE h2b_l			; until done
+	RTS					; value is at tmp
+h2b_err:
+	DEX					; will try to reprocess this char
+	RTS
+
+; * fetch more than one byte from hex input buffer *
+fetch_word:
+	JSR getNextChar		; point to operand
+	JSR hex2byte		; get first byte (MSB) in tmp
+	LDY tmp				; leave room for next
+	STY tmp+1
+	JSR hex2byte		; get second byte, tmp is little-endian now
+	RTS
+
+; * call command routine *
+call_mcmd:
+	_JMPX(cmd_ptr)		; indexed jump macro
+
+; *** pointers to command routines ***
+cmd_ptr:
+	.word	set_A			; .A
+	.word	store_byte		; .B
+	.word	call_address	; .C
+	.word	unrecognised	; .D
+	.word	examine			; .E
+	.word	force			; .F
+	.word	set_SP			; .G
+	.word	help			; .H
+	.word	unrecognised	; .I
+	.word	jump_address	; .J
+	.word	unrecognised	; .K
+	.word	unrecognised	; .L
+	.word	move			; .M
+	.word	set_count		; .N
+	.word	origin			; .O
+	.word	set_PSR			; .P
+	.word	quit			; .Q
+	.word	reboot			; .R
+	.word	store_str		; .S
+	.word	unrecognised	; .T
+	.word	set_lines		; .U
+	.word	view_regs		; .V
+	.word	store_word		; .W
+	.word	set_X			; .X
+	.word	set_Y			; .Y
+	.word	poweroff		; .Z
+
+; *** strings and other data ***
+title:
+	.asc	"miniMonitor", 0
+
+splash:
+	.asc	"minimOS 0.5b1 shell", CR
+	.asc	" (c) 2016 Carlos J. Santisteban", CR, 0
+
+prompt:
+	.asc	">", 0
+
+;err_mmod:
+;	.asc	"***Missing module***", CR, 0
+
+err_bad:
+	.asc	"*** Bad command ***", CR, 0
+
+help_str:
+	.asc	"---Command list---", CR
+	.asc	"(d = 2 hex char.)", CR
+	.asc	"(a = 4 hex char.)", CR
+	.asc	"(s = raw string)", CR
+	.asc	"Ad = set A reg.", CR
+	.asc	"Bd = store byte", CR
+	.asc	"Ca = call subr.", CR
+	.asc	"Ea = dump 'u' lines", CR
+	.asc	"F = cold boot", CR
+	.asc	"Gd = set SP reg.", CR
+	.asc	"H = show this list", CR
+	.asc	"Ja = jump", CR
+	.asc	"Ma =copy n byt. to a", CR
+	.asc	"Na = set 'n' bytes", CR
+	.asc	"Oa = set address", CR
+	.asc	"Pd = set Status reg.", CR
+	.asc	"Q = quit", CR
+	.asc	"R = reboot", CR
+	.asc	"Ss = put raw string", CR
+	.asc	"Ud = set 'u' lines", CR
+	.asc	"V = view registers", CR
+	.asc	"Wa = store word", CR
+	.asc	"Xd = set X reg.", CR
+	.asc	"Yd = set Y reg.", CR
+	.asc	"Z = poweroff", CR, 0
