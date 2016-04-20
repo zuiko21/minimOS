4c837d9886807229afa92c506cc82cc3f3baeb6f
Carlos Santisteban <zuiko21@gmail.com>
2016-04-20 14:40:45 +0200

    start designing line editor

---
+++ forge/lined.c
@@ -0,0 +1,16 @@
+/* line editor for minimOS!
+ * v0.5a1
+ * (c)2016 Carlos J. Santisteban
+ * last modified 20160420-1406 */
+ 
+#include <stdio.h>
+
+unsigned char ram[65536];
+unsigned char a, x, y;
+int ptr, cur;
+
+int main(void)
+{
+	
+	return 0;
+}
