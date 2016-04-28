eb6035329e85873cf9edd070bbd3e1ff8e7e6550
Carlos Santisteban <zuiko21@gmail.com>
2016-04-28 14:38:29 +0200

    cleanup on lined.c stub

--- forge/lined.c
+++ forge/lined.c
@@ -1,86 +1,161 @@
 /* line editor for minimOS!
  * v0.5a1
  * (c)2016 Carlos J. Santisteban
- * last modified 20160428-1012 */
+ * last modified 20160428-1426 */
  
 #include <stdio.h>
 
-typedef b8 unsigned char;
+#define	ctl_g	'\a'
+#define	ctl_e	'\t'
+#define	ctl_x	'\b'
+#define	cr		'\n'
+#define	down	'S'
+#define	up		'W'
+#define	FALSE	0
+#define	TRUE	-1
 
-b8 ram[65536], a, x, y;
-int cur, ptr;
+
+typedef unsigned char b8;
+
+b8	ram[65536], a, x, y;
+b8	key, edit=FALSE;
+int cur, ptr, optr, src, dest, delta;
+
+void prev() {
+	printf("PREV\n");
+}
+
+void pop() {
+	printf("POP\n");
+	
+}
+
+void prompt() {
+	printf("PROMPT\n");
+	
+}
+
+void move_dn(int s, int d) {
+	printf("MOVE_DN %d -> %d\n", s, d);
+	
+}
+
+void move_up(int s, int d) {
+	printf("MOVE_UP %d -> %d\n", s, d);
+	
+}
+
+b8 buflen() {
+	printf("BUFLEN\n");
+	return 0;
+}
+
+void push() {
+	printf("PUSH\n");
+	
+}
+
+void indent() {
+	printf("INDENT\n");
+	
+}
+
+void next() {
+	printf("NEXT\n");
+	
+}
+
+void show() {
+	printf("SHOW\n");
+	
+}
+
+int ask() {
+	printf("ASK\n");
+	return 0;
+}
+
+b8 valid(b8 k) {
+	printf("VALID %c\n", k);
+	if (k=='\t' || ('a'<=k && k<='z') || ('A'<=k && k<='Z') || ('0'<=k && k<='9'))
+		return TRUE;
+	else
+		return FALSE;
+}
 
 int main(void)
 {
 //pseudo code ahead
 	do {
-	  if (key==ctl_e) { //edit previous
-		if (cur>0) cur--;
-		edit=TRUE; //mode: edit existing
-		prev(); //go back until CR or begin
-		pop(); //copy line into buffer
-		prompt(); //print cur> and buffer contents
-	  }
-	  if (key==ctl_x) { //delete previous
-		if (cur>0) { //none if empty
-		  src=ptr; //start of current line
-		  prev(); //back to previous
-		  move_dn(src,ptr); //displace
-		  cur--;
+		key = getchar();			//read key
+		if (key==ctl_e) {			//***edit previous***
+			if (cur>0)		cur--;
+			edit=TRUE;					//mode: edit existing
+			prev();						//go back until CR or begin
+			pop();						//copy line into buffer
+			prompt();					//print cur> and buffer contents
 		}
-	  }
-	  if (key==cr) { //insert or accept current
-		if (!edit) { //insert new line
-		  src=ptr; //current is kept
-		  dest=ptr+buflen(); //room for buffer
-		  move_up(src,dest)
-		} else { //replace old
-		  edit=FALSE;
-		  optr=ptr; //sace current pos
-		  next(); //set ptr to next line (advance current size)
-		  delta=optr+buflen()-ptr; //new vs old length
-		  if (delta>0) { //now is longer)
-			move_up(optr,optr+delta); //get extra room
-		  } else if (delta<0) { //now is shorter
-			move_dn(optr+delta,optr); //shrink
-		  } //don't move if same length
-		  ptr=optr; //retrieve
+		if (key==ctl_x) {			//***delete previous***
+			if (cur>0) {				//none if empty
+				src=ptr;					//start of current line
+				prev();						//back to previous
+				move_dn(src,ptr);			//displace
+				cur--;
+			}
 		}
-		push(); //copy buffer @ptr
-		indent(); //copy leading whitespace into buffer and terminate it
-		next(); //point to next
-		cur++;
-		prompt(); //show cur> and buffer (indent)
-	  }
-	  if (key==up && cur>1) { //show previous
-		cur-=2;
-		prev();
-		//this should be common
-		indent(); //get leading whitespace
-		show(); //print cur: and line @ptr, advance ptr! otherwise next()
-		cur++;
-		prompt(); //show new cur> and buffer (indent)
-	  }
-	  if (key==down && ram[ptr]) { //show next if not at end
-		//common as above
-	  }
-	  //should do start & end keys
-	  //what to do on ESC? common block?
-	  if (key==ctl_g) { //goto line
-		dest=ask(); //prompt for line number
-		for(cur=0;cur<dest;cur++) {
-		  optr=ptr; //keep old pos
-		  next(); //advance one line
-		  if (!ram[ptr]) break; //abort at end
+		if (key==cr) {				//***insert or accept current***
+			if (!edit) {				//*insert new line*
+				src=ptr;					//current is kept
+				dest=ptr+buflen()		;	//room for buffer
+				move_up(src,dest);
+			} else {					//*replace old*
+				edit=FALSE;
+				optr=ptr;					//save current pos
+				next();						//set ptr to next line (advance current size)
+				delta=optr+buflen()-ptr;	//new vs old length
+				if (delta>0) {				//now is longer
+					move_up(optr,optr+delta);	//get extra room
+				} else if (delta<0) { 		//now is shorter
+					move_dn(optr+delta,optr);	//shrink
+				}							//don't move if same length
+				ptr=optr;					//retrieve
+			}
+			push();						//copy buffer @ptr
+			indent();					//copy leading whitespace into buffer and terminate it
+			next();						//point to next
+			cur++;
+			prompt();					//show cur> and buffer (indent)
 		}
-		ptr=optr; //back once, should decr. cur???
-		//common as above
-	  }
-	  if (valid(key)) { //including tabs shown raw
-		//edit like READLN in raw
-		//don't manage CR/ESC
-	  }
-	} while(-1);	// loop forever
+		if (key==up && cur>1) {		//***show previous***
+			cur-=2;
+			prev();
+			//this should be common
+			indent();					//get leading whitespace
+			show();						//print cur: and line @ptr, advance ptr! otherwise next()
+			cur++;
+			prompt();					//show new cur> and buffer (indent)
+		}
+		if (key==down && ram[ptr]) {	//***show next if not at end***
+			//common as above
+		}
+		//should do start & end keys
+		//what to do on ESC? common block?
+		if (key==ctl_g) {			//***goto line***
+			dest=ask();					//prompt for line number
+			for(cur=0;cur<dest;cur++) {
+				optr=ptr;					//keep old pos
+				next();						//advance one line
+				if (!ram[ptr])	break;		//abort at end
+			}
+			ptr=optr;					//back once, should decr. cur???
+			//common as above
+		}
+		if (valid(key)) {			//***including tabs shown raw***
+			//edit like READLN in raw
+			//don't manage CR/ESC
+			putchar(key);
+		}
+	} while(-1);				// loop forever
 	
 	return 0;
 }
