951cd26159f7b63e5d0c39d833d6908f65a803e2
Carlos Santisteban <zuiko21@gmail.com>
2016-04-29 09:47:23 +0200

    almost complete functions on lined.c

--- forge/lined.c
+++ forge/lined.c
@@ -1,7 +1,7 @@
 /* line editor for minimOS!
  * v0.5a1
  * (c)2016 Carlos J. Santisteban
- * last modified 20160428-1437 */
+ * last modified 20160429-0945 */
  
 #include <stdio.h>
 
@@ -17,29 +17,50 @@
 
 typedef unsigned char b8;
 
-b8	ram[65536], a, x, y;
+b8	ram[65536]={"Hola\nMundo\n\0"};
+b8	a, x, y;
 b8	buffer[256];
-b8	key, edit=FALSE;
-int cur, ptr, optr, src, dest, delta;
+b8	key, edit;
+int cur, ptr, optr, src, dest, delta, top;
 
 void prev() {
-	printf("PREV\n");
+	printf("(PREV)\n");
+	b8	c;
+	
+	if (ptr>0) {
+		ptr--;
+		c = ram[ptr];
+		while (c!='\n' && c!='\0' && ptr>0) {
+			ptr--;
+		}
+	}
 }
 
 void pop() {		//copy line into buffer
-	printf("POP\n");
+	printf("(POP)\n");
 	int	i=0;
 	b8	c;
-	while (c=ram[ptr+i]) {
+	
+	c=ram[ptr+i];
+	while (c!='\0' && c!='\n') {
 		buffer[i]=c;
 		i++;
+		c=ram[ptr+i];
 	}
 	buffer[i] = '\0';
 }
 
 void prompt() {
-	printf("PROMPT\n");
+	printf("(PROMPT)\n");
+	int	i=0;
+	b8	c;
 	
+	printf("%04x>", cur);
+	c = buffer[i];
+	while (c!='\n' && c!='\0') {
+		putchar(c);
+		c = buffer[++i];
+	}
 }
 
 void move_dn(int s, int d) {
@@ -53,15 +74,22 @@ void move_up(int s, int d) {
 }
 
 b8 buflen() {
-	printf("BUFLEN\n");
-	return 0;
+	printf("(BUFLEN)\n");
+	int	i=0;
+	
+	while(buffer[i]!='\0') {
+		i++;
+	}
+	
+	return i;
 }
 
 void push() {		//copy buffer @ptr
-	printf("PUSH\n");
+	printf("(PUSH)\n");
 	int	i=0;
 	b8	c;
-	while (c=buffer[i]) {
+	
+	while ((c=buffer[i]) != '\0') {
 		ram[ptr+i]=c;
 		i++;
 	}
@@ -69,27 +97,62 @@ void push() {		//copy buffer @ptr
 }
 
 void indent() {
-	printf("INDENT\n");
+	printf("(INDENT)\n");
 	
+	b8	c;
+	int	i=0;
+	
+	if (ram[ptr]) {		// not the end
+		ptr++;
+		c = ram[ptr];
+		while (c==' ' || c=='\t') {
+			buffer[i++]=c;
+			ptr++;
+			c = ram[ptr];
+		}
+	}
+	buffer[i]='\0';
 }
 
 void next() {
-	printf("NEXT\n");
+	printf("(NEXT)\n");
 	
+	b8	c;
+	
+	if (ptr<top) {
+		ptr++;
+		c = ram[ptr];
+		while (c!='\n' && c!='\0' && ptr>0) {
+			ptr++;
+		}
+	}
 }
 
-void show() {
-	printf("SHOW\n");
+void show() {			//print cur: and line @ptr, advance ptr! otherwise next()
+	printf("(SHOW)\n");
+	b8	c;
 	
+	printf("%04x:", cur);
+	c = ram[ptr];
+	while (c!='\n' && c!='\0') {
+		putchar(c);
+		ptr++;
+		c = ram[ptr];
+	}
+	printf("\n");
 }
 
 int ask() {
-	printf("ASK\n");
-	return 0;
+	int i;
+	
+	printf("Line: ");
+	scanf("%d", &i);
+	
+	return i;
 }
 
 b8 valid(b8 k) {
-	printf("VALID %c\n", k);
+	printf("(VALID %c)\n", k);
 	if (k=='\t' || ('a'<=k && k<='z') || ('A'<=k && k<='Z') || ('0'<=k && k<='9'))
 		return TRUE;
 	else
@@ -98,9 +161,18 @@ b8 valid(b8 k) {
 
 int main(void)
 {
-//pseudo code ahead
+	cur = 0;
+	top = 0;
+	ptr = 0;
+	ram[ptr]='\0';
+	buffer[0]=' ';
+	buffer[1]='\0';
+	
+	edit=FALSE;
+	pop();
+	prompt();
 	do {
-		key = getchar();			//read key
+		key = getchar_unlocked();			//read key
 		if (key==ctl_e) {			//***edit previous***
 			if (cur>0)		cur--;
 			edit=TRUE;					//mode: edit existing
@@ -117,6 +189,7 @@ int main(void)
 			}
 		}
 		if (key==cr) {				//***insert or accept current***
+			x=0;
 			if (!edit) {				//*insert new line*
 				src=ptr;					//current is kept
 				dest=ptr+buflen()		;	//room for buffer
@@ -150,11 +223,16 @@ int main(void)
 		}
 		if (key==down && ram[ptr]) {	//***show next if not at end***
 			//common as above
+			indent();					//get leading whitespace
+			show();						//print cur: and line @ptr, advance ptr! otherwise next()
+			cur++;
+			prompt();					//show new cur> and buffer (indent)
 		}
 		//should do start & end keys
 		//what to do on ESC? common block?
 		if (key==ctl_g) {			//***goto line***
 			dest=ask();					//prompt for line number
+			ptr=0;						//eeeeeeek!
 			for(cur=0;cur<dest;cur++) {
 				optr=ptr;					//keep old pos
 				next();						//advance one line
@@ -167,6 +245,7 @@ int main(void)
 			//edit like READLN in raw
 			//don't manage CR/ESC
 			putchar(key);
+			buffer[x++]=key;
 		}
 	} while(-1);				// loop forever
 	
