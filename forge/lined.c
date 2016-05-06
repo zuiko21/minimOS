9be03fb52d8f4849505b0e2b32845a7f64717ed7
Carlos Santisteban <zuiko21@gmail.com>
2016-05-06 10:10:49 +0200

    stupid errors on lined.c

--- forge/lined.c
+++ forge/lined.c
@@ -1,7 +1,7 @@
 /* line editor for minimOS!
  * v0.5a2
  * (c)2016 Carlos J. Santisteban
- * last modified 20160505-1438 */
+ * last modified 20160506-1010 */
 
 /* See more info at http://hughm.cs.ukzn.ac.za/~murrellh/os/notes/ncurses.html */
 
@@ -16,8 +16,8 @@
 #define	ctl_e	0x5
 #define	ctl_x	0x18
 #define	cr		'\n'
-// cannot use ^S for down, use ^R instead
-#define	down	0x14
+// cannot use ^S for down, use ^R instead EEEEEEK
+#define	down	0x12
 #define	up		0x17
 #define	FALSE	0
 #define	TRUE	-1
@@ -46,25 +46,27 @@ byte	a, x, y;
 byte	key, edit;
 int		cur, ptr, optr, src, dest, delta, top, zz;
 
-void prev() {
-	if (ram[ptr]!='\0') {	// needs leading terminator!!!
+void prev() {			// *** point to previous line
+	if (ram[ptr-1]) {		// not at the beginning
 		ptr--;					// end of last line
 		a = ram[ptr];
 		while (a!='\n' && a!='\0') {	// seek for newline or start
 			ptr--;							// backwards
+			a = ram[ptr];					// eeeeek!
 		}
 		cur--;					// one less line
 	} else {
-		printf ("{START}\n");	// cannot get back
+		printf("{START}\n");	// no way to go back
 	}
 }
 
-void next() {
+void next() {			// *** point to next line
 	if (ram[ptr]!='\0') {	// not at end already
 		ptr++;					// start of this line
 		a = ram[ptr];
-		while (a!='\n' && a!='\0' && ptr>0) {	// seek for newline or end
-			ptr++;									// forth
+		while (a!='\n' && a!='\0') {	// seek for newline or end
+			ptr++;							// forth
+			a = ram[ptr];					// eeeeek!
 		}
 		cur++;				// one more line
 	} else {
@@ -72,7 +74,7 @@ void next() {
 	}
 }
 
-void pop() {		// *** copy line into buffer
+void pop() {			// *** copy line into buffer
 	x=1;					// leading CR is not to be copied
 	a=ram[ptr+x];			// first character
 	while (a!='\0' && a!='\n') {	// until newline or end
@@ -83,19 +85,22 @@ void pop() {		// *** copy line into buffer
 	ram[buffer-1+x] = '\0';	// terminate buffer
 }
 
-void push() {		// *** copy buffer @ptr
-	x=1;					// respect leading CR
-	while ((a=ram[buffer-1+x]) != '\0') {	// until buffer is terminated
-		ram[ptr+x]=a;							// copy from buffer
-		x++;									// next
+void push() {			// *** copy buffer @ptr
+	x=0;					// reset index
+	a=ram[buffer];			// first character
+	while (a != '\0') {		// until buffer is terminated
+		ptr++;					// should increase too!
+		ram[ptr]=a;				// copy from buffer
+		x++;
+		a=ram[buffer+x];		// next character
 	}
-	ram[ptr+x] = '\n';						// place trailing CR
+	ram[++ptr] = '\n';		// place trailing CR
 }
 
-void prompt() {		// *** show cur> and buffer contents
-	x=0;
+void prompt() {			// *** show cur> and buffer contents
+	x=0;					// reset index
 	printf("%04x>", cur);	// ask for current line
-	a = ram[buffer+x];		// first char in buffer
+	a = ram[buffer];		// first char in buffer
 	while (a!='\0') {		// until terminated
 		putchar(a);				// print it
 		x++;
@@ -133,33 +138,33 @@ void move_up(int s, int d) {
 
 }
 
-byte buflen() {
-	x=0;				// reset counter
+byte buflen() {			// *** return buffer length
+	x=0;							// reset counter
 
 	while(ram[buffer+x]!='\0') {	// until terminated
 		x++;							// there is one more
 	}
 
-	return x;			// result
+	return x;						// result
 }
 
-void indent() {
+void indent() {			// *** copy leading whitespace into buffer
 	x=1;							// points at first character on current line
-	if (a = ram[ptr+x] != '\0') {	// not the end?
-		while (a==' ' || a=='\t') {	// while there is whitespace
-			ram[buffer-1+x]=a;			// put it on buffer
-			x++;
-			a = ram[ptr+x];				// next character, do not mess with ptr!
-		}
+	a = ram[ptr+1];					// get first char
+	while (a==' ' || a=='\t') {		// while there is whitespace
+		ram[buffer-1+x]=a;				// put it on buffer, note offset
+		x++;
+		a = ram[ptr+x];					// next character, do not mess with ptr!
 	}
-	ram[buffer-1+x]='\0';			// temporary buffer termination
+	ram[buffer-1+x]='\0';				// temporary buffer termination
 }
 
-void show() {			// *** print cur: and line @ptr, advance ptr! otherwise next()
-	a = ram[++ptr];			// check char on next position
-	if (!a) {				// already at end?
-		printf("{end}\n");		// nothing to do
+void show() {			// *** print cur: and line @ptr, advance ptr!
+	if (ram[ptr]=='\0') {	// already at end?
+		printf("{end}\n");		// complain
 	} else {
+		ptr++;							// first char in line
+		a = ram[ptr];					// check char
 		printf("%04x:", cur);			// non-editing prompt
 		while (a!='\n' && a!='\0') {	// until the end of this line
 			putchar(a);						// print it
@@ -171,7 +176,7 @@ void show() {			// *** print cur: and line @ptr, advance ptr! otherwise next()
 	}
 }
 
-int ask() {
+int ask() {				// *** ask for a line number to jump at
 	int i;
 
 	printf("Line: ");
@@ -180,37 +185,37 @@ int ask() {
 	return i;
 }
 
-byte valid(byte k) {
-	printf("(VALID %c)\n", k);
-	if (k=='\t' || ('a'<=k && k<='z') || ('A'<=k && k<='Z') || ('0'<=k && k<='9'))
+byte valid(byte k) {	// *** check whether it is printable or not
+//	printf("(VALID %c)\n", k);
+	if ((k=='\t') || (' '<=k))	// eeeeek
 		return TRUE;
 	else
 		return FALSE;
 }
 
-void load() {
+void load() {			// *** check current 'file' and go to its end
 
-// preload some content
+// ********preload some content*********
 	int i=0;
-	byte texto[10]={"123\n456\0"};	// couple of sample lines
+	byte texto[80]={" 123\n  456\n   789\0"};	// couple of sample lines
 	
 	while(ram[start+i]=texto[i]) {
 		printf("%c", texto[i]);
 		i++;
 	}
-// continue as usual
+// *********continue as usual*********/
 
-	ptr = start;
-	cur = 1;
+	ptr = start;				// initial values for debugging
+	cur = 1;					// at least one line unless empty
 	
-	while (ram[ptr]!='\0') {
-		if (ram[ptr]=='\n')		cur++;
+	while (ram[ptr]!='\0') {	// until the end
+		if (ram[ptr]=='\n')		cur++;	// found a newline
 		ptr++;
 	}
-	if (ptr==start) {		// no content!
-		cur = 0;			// empty doc
+	if (ptr==start) {			// no content!
+		cur = 0;					// empty doc
 	}
-	top = ptr;
+	top = ptr;					// position of TRAILING terminator
 	printf("{%d bytes, %d lines}\n", ptr-start, cur);
 }
 
@@ -221,8 +226,10 @@ int main(void)
 	ram[start-1] = 0;			// needs leading terminator!!!!
 
 	edit=FALSE;					// standard mode
+//printf("cur=%d, ptr=%d---",cur,ptr);
 	prev();						// get last line
 	if (ram[ptr]) {				// not empty?
+//printf("notEmpty ");
 		indent();					// get whitespace into buffer
 		show();						// print it!
 	}
@@ -230,13 +237,13 @@ int main(void)
 	do {
 		key = getch();				//read key
 
-		if (key==0x14) {			//***DEBUG ^T shows all***
+		switch(key) {				// compare values...
+		case 0x14: 					//***DEBUG ^T shows all***
 			printf("\nCONTENTS:\n");
 			for (zz=start; zz<top; zz++)	printf("%c", ram[zz]);
 			printf("\n-----\n");
-		} else
-		
-		if (key==ctl_e) {			//***edit previous***
+			break;
+		case ctl_e:					//***edit previous***
 //		printf("Ctl-E ");
 			if (cur==0) {				// insert at top
 				printf("{start}\n");		// complain
@@ -247,9 +254,8 @@ int main(void)
 				pop();						// copy line into buffer
 			}
 			prompt();					//print cur> and buffer contents
-		} else
-		
-		if (key==ctl_x) {			//***delete previous***
+			break;
+		case ctl_x:					//***delete previous***
 //		printf("Ctl-X ");
 			if (cur==0) {				// no previous line to delete
 				printf("{start}\n");		// complain
@@ -258,9 +264,8 @@ int main(void)
 				prev();						// back to previous
 				move_dn(src,ptr);			// displace...
 			}
-		} else
-		
-		if (key==cr) {				//***insert or accept current***
+			break;
+		case cr:					//***insert or accept current***
 			y=0;
 			if (!edit) {				//*insert new line*
 				src=ptr+1;					//current is kept
@@ -283,9 +288,8 @@ int main(void)
 			indent();					//copy leading whitespace into buffer and terminate it
 			next();						//point to next
 			prompt();					//show cur> and buffer (indent)
-		} else
-		
-		if (key==up) {		//***show previous***
+			break;
+		case up:					//***show previous***
 //		printf("Ctl-W ");
 			prev();						// skip pointed buffer...
 			prev();						// ...and previous line to show!
@@ -293,18 +297,17 @@ int main(void)
 			indent();					// get leading whitespace
 			show();						// print cur: and line @ptr, advance ptr! otherwise next()
 			prompt();					// show new cur> and buffer (indent)
-		} else
-		
-		if (key==down) {	//***show next if not at end***
+			break;
+		case down:					//***show next if not at end***
 //		printf("Ctl-S ");
 			//common as above
 			indent();					//get leading whitespace
 			show();						//print cur: and line @ptr, advance ptr! otherwise next()
 			prompt();					//show new cur> and buffer (indent)
-		} else
+			break;
 		//should do start & end keys
 		//what to do on ESC? common block?
-		if (key==ctl_g) {			//***goto line***
+		case ctl_g:					//***goto line***
 //		printf("Ctl-G ");
 			dest = ask();					// prompt for line number
 			ptr = start;					// eeeeeeek!
@@ -312,7 +315,6 @@ int main(void)
 			for(zz=0;zz<dest;zz++) {
 //				optr=ptr;					//keep old pos
 				next();						// advance one line
-//***cannot mess with cur!!!!!
 				if (!ram[ptr])	break;		//abort at end
 			}
 //			ptr=optr;					//back once, should decr. cur???
@@ -320,14 +322,15 @@ int main(void)
 			indent();					// get leading whitespace
 			show();						// print cur: and line @ptr, advance ptr! otherwise next()
 			prompt();					// show new cur> and buffer (indent)
-		} else
-		
-		if (valid(key)) {			//***including tabs shown raw***
-			//edit like READLN in raw
-			//don't manage CR/ESC
-			putchar(key);
-			ram[buffer+y]=key;
-			y++;
+			break;
+		default:					// manage regular typing
+			if (valid(key)) {			//***including tabs shown raw***
+				//edit like READLN in raw
+				//don't manage CR/ESC
+				putchar(key);
+				ram[buffer+y]=key;
+				y++;
+			}
 		}
 	} while(-1);				// loop forever
 
