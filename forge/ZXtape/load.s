d0f9a3771b432248a6b79693a9a5c198b8b6e108
Carlos Santisteban <zuiko21@users.noreply.github.com>
2016-08-05 13:02:41 +0200

    Info about original ZX Spectrum load routine

--- forge/ZXtape/load.s
+++ forge/ZXtape/load.s
@@ -1,7 +1,7 @@
 ; minimOS ZX tape interface loader!
-; v0.1a1
+; v0.1a2
 ; (c) 2016 Carlos J. Santisteban
-; last modified 20160804-1723
+; last modified 20160805-1301
 
 ; ***** Load block of bytes at (data_pt), size stored at data_size *****
 ; ****   ONLY if enabled by setting 'flag' to look for data ($FF) ****
@@ -15,6 +15,7 @@
 ; px6in = VIA IORx, easy detection via BIT instruction (V) though not used!
 ; speedcode = stores CPU speed in fixed point format ($10 = 1 MHz)
  mask = %01000000  ; preset for bit 6 but change otherwise
+ 
 ; *** zeropage ***
 ; pointer (word)
 ; length (word) is best on ZP
@@ -26,6 +27,20 @@
 ; sync = 191/210 uS
 ; zero = 244/244 uS
 ; one = 489/489 uS
+
+; *** ADVICE ***
+; wait about 102 uS before entering timing loop
+; compare against 686 uS ???
+; other looks for 314 uS? (one vs zero?)
+; 'ld edge 1' takes 133 uS + 17 for each failed pass
+;   and will return with NO Carry if no edge found within 4 mS
+;     otherwise wait for a second and continue if TWO edges within time frame (ld edge 2)
+;   now accept only guide, timing $9C, but edges must be within 857 uS (B=$C6)
+;     needs at least 256 pairs
+;   for sync, timing is $C9 waiting for a single short edge (ld edge 1)
+;     keep waiting aginst $D4, call 'ld edge 1' again for the other edge
+;   reading bits use timing $B0 ($B2 for flag?)
+; * two pulses in less than 713 uS means 0, otherwise 1, timeout after 1575 uS *
 
 zx_load:
 
