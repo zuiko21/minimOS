d798234a5c468f4bc61d109ebabfd75299eba2f5
Carlos Santisteban <zuiko21@gmail.com>
2016-11-14 13:40:49 +0100

    accurate timing on bit-banged serial

--- OS/drivers/soft232.s
+++ OS/drivers/soft232.s
@@ -1,7 +1,7 @@
 ; software serial port emulator for minimOS!
-; v0.5a2, for generic 65C02 (65816-savvy)
+; v0.5b2, for generic 65C02 (65816-savvy)
 ; (c) 2016 Carlos J. Santisteban
-; last modified 20161114-1106
+; last modified 20161114-1339
 
 ; VIA bit functions
 ; Tx	= PA0 (any 0...5, set masks accordingly)
@@ -36,22 +36,28 @@
 	.byt	0			; reserved, D_MEM
 
 ; *** driver description ***
-mm_info:
-	.asc	"Serial-port emulation v0.5a2", 0
+srs_info:
+	.asc	"Serial-port emulation v0.5b2", 0
 
-; *** single local variable, could be elsewhere in zeropage ***
-zsr_vote	= sys_sp
+; *** some definitions ***
+zsr_vote	= sys_sp	; single local variable, could be elsewhere in zeropage
+
+IN_MASK		= %00111111	; PA6-7 as input
+OUT_MASK	= %00000011	; PA0-1 as output
+TX_MASK		= %00000001	; mask for Tx (PA0)
+RTS_MASK	= %00000010	; mask for /RTS (PA1)
+INIT_MASK	= TX_MASK + RTS_MASK	; set Tx (idle) and negate RTS (disable reception)
 
 ; ************************
 ; *** initialise stuff ***
 ; ************************
 srs_init:
 	LDA VIA1+IORA		; previous data
-	ORA #3				; set Tx (idle) and negate RTS (disable reception)
+	ORA #INIT_MASK		; set Tx (idle) and negate RTS (disable reception)
 	STA VIA1+IORA		; set initial status
 	LDA VIA1+DDRA		; get I/O config
-	AND #%00111111		; PA6-7 as input
-	ORA #%00000011		; PA0-1 as output
+	AND #IN_MASK		; PA6-7 as input
+	ORA #OUT_MASK		; PA0-1 as output
 	STA VIA1+DDRA		; set config
 	_DR_OK				; succeeded
 
@@ -76,7 +82,7 @@ srss_cts:
 	SEC					; this will be the stop bit!
 ; now put the start bit
 	LDA #1				; mask for Tx, will be used anywhere (2)
-	TRB VIA1+IORA1		; REset it! EEEEEEK (6)
+	TRB VIA1+IORA		; REset it! EEEEEEK (6)
 	PHA: PLA			; 7 extra clocks delay!
 	JSR srs_83us		; delay (83)
 ; still on until next bit is read
@@ -91,24 +97,9 @@ srss_nzero:
 srss_sent:
 		JSR srs_83us		; delay (83)
 		DEX					; next bit (2)
-		BNE ssrs_bit		; until done (3/2)
+		BNE srss_bit		; until done (3/2)
 	_EXIT_CS			; restore interrupts
 	_DR_OK				; succeeded
-
-; ** delay for a bit (83us incl JSR/RTS) **
-srs_83us:
-	INC sys_sp			; for timing (5)
-	LDY #13				; delay constant (2)
-srs83_loop:
-		DEY					; update countdown (2)
-		BNE srs83_loop		; until done (3/2) total 64 for 13 iterations
-srs_exit:
-	RTS					; done (6)
-
-; ** half-bit delay (41us incl JSR/RTS) **
-srs_41us:
-	LDY #5				; delay constant (2)
-	BRA srs82_loop		; continue with accurate timing (3)
 
 ; ************************
 ; *** receive one byte ***
@@ -122,7 +113,6 @@ srs_rcv:
 srsr_wait:
 		BIT VIA1+IORA		; check input bits (4)
 			BPL srsr_start		; exit loop upon start bit reception (2/3)
-;		JSR srs_41us		; half bit wait (41)
 		DEX					; keep waiting (2)
 		BNE srsr_wait		; until timeout (3/2)
 	_EXIT_CS			; restore previous interrupt state
@@ -131,9 +121,7 @@ srs_nreq:
 	_DR_ERR(EMPTY)		; could not receive anything within time frame
 srsr_start:
 ; wait for the start bit to finish ** a bit of a kludge
-	JSR srs_83us		; long wait (83)
-	JSR srs_exit		; another (12)
-	PHA: NOP: PLA		; remaining delay (9)
+	JSR srs_fullbit		; full-bit wait (104)
 ; let us read one byte
 	LDX #8				; bits per byte (2)
 	STZ io_c			; clear received value (3)
@@ -149,11 +137,12 @@ srsr_zero:
 			DEC zsr_vote		; vote for 0 (5/0)
 			NOP					; equalise paths (2/0)
 srsr_took:
-			LDA $0100			; minimal delay! (8)
-			LDA $0100
+			LDA $0100			; minimal delay! (4)
+			LDA zsr_vote		; a bit more (3)
 			DEY					; next sample (2)
 			BNE srsr_sample		; vote again (3/2)
-; three samples should take 79t, this takes 80 anyway
+		NOP					; now three samples took 79 uS! (2)
+; let us decide what the bit is according to votes
 		BIT zsr_vote		; check sign (3)
 		BPL srsr_one		; was set (3/2)
 			CLC					; otherwise clear bit (0/2)
@@ -170,17 +159,39 @@ srsr_carry:
 	JSR srs_41us		; wait half a bit
 	BIT VIA1+IORA		; check Rx line
 	BMI srsr_stop		; looks like a stop bit
-		JSR srsr_rts		; otherwise something went wrong
+		LDA #RTS_MASK		; otherwise something went wrong
+		TSB VIA1+IORA		; disable reception!
+		_EXIT_CS			; restore interrupts first eeeeeeeeeek
 		_DR_ERR(CORRUPT)	; notify error
 ; disable receiver and finish
 srsr_stop:
-	JSR srs_41us		; wait a bit more (!)
-	JSR srsr_rts		; disable receiver
+	JSR srs_41us		; wait a bit more (really needed?)
+	LDA #RTS_MASK		; mask for /RTS
+	TSB VIA1+IORA		; disable reception!
+	_EXIT_CS			; no longer in a hurry
 	_DR_OK
 
-; ** disable receiver and enable interrupts **
-srsr_rts:
-	_EXIT_CS			; no longer in a hurry
-	LDA #2				; mask for /RTS
-	TSB VIA1+IORA		; disable reception!
-	RTS
+; ****************************
+; *** some useful routines ***
+; ****************************
+
+; * delay for a bit (83us incl JSR/RTS) *
+srs_83us:
+	INC sys_sp			; for timing (5)
+	LDY #13				; delay constant (2)
+srs83_loop:
+		DEY					; update countdown (2)
+		BNE srs83_loop		; until done (3/2) total 64 for 13 iterations
+srs_exit:
+	RTS					; done (6)
+
+; * half-bit delay (41us incl JSR/RTS) *
+srs_41us:
+	LDY #5				; delay constant (2)
+	BRA srs83_loop		; continue with accurate timing (3)
+
+; * full-bit delay (104uS) *
+srs_fullbit:
+	LDY #17				; for a 104uS delay
+	LDA zsr_vote		; extra delay (3)
+	BRA srs83_loop		; continue delay (93)
