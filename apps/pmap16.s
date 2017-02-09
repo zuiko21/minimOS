4e368526e9e1dc2ffa1614a19f499fd63da863c2
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-02-09 14:39:01 +0100

    upgrading pmap16 to 65816 code! revise divide-by-10

--- apps/pmap16.s
+++ apps/pmap16.s
@@ -1,6 +1,6 @@
 ; memory map for minimOS! KLUDGE
-; v0.5.1b3
-; last modified 20170209-0925
+; v0.5.1b4
+; last modified 20170209-1438
 ; (c) 2016-2017 Carlos J. Santisteban
 
 #include "usual.h"
@@ -19,7 +19,7 @@
 pmapHead:
 ; *** header identification ***
 	BRK						; do not enter here! NUL marks beginning of header
-	.asc	"m", CPU_TYPE	; minimOS app!
+	.asc	"mV", CPU_TYPE	; minimOS app! 65c816 only
 	.asc	"****", 13		; some flags TBD
 
 ; *** filename and optional comment ***
@@ -31,7 +31,7 @@ pmapHead:
 	.dsb	pmapHead + $F8 - *, $FF	; for ready-to-blow ROM, advance to time/date field
 
 ; *** date & time in MS-DOS format at byte 248 ($F8) ***
-	.word	$4B60			; time, 09.27
+	.word	$6000			; time, 12.00
 	.word	$4A49			; date, 2017/2/09
 
 pmap16Size	=	pmapEnd - pmapHead -256	; compute size NOT including header!
@@ -59,48 +59,45 @@ go_pmap:
 	STA z_used			; set needed ZP space as required by minimOS
 ; will not use iodev as will work on default device
 ; ##### end of minimOS specific stuff #####
-
-	LDA #>splash		; address of splash message (plus column header)
-	LDY #<splash
-	JSR prnStr			; print the string!
+	.al: REP #$20		; *** 16-bit memory ***
+	
+	LDA #splash			; address of splash message (plus column header)
+	JSR prnStrW			; print the string!
 
 ; ********************
 ; *** begin things ***
 ; ********************
 
-	STZ current			; reset index
+	LDY #0				; reset index
+	STY current			; avoid 16-bit STZ
 pmap_loop:
-		LDA #'$'			; print hex radix
-		JSR prnChar
+		LDX #'$'			; print hex radix
+		JSR prnCharW
 		LDY current			; retrieve index
 		LDA ram_pos, Y		; get this block address
 		STA page			; store for further size computation
-		PHA					; keep for later
-		LDA ram_pos+1, Y	; same for bank!
-		STA page+1
+		XBA					; let us look the bank address before
 		JSR byte2hex		; print bank...
-		PLA					; ...retrieve MSB...
-		JSR byte2hex		; ...then print it
-		LDY #<pmt_lsb		; string for trailing zeroes
-		LDA #>pmt_lsb
-		JSR prnStr
-		LDY current			; index again
+		XBA					; ...and switch to page address
+		JSR byte2hex
+		LDA #pmt_lsb		; string for trailing zeroes
+		JSR prnStrW
+		LDY current			; index again, needed?
 		LDX ram_stat, Y		; check status of this, use as index
 		JMP (pmap_tab, X)	; process as appropriate
 
-; * print suffix in A, new line and complete loop *
+; * print suffix in X, new line and complete loop *
 pmap_next:
-		JSR prnChar			; print suffix
+		JSR prnCharW		; print suffix
 pmap_cr:
-		LDA #CR				; new line
-		JSR prnChar
+		LDX #CR				; new line
+		JSR prnCharW
 		BRA pmap_loop		; and go for next entry
 
 ; manage used block
 pmap_used:
-	LDY #<pmt_pid		; string for PID prefix
-	LDA #>pmt_pid
-	JSR prnStr
+	LDA #pmt_pid		; string for PID prefix
+	JSR prnStrW
 	LDY current			; restore index
 	LDA ram_pid, Y		; get corresponding PID
 	JSR byte2hex		; print it
@@ -108,27 +105,40 @@ pmap_used:
 
 ; * common ending with printed size, pages or KB *
 pmap_size:
-	LDA #' '			; print leading space
-	JSR prnChar
-	INC current			; check next block!
-	INC current			; 16 bit entries!!!!
-	LDY current			; set index
+	LDX #' '			; print leading space
+	JSR prnCharW
+	LDY current			; get old index
+	INY					; check next block!
+	INY					; 16 bit entries!!!!
+	STY current			; update byte
 	LDA ram_pos, Y		; get next block position
 	SEC
 	SBC page			; subtract start of block
 	CMP #4				; check whether below 1k
 	BCS pmap_kb
-		_INC				; round up pages!
+		INC					; round up pages!
 		JSR b2h_num			; will not be over 4
-		LDA #'p'			; page suffix
+		LDX #'p'			; page suffix
 		BRA pmap_next		; print suffix, CR and go for next
 pmap_kb:
 	LSR					; divide by 4
 	LSR
 		BCC pm_nround	; if C, round up!
-			_INC
+			INC
 pm_nround:
-; print A in decimal and continue!
+; print A in decimal and continue! *******REVISE
+/* have a look at this
+unsigned divu10(unsigned n) {
+    unsigned q, r;
+    q = (n >> 1) + (n >> 2);
+    q = q + (q >> 4);
+    q = q + (q >> 8);
+    q = q + (q >> 16);
+    q = q >> 3;
+    r = n - (((q << 2) + q) << 1);
+    return q + (r > 9);
+}
+*/
 	LDX #0				; decade counter
 pkb_div10:
 		CMP #10				; something to count?
@@ -138,18 +148,17 @@ pkb_div10:
 		BRA pkb_div10		; until exit above
 pkb_unit:
 	PHA					; save units
-	TXA					; decades will not be over 6
+	TXA					; decades will not be over 6*****
 	JSR b2h_num			; print ASCII
 	PLA					; retrieve units
 	JSR b2h_ascii		; convert & print
-	LDA #'K'
+	LDX #'K'
 	BRA pmap_next		; print suffix, CR and go for next
 
 ; manage locked list
 pmap_lock:
-	LDY #<pmt_lock		; string for locked label
-	LDA #>pmt_lock
-	JSR prnStr
+	LDA #pmt_lock		; string for locked label
+	JSR prnStrW
 	BRA pmap_size		; finish line with block size
 
 ; manage free block
@@ -161,9 +170,8 @@ pmap_free:
 
 ; manage end of list
 pmap_end:
-	LDY #<pmt_end		; string for end label
-	LDA #>pmt_end
-	JSR prnStr
+	LDA #pmt_end		; string for end label
+	JSR prnStrW
 	_FINISH				; *** all done ***
 
 
@@ -180,13 +188,13 @@ pmap_tab:
 ; * print binary in A as two hex ciphers *
 byte2hex:
 	PHA			; keep whole value
-	LSR			; shift right four times (just the MSB)
+	LSR			; shift right four times (just the MSN)
 	LSR
 	LSR
 	LSR
 	JSR b2h_ascii	; convert and print this cipher
 	PLA			; retrieve full value
-	AND #$0F	; keep just the LSB... and repeat procedure
+	AND #$000F	; keep just the LSN... and repeat procedure
 b2h_ascii:
 	CMP #10		; will be a letter?
 	BCC b2h_num	; just a number
@@ -194,19 +202,19 @@ b2h_ascii:
 b2h_num:
 	ADC #'0'	; convert to ASCII (carry is clear)
 ; ...and print it (will return somewhere)
+	TAX			; where the following function expects it
 
 ; * print a character in A *
-prnChar:
-	STA io_c			; store character
+prnCharW:
+	STX io_c			; store character
 	LDY #0				; use default device
 	_KERNEL(COUT)		; output it ##### minimOS #####
 ; ignoring possible I/O errors
 	RTS
 
 ; * print a NULL-terminated string pointed by $AAYY *
-prnStr:
-	STA str_pt+1		; store MSB
-	STY str_pt			; LSB
+prnStrW:
+	STA str_pt			; store full pointer
 	LDY #0				; standard device
 	_KERNEL(STRING)		; print it! ##### minimOS #####
 ; currently ignoring any errors...
