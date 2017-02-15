6b957cfbbcbaaeb381f0fbe1d726769da2374270
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-02-15 12:50:21 +0100

    IRQ16 takes care of DBR!!!

--- OS/isr/irq16.s
+++ OS/isr/irq16.s
@@ -1,34 +1,42 @@
 ; ISR for minimOS·16
-; v0.5.1a2, should match kernel16.s
-; features TBD
-; (c) 2016 Carlos J. Santisteban
-; last modified 20161010-1319
+; v0.5.1a3, should match kernel16.s
+; (c) 2016-2017 Carlos J. Santisteban
+; last modified 20170215-1250
 
 #define		ISR		_ISR
 
 #include "usual.h"
 
-; performance for maximum priority async routine = 39 clocks!
-; **** the ISR code **** (initial tasks take 18 clocks)
+; fastest async routine runs after 53 clocks!
+; **********************
+; **** the ISR code **** (initial tasks take 21 clocks)
+; **********************
 	.al: .xl: REP #$30		; status already saved, but save register contents in full (3)
 	PHA						; save registers (3x4)
 	PHX
 	PHY
+	PHB						; eeeeeeeeeeeeeek (3)
 	.as: .xs: SEP #$30		; back to 8-bit size (3)
-
+; should preset DBR !!! (9)
+	LDA #0					; a zero (2)
+	PHA						; into the stack (3)
+	PLB						; no other way to set it (4)
 ; *** place here HIGH priority async tasks, if required ***
 
 ; check whether jiffy or async (7 if periodic, 6 if async)
-	BIT VIA_J+IFR			; much better than LDA + ASL + BPL! (4)
+; might use (future) firmware call IRQ_SOURCE for complete generic kernel!
+BIT VIA_J+IFR			; much better than LDA + ASL + BPL! (4)
 		BVS periodic			; from T1 (3/2)
 
-; *** async interrupt otherwise ***
+; *********************************
+; *** async interrupt otherwise *** (arrives here in 36 clocks)
+; *********************************
 ; execute D_REQ in drivers (7 if nothing to do, 6+22*number of drivers until one replies, plus inner codes)
 	LDX dreq_mx				; get queue size (4)
 	BEQ ir_done				; no drivers to call (2/3)
 i_req:
 		PHX						; keep index! (3)
-		JSR (drv_async-2, X)	; call from table (6+...) expected to return in 8-bit size, at least indexes
+		JSR (drv_async-2, X)	; call from table (8+...) expected to return in 8-bit size, at least indexes
 		PLX						; restore index (4)
 			BCC isr_done			; driver satisfied, thus go away NOW (2/3)
 		DEX						; go backwards to be faster! (2+2)
@@ -37,14 +45,17 @@ i_req:
 ir_done:					; otherwise is spurious, due to separate BRK handler on 65816
 isr_done:
 	.al: .xl: REP #$30		; restore saved registers in full, just in case (3)
+	PLB						; eeeeeeeeek (4)
 	PLY						; restore registers (3x5 + 6)
 	PLX
 	PLA
 	RTI						; this will restore appropriate register size
 
+; *********************************************
 ; *** here goes the periodic interrupt code *** (4)
+; *********************************************
 periodic:
-	LDA VIA_J+T1CL			; acknowledge periodic interrupt!!! (4)
+	LDA VIA_J+T1CL			; acknowledge periodic interrupt!!! (4) *** IRQ_SOURCE should have done it already, if used
 
 ; *** scheduler no longer here, just an optional driver! But could be placed here for maximum performance ***
 
@@ -56,7 +67,7 @@ i_poll:
 		DEX						; go backwards to be faster! (2+2)
 		DEX						; no improvement with offset, all of them will be called anyway
 		PHX						; keep index! (3)
-		JSR (drv_poll, X)		; call from table (6...)
+		JSR (drv_poll, X)		; call from table (8...)
 ; *** here is the return point needed for B_EXEC in order to create the stack frame ***
 isr_sched_ret:					; *** take this standard address!!! ***
 		PLX						; restore index (4)
@@ -77,7 +88,7 @@ ip_done:
 ; execute D_SEC code if applies, take it much easier! (7 if none, 5+26*drivers, plus inner codes)
 ; to be done - balancing into, say, 8 time slots
 second:
-	.as: SEP #$30			; back to 8-bit memory (and indexes, just in case)
+	.as: .xs: SEP #$30		; back to 8-bit memory (and indexes, just in case)
 	LDX dsec_mx				; get queue size (4)
 		BEQ isr_done			; no drivers to call (2/3)
 i_sec:
@@ -92,5 +103,3 @@ i_sec:
 ; *** BRK handler ***
 brk_handler:				; this has a separate handler, check compatible label in firmware
 #include "isr/brk16.s"
-
-
