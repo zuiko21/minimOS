c02d0dfe8ce612559fd0d6845bd2b1620766bc3a
Carlos Santisteban <zuiko21@gmail.com>
2017-07-21 17:30:39 +0200

    CPUcheck minor cleanup

--- OS/firmware/modules/cpu_check.s
+++ OS/firmware/modules/cpu_check.s
@@ -1,29 +1,27 @@
 ; firmware CPU determining code for minimOS
-; version 0.9rc1
-; (c) 2015-2016 Carlos J. Santisteban
+; version 0.9.6a1
+; (c) 2015-2017 Carlos J. Santisteban
 ; essentially from the work of David Empson, Oct. '94
-; last modified 20170511-1357
+; last modified 20170721-1720
 
-	LDY #$00			; by default, NMOS 6502
+	LDY #$00			; by default, NMOS 6502 (0)
 	SED					; decimal mode
 	LDA #$99			; load highest BCD number
 	CLC					; prepare to add
 	ADC #$01			; will wrap around in Decimal mode
 	CLD					; back to binary
 		BMI cpuck_set		; NMOS, N flag not affected by decimal add
-	LDY #$03			; let's assume now '816
+	LDY #$03			; let's assume now '816 (3)
 	LDX #$00			; sets Z temporarily
-	.byt	$BB			; TYX, 65802 instruction will clear Z, NOP on all 65C02s won't
+	.byt	$BB			; TYX, 65816 instruction will clear Z, NOP on all 65C02s won't
 		BNE cpuck_set		; Branch only on 65802/816
-	LDX $EA				; save contents of $EA, really needed???
-	DEY					; try now with Rockwell
-	STY $EA				; store '2' there
-	.byt	$17, $EA	; RMB1 $EA, Rockwell R65C02 instruction
+	DEY					; try now with Rockwell (2)
+	STY $EA				; store '2' there, irrelevant contents
+	.byt	$17, $EA	; RMB1 $EA, Rockwell R65C02 instruction will reset stored value, otherwise NOPs
 	CPY $EA				; Location $EA unaffected on other 65C02
-	STX $EA				; restore contents, really needed???
 		BNE cpuck_set		; Branch only on Rockwell R65C02 (test CPY)
-	DEY					; revert to generic 65C02
-		BNE cpuck_set		; It's some form of 65C02, thus Y>0 (safer)
+	DEY					; revert to generic 65C02 (1)
+		BNE cpuck_set		; It's not zero, thus no need for BRA
 cpuck_list:
 	.asc	"NBRV"		; codes for NMOS (N), generic CMOS (B), Rockwell 65C02 (R?) and 65816/65802 (V)
 cpuck_set:
