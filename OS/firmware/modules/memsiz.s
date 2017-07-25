c349b74f94dd904720a37d75206121ba23ac00a8
Carlos Santisteban <zuiko21@gmail.com>
2017-07-25 20:33:14 +0200

    work on memsiz

--- OS/firmware/modules/memsiz.s
+++ OS/firmware/modules/memsiz.s
@@ -3,7 +3,7 @@
 ; 20-bit Jalapa version
 ; v0.6a3
 ; (c) 2017 Carlos J. Santisteban
-; last modified 20170724-2116
+; last modified 20170725-1948
 
 	LDA #$FF		; initial MSB
 	STA uz			; set pointer LSB
@@ -18,64 +18,36 @@ rs_hiram:
 		BNE rs_hiram		; repeat until bank 0
 	STZ $7FFF		; in case 64K or less
 	LDA @$77FFF		; *** number of RAM banks ***
-
-
+; store somewhere...
+	STZ uz			; ROM lsb
+	LDA #$80		; buggest ROM
 	LDA #$5A		; initial value (will swap with $A5)
 rs_shft:
-			STA [uz]		; store current value
-			CMP [uz]		; is correct?
+			STA (uz)		; store current value
+			CMP (uz)		; is correct?
 				BNE rs_not		; not, it must be ROM
 			EOR #$FF		; otherwise switch value...
 			BMI rs_shft		; ...until both done
-		LSR uz+2		; shift bank
-		BNE rs_shft		; try half size
-; *** size detected, or no more than 64K ***
-;
-; *** no longer in RAM, but check whether decoded? ***
-rs_not:
-#ifdef	SAFE
-#endif
-	SEC
-	ROR uz+1		; recover lost bit
-;******************continue...
-	LDA #1			; we will subtract to the end
-	SBC uz+2		; subtract selected bank (C was clear)
-	LDY #0			; no partial banks!
-	STZ uz			; neither partial pages
-	BRA rs_end		; store size and we're done
-
-; *** check whether the vectors appear at pointed bank [uz] ***
-rs_cmp:
-	LDY #5			; from last vector, backwards
-rs_loop:
-		LDA [uz], Y		; get byte pointed at high ROM
-		CMP $FFFA, Y		; compare with kernel-area vectors
-			BNE rs_dif		; not the same
-		DEY			; go for next byte
-		BPL rs_loop		; zero is included
-	SEC			; mark as repeat
-	RTS
-rs_dif:
-	CLC			; otherwise is not the same
-	RTS
-
-; *** compute small ROM size ***
-rs_small:
-	STZ uz			; pointer now goes at start
-	STZ uz+1
-	LDA @$F80000		; this is the first byte in ROM (bank $80 for 24-bit)
-rss_shft:
-		SEC			; will insert 1 from left
-		ROR uz+1		; shift page
-; might check C, if set something must be corrupt!
-		CMP [uz]		; coincides with what was inside?
-		BEQ rss_shft		; no, more mirroring
-; get size in pages
-	LDA #0
-	SEC
-	SBC uz+1
-	TAY
-	LDA #0
-rs_end:
-	STA uz+2		; store size in banks
-	STY uz+1		; just in case, store size in pages
+		SEC
+		ROR uz+1		; next size
+		BCC rs_shft		; at least 256 byte ROM
+	LDA uz+1		; *** first ROM page ***
+; store somewhere...
+; to be safe, check for RAM 32K or less
+	PHA			; save for later
+	DEC uz+1		; one less is max low-RAM address
+	DEC uz			; was 0, thus borrow OK
+	DEC			; page number...
+	STA (uz)		; ...written there
+	LDA #$7F		; standard 32K
+	STA uz+1		; new pointer
+rs_low:
+		STA (uz)		; page number written there
+		LSR			; in half
+		STA uz+1		; update
+		BNE rs_low		; at least 256 bytes
+	PLA			; theoretical end of RAM
+	STA uz+1		; restore pointer
+	LDA (uz)		; get supposed size
+	CMP $7FFF		; more than 32k?
+	BCS rs_done		; size in A is OK
