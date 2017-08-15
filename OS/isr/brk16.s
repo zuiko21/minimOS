3403f7ddddc91b445ccc03d98d12914993dc6e1f
Carlos Santisteban <zuiko21@gmail.com>
2017-08-15 17:58:04 +0200

    revised brk16 placeholder

--- OS/isr/brk16.s
+++ OS/isr/brk16.s
@@ -1,7 +1,7 @@
 ; BRK handler for minimOS·16
-; v0.5.1a1
-; (c) 2016 Carlos J. Santisteban
-; last modified 20161010-1320
+; v0.6a1
+; (c) 2016-2017 Carlos J. Santisteban
+; last modified 20170815-1757
 
 ; old handler as a placeholder, should end in RTS
 	.as: .xs: SEP #$30		; back to old 8-bit size, just in case
