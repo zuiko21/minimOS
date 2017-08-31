ff711b99e94ca91a4049eb4144d581d4cc35d349
Carlos Santisteban <zuiko21@gmail.com>
2017-08-31 16:20:14 +0200

    revised run816 block driver

--- OS/drivers/drv_run816.s
+++ OS/drivers/drv_run816.s
@@ -1,51 +1,86 @@
 ; minimOS basic I/O driver for run65816 BBC simulator
-; v0.9.6a3
+; v0.9.6a4
 ; *** new format for mOS 0.6 compatibility ***
 ; (c) 2017 Carlos J. Santisteban
-; last modified 20170524-1311
+; last modified 20170831-1551
 
 #include	"usual.h"
 .(
 ; *** begins with sub-function addresses table ***
 	.byt	DEV_CONIO	; D_ID, new values 20150323
-	.byt	A_CIN | A_COUT	; character I/O only, non relocatable
-	.word	kow_cin		; cin, input from keyboard
-	.word	kow_cout	; cout, output to display
-	.word	kow_rts		; initialize device, called by POST only
+	.byt	A_BLIN | A_BOUT	; I/O only, non relocatable
+	.word	kow_blin	; block? input from keyboard
+	.word	kow_bout	; block output to display
+	.word	kow_rts		; initialise device, does nothing
 	.word	kow_rts		; poll, NOT USED
-	.word	1			; irrelevant value as no polled interrupts
-	.word	kow_rts		; req, this one can't generate IRQs, thus CLC+RTS
-	.word	kow_rts		; sin, no block input
-	.word	kow_rts		; sout, no block output
+	.word	0			; irrelevant value as no polled interrupts
+	.word	kow_err		; req, this one can't generate IRQs, thus SEC+RTS
+	.word	kow_err		; no config
+	.word	kow_err		; no status
 	.word	kow_rts		; bye, no shutdown procedure
 	.word	debug_info	; info string
 	.word	0			; reserved for D_MEM, this is non-relocatable
 
 ; *** info string ***
 debug_info:
-	.asc	"Console I/O driver for run65816 BBC simulator, v0.9.6a1", 0
+	.asc	"Console I/O driver for run65816 BBC simulator, v0.9.6a4", 0
 
 ; *** output ***
+kow_bout:
+#ifdef	SAFE
+	LDA bl_siz		; check size in case is zero
+	ORA bl_siz+1
+		BEQ kow_rts		; nothing to do then
+#endif
+	LDA bl_ptr+1		; save pointer MSB...
+	PHA			; ...in case it changes
+; all checked, do block output!
+	LDY #0			; reset index
 kow_cout:
-	LDA io_c		; get char in case is control
+	LDA (bl_ptr), Y		; get char in case is control
 	CMP #13			; carriage return?
-	BNE kow_ncr		; if so, should generate CR+LF
+	BNE kow_ncr		; if so, should generate LF instead
 		LDA #10			; LF first (and only)
 kow_ncr:
 	JSR $c0c2		; print it
+	DEC bl_siz		; one less to go
+	BNE kow_blk		; go for next
+		LDA bl_siz+1		; are we done?
+			BEQ kow_end		; yeah!
+		DEC bl_siz+1		; or one page less
+kow_blk:
+	INY			; point to next
+	BNE kow_bout		; did not wrap
+		INC bl_ptr+1		; or update MSB
+		_BRA kow_bout		; and continue
+kow_end:
+	PLA			; retrieve saved MSB
+	STA bl_siz+1
 kow_rts:
 	_DR_OK
 
-; *** input ***
+; *** input *** will only get one!
 kow_cin:
+#ifdef	SAFE
+	LDA bl_siz		; check size in case is zero
+	ORA bl_siz+1
+		BEQ kow_rts		; nothing to do then
+#endif
 	JSR $c0bf		; will this work???
 ;	BCS kow_empty	; nothing available
 		CMP #LF			; linux-like LF?
 		BNE kow_emit	; do not process
 			LDA #CR			; or convert to CR
 kow_emit:
-		STA io_c		; store result otherwise
-		_DR_OK
+		_STAY(bl_ptr)		; store result otherwise
+		DEC bl_siz		; one less
+		LDA bl_siz
+		CMP #$FF		; will it wrap?
+		BNE kow_rts		; not
+			DEC bl_siz+1		; or update MSB
+		_DR_OK			; perhaps some special error code...
 ;kow_empty:
 ;	_DR_ERR(EMPTY)		; nothing yet
+kow_err:
+	_DR_ERR(UNAVAIL);
 .)
