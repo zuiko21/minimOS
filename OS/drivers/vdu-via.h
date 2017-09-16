47776b4049799bdea6ac5ff76faaa13c9a307d64
Carlos Santisteban <zuiko21@gmail.com>
2017-09-16 21:21:33 +0200

    fixed vars on VIA-VDU

--- OS/drivers/vdu-via.h
+++ OS/drivers/vdu-via.h
@@ -1,9 +1,9 @@
 ; minimOS driver for VIA-connected VDU
 ; variables for v0.6
-; last modified 20170915-2120
+; last modified 20170916-2120
 
 vdu_xor		.byt	0
-vdu_sc		.byt	0	; ???
+vdu_sch		.byt	0
 vdu_ba		.word	0
 vdu_cur		.word	0
 
