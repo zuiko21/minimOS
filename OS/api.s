29a0df7c09c2292b3e9391a8bea3aee0b128cf9b
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-11-20 10:26:42 +0100

    preparing new GESTALT interface, LOADLINK not seeting executables

--- OS/api.s
+++ OS/api.s
@@ -1,7 +1,7 @@
 ; minimOS generic Kernel API
 ; v0.6b3, must match kernel.s
 ; (c) 2012-2017 Carlos J. Santisteban
-; last modified 20171120-1009
+; last modified 20171120-1022
 
 ; no way for standalone assembly...
 
@@ -832,15 +832,15 @@ ll_found:
 	LDA (rh_scan), Y	; get it
 ; this is done instead of LDX fw_cpu
 ;	ADMIN(GESTALT)		; get full system info
-;	LDX cpu_ll		; installed CPU
-	LDX fw_cpu		; ********************* HACK AGAIN. MUST REVISE GESTALT INTERFACE ************************
-	CPX #'R'		; is it a Rockwell/WDC CPU?
+;	LDY cpu_ll		; installed CPU *** should return it in Y!
+	LDY fw_cpu		; ********************* HACK AGAIN. MUST REVISE GESTALT INTERFACE ************************
+	CPY #'R'		; is it a Rockwell/WDC CPU?
 		BEQ ll_rock		; from R down is OK
-	CPX #'B'		; generic 65C02?
+	CPY #'B'		; generic 65C02?
 		BEQ ll_cmos		; from B down is OK
-	CPX #'V'		; 65816 is supported but no better than a generic 65C02
+	CPY #'V'		; 65816 is supported but no better than a generic 65C02
 		BEQ ll_cmos
-	CPX #'N'		; old NMOS?
+	CPY #'N'		; old NMOS?
 		BEQ ll_nmos			; only NMOS code will do
 		_PANIC("{CPU?}")	; *** should NEVER arrive here, unless firmware variables are corrupt! ***
 ll_rock:
@@ -854,11 +854,13 @@ ll_nmos:
 		BNE ll_wrap		; otherwise is code for another architecture!
 ; present CPU is able to execute supplied code
 ll_valid:
+/*
 	LDY rh_scan+1	; and MSB
 	INY				; start from next page
 	_STZA ex_pt		; *** assume all headers are page-aligned ***
 	STY ex_pt+1		; save rest of execution pointer
-	_EXIT_OK
+*/
+_EXIT_OK
 ll_wrap:
 	_ERR(INVALID)	; something was wrong
 #else
