b42f8982774d7c93d6fcb9fed44b7edab411ab83
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-12-21 13:23:05 +0100

    CPU check code for new firmware template

--- OS/firmware/modules/cpu_check.s
+++ OS/firmware/modules/cpu_check.s
@@ -1,31 +1,34 @@
-; firmware CPU determining code for minimOS
-; version 0.9.6b1
+; firmware module for minimOS·65
 ; (c) 2015-2017 Carlos J. Santisteban
-; essentially from the work of David Empson, Oct. '94
-; last modified 20171121-0952
+; last modified 20171221-1317
 
+; *** CPU determining code *** 0.9.6b2
+; essentially from the work of David Empson, Oct. '94
+; NMOS and 65816 savvy, of course!
+; alters fw_cpu
+
+.(
 	LDY #$00			; by default, NMOS 6502 (0)
 	SED					; decimal mode
 	LDA #$99			; load highest BCD number
 	CLC					; prepare to add
 	ADC #$01			; will wrap around in Decimal mode
 	CLD					; back to binary
-		BMI cpuck_set		; NMOS, N flag not affected by decimal add
-	LDY #$03			; let's assume now '816 (3)
+		BMI cck_set			; NMOS, N flag not affected by decimal add
+	LDY #$03			; assume now '816 (3)
 	LDX #$00			; sets Z temporarily
-	.byt	$BB			; TYX, 65816 instruction will clear Z, NOP on all 65C02s won't
-		BNE cpuck_set		; Branch only on 65802/816
+	.byt	$BB			; TYX, 65816 instruction will clear Z, NOP on all 65C02s will not
+		BNE cck_set			; branch only on 65802/816
 	DEY					; try now with Rockwell (2)
 	STY $EA				; store '2' there, irrelevant contents
 	.byt	$17, $EA	; RMB1 $EA, Rockwell R65C02 instruction will reset stored value, otherwise NOPs
-	CPY $EA				; Location $EA unaffected on other 65C02
-		BNE cpuck_set		; Branch only on Rockwell R65C02 (test CPY)
+	CPY $EA				; location $EA unaffected on other 65C02s
+		BNE cck_set			; branch only on Rockwell R65C02 (test CPY)
 	DEY					; revert to generic 65C02 (1)
-		BNE cpuck_set		; It's not zero, thus no need for BRA
-cpuck_list:
-	.asc	"NBRV"		; codes for NMOS (N), generic CMOS (B), Rockwell 65C02 (R?) and 65816/65802 (V)
-cpuck_set:
-	LDA cpuck_list, Y	; get proper code from investigation
-pha:pha
-;	STA fw_cpu			; store in variable ** firmware standard label
-; will just return type in A
+		BNE cck_set			; cannot be zero, thus no need for BRA
+cck_lst:
+	.asc	"NBRV"		; codes for NMOS (N), generic CMOS (B), Rockwell 65C02 (R) and 65816/65802 (V)
+cck_set:
+	LDA cck_lst, Y		; get proper code from investigation
+	STA fw_cpu			; store in variable (now nothing to do with firmware template)
+.)
