ab472160d69b3ed3ec9a453a8d9397d4f8e8e678
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-12-21 13:32:29 +0100

    NMOS savvyness module

---
+++ OS/firmware/modules/nmos_savvy.s
@@ -0,0 +1,18 @@
+; firmware module for minimOS·65
+; (c) 2017 Carlos J. Santisteban
+; last modified 20171221-1330
+
+; *** NMOS-savvyness check ***
+; assume A holds CPU code!
+
+.(
+#ifdef	SAFE
+#ifndef	NMOS
+;	LDA fw_cpu		; ...in case A was not preserved after CPU check
+	CMP #'N'		; is it NMOS? not supported!
+	BNE fw_cpuOK	; otherwise continue
+		JMP lock		; cannot handle BRK, alas
+fw_cpuOK:
+#endif
+#endif
+.)
