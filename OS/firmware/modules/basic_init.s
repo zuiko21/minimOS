765eee90a44c3429151f5688e1524d6d5e3c0054
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-12-21 13:00:33 +0100

    created basic init module

---
+++ OS/firmware/modules/basic_init.s
@@ -0,0 +1,19 @@
+; firmware module for minimOS·65
+; (c)2017 Carlos J. Santisteban
+; last modified 20171221-1258
+
+; *** basic initialisation ***
+; assumed to start at 'reset' label (in template file)
+; NMOS and 65816 savvy
+; no interface needed
+
+-reset:
+	SEI				; cold boot, best assume nothing (2)
+	CLD				; just in case, a must for NMOS (2)
+; * this is in case a 65816 is being used, but still compatible with all *
+	SEC				; would set back emulation mode on C816
+	.byt	$FB		; XCE on 816, NOP on C02, but illegal 'ISC $0005, Y' on NMOS!
+	ORA $0			; the above would increment some random address in zeropage (NMOS) but this one is inocuous on all CMOS
+; * end of 65816 specific code *
+	LDX #SPTR		; initial stack pointer, machine-dependent, must be done in emulation for '816 (2)
+	TXS				; initialise stack (2)
