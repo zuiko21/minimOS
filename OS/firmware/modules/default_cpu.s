aa855b245e37bb8544124a3dc9bdeb7abacd2574
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-12-21 13:26:06 +0100

    set default CPU type

---
+++ OS/firmware/modules/default_cpu.s
@@ -0,0 +1,10 @@
+; firmware module for minimOS·65
+; (c) 2017 Carlos J. Santisteban
+; last modified 20171221-1324
+
+; *** set default CPU type ***
+; NMOS and 65816 savvy
+; alters fw_cpu
+
+	LDA #CPU_TYPE		; from options.h (2)
+	STA fw_cpu			; store variable (4)
