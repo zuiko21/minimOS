b0b2a434b42c8a46a2e6c72617ae1c18ce0b669c
Carlos Santisteban <zuiko21@gmail.com>
2018-01-17 14:31:55 +0100

    proper 16-bit std_nmi handler

--- OS/firmware/modules/std_nmi16.s
+++ OS/firmware/modules/std_nmi16.s
@@ -8,9 +8,9 @@
 
 ; placeholder, just enable interrupts and return
 	TSX				; get current stack pointer
-	LDA $010A, X	; in order to get P from the stack (jsr, 4@sysptr, 4regs!)
+	LDA $010E, X	; in order to get P from the stack (proper 16-bit stackframe)
 	AND #%11110011	; remove interrupt mask & decimal mode bits
-	STA $010A, X	; ints will be restored upon exit
+	STA $010E, X	; ints will be restored upon exit
 	RTL				; back to handler end, hopefully everything settled now
 ; 65816 should use RTL but as long as it is in bank 0, it is OK
 ; however, as this peeks into stack, a proper 16-bit version is needed
