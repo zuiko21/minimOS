3ee859aedc00caa4c2c4b5ffb182afbebd393fbf
Carlos Santisteban <zuiko21@gmail.com>
2018-01-20 21:43:16 +0100

    8b FW vars from template

--- OS/firmware/chihuahua.h
+++ OS/firmware/chihuahua.h
@@ -1,16 +1,9 @@
 ; firmware variables for minimOS on Chihuahua (PLUS)
-; v0.9.6b2
-; (c) 2015-2017 Carlos J. Santisteban
-; last modified 20171114-0929
+; v0.9.6b3
+; (c) 2015-2018 Carlos J. Santisteban
+; last modified 20180120-2140
 
 -sysram:
-fw_table	.dsb	LAST_API, $0	; more efficient usage 171114, NOT available in 128-byte systems
-fw_isr		.word	0			; ISR vector
-fw_nmi		.word	0			; NMI vector, fortunately checks for integrity
-fw_brk		.word	0			; BRK vector eeeeeeeeeek
-fw_warm		.word	0			; start of kernel, new 20150220
-fw_cpu		.byt	'B'			; CPU type ('B'=generic 65C02...)
-himem		.byt	0			; number of available 'kernel-RAM' pages, 0 means 128-byte RAM
-irq_freq	.word	200			; jiffy interrupts per second, was into sysvars
-
+; *** standard FW vars from template ***
+#include "firmware/template.h"
 ; kernel system variables to follow
--- OS/firmware/run02.h
+++ OS/firmware/run02.h
@@ -1,20 +1,14 @@
 ; firmware variables for minimOS on run65816 BBC simulator
 ; 8-bit kernels!
-; v0.9.6b2
-; (c) 2017 Carlos J. Santisteban
-; last modified 20171114-0932
+; v0.9.6b3
+; (c) 2017-2018 Carlos J. Santisteban
+; last modified 20180120-2138
 
 -sysram:
 ; lowram option for testing only
 #ifndef	LOWRAM
 fw_table	.dsb	LAST_API, $0	; more efficient usage 171114, NOT available in 128-byte systems
 #endif
-fw_isr		.word	0			; ISR vector
-fw_nmi		.word	0			; NMI vector, fortunately checks for integrity
-fw_brk		.word	0			; BRK vector, new 20170602
-fw_warm		.word	0			; start of kernel, new 20150220
-fw_cpu		.byt	'V'			; CPU type ('V'= 65C816)
-himem		.byt	0			; number of available 'kernel-RAM' pages, 0 means 128-byte RAM
-irq_freq	.word	200			; jiffys per second
-
+; *** standard FW vars from template ***
+#include "firmware/template.s"
 ; kernel sysvars to follow
