5a2e34fb87de2a44edad4cbb0d0866acb7de09a5
Carlos Santisteban <zuiko21@gmail.com>
2018-01-29 14:34:36 +0100

    fixed BRK handler for NMOS

--- OS/firmware/modules/brk_hndl.s
+++ OS/firmware/modules/brk_hndl.s
@@ -1,12 +1,16 @@
 ; firmware module for minimOS·65
 ; (c)2018 Carlos J. Santisteban
-; last modified 20180129-1428
+; last modified 20180129-1434
 
 ; *** generic BRK handler for 65(C)02 ***
 ; **** preliminary, 65816-like  code ****
+; NMOS-savvy
 
 -brk_hndl:				; label from vector list
 .(
+#ifdef	NMOS
+	CLD					; eeeeeeeeeeeeeeeek
+#endif
 ; much like the ISR start
 	PHA					; save registers (3x3)
 	_PHX
@@ -19,7 +23,7 @@
 ; restore full status and exit
 	_PLY				; restore status and return (3x4)
 	_PLX
-	_PLA
+	PLA
 	RTI
 ; as no indirect call is available, call here and return to handler
 brk_call:
