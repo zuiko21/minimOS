142b04140e2ab7011b835fe632e0b9edea7caa5b
Carlos Santisteban <zuiko21@gmail.com>
2018-01-31 09:04:05 +0100

    some CPU-specific FW modules optimisation

---
+++ OS/firmware/modules/basic_init02.s
@@ -0,0 +1,14 @@
+; firmware module for minimOS·65
+; (c) 2017-2018 Carlos J. Santisteban
+; last modified 20180131-0855
+
+; *** basic initialisation ***
+; assumed to start at 'reset' label (in template file)
+; NMOS savvy, save a few bytes if a 65816 cannot be used!
+; no interface needed
+
+-reset:
+	SEI				; cold boot, best assume nothing (2)
+	CLD				; just in case, a must for NMOS (2)
+	LDX #SPTR		; initial stack pointer, machine-dependent, must be done in emulation for '816 (2)
+	TXS				; initialise stack (2)
--- OS/firmware/modules/std_nmi16.s
+++ OS/firmware/modules/std_nmi16.s
@@ -1,17 +1,14 @@
 ; standard NMI handler for minimOS·16 firmware
-; v0.6a4
 ; (c) 2015-2018 Carlos J. Santisteban
-; last modified 20180117-1440
+; last modified 20180131-0900
 
 ; in case of standalone assembly
 #include "usual.h"
 
 ; placeholder, just enable interrupts and return
-	.xl: REP #$10		; *** 16-bit index ***
-	TSX					; get current stack pointer, full 16-bit
-	LDA $E, X			; in order to get P from the stack (proper 16-bit stackframe)
+	LDA $E, S			; in order to get P from the stack (proper 16-bit stackframe)
 	AND #%11110011		; remove interrupt mask & decimal mode bits
-	STA $E, X			; ints will be restored upon exit
+	STA $E, S			; ints will be restored upon exit
 	RTL					; back to handler end, hopefully everything settled now
 ; 65816 should use RTL but as long as it is in bank 0, it is OK
 ; however, as this peeks into stack, a proper 16-bit version is needed
