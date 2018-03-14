a09f452950ef991680c708a4cde3cb7d9b1fce15
Carlos Santisteban <zuiko21@gmail.com>
2018-03-14 09:11:51 +0100

    PB7 approach for theremin

--- forge/theremin.s
+++ forge/theremin.s
@@ -1,70 +1,85 @@
 ; stub for optical Theremin app
 ; (c) 2018 Carlos J. Santisteban
-; v0.1
-; last modified 20180313-1118
+; v0.2
+; last modified 20180314-0911
 
 ; to be assembled from OS/
 #include "usual.h"
 
-; appropriate interrupt speed for a 1 MHz system
-#define		IRQ_US		600
+; new approach, notes are generated via PB7/T1 interrupts
+; volume is set via PWM thru Serial register, T2 at full speed
+; buzzer sounds when PB7=0 & CB2=1 (use a diode or suitable amp)
+; volume polling was done every ~80ms, this way could be up to 145ms, still OK
 
 ; *** init code ***
-; must set SS in T2 free-run mode, T1 as continuous interrupts (NOT toggling PB7) every ~600 uS
-; CA2 is NON-independent interrupt on low-to-high (for tone)
-; make sure PB7 low, PA7 input (volume interrupt) and PA0...6 as output
+; must set SS in T2 free-run mode, T1 as continuous interrupts (toggling PB7)
+; CA2 is independent interrupt on low-to-high (for pitch ionterrupt)
+; PB7 output, PA7 input (volume interrupt*) and PA0...6 as output
+; perhaps CA1 as volume interrupt will make things simpler!
+; pitch DAC thru weighted resistors at PA0...4, volume DAC at PA5-PA6
 ot_init:
-	LDA #%
+;	LDA #%
 	
-	LDA #<IRQ_US-2		; counter LSB value
+	LDA #110			; counter LSB value, about 440Hz PB7 @ 1 MHz (will be set later, but at least get it running)
 	STA VIA_J+T1CL		; set counter (and latch)
-	LDA #>IRQ_US-2		; same for MSB
+	LDA #4				; same for MSB
 	STA VIA_J+T1CH		; start counting!
 
 ; *** jiffy interrupt task, will increase counters and poll the volume sensor ***
 ot_irq:
 	PHA					; will be altered anyway
-; *** must check whether periodic or from CA2 ***
+; *** must check whether periodic (next value) or from CA2 (set pitch) ***
 	LDA #1				; mask for CA2
 	BIT VIA_J+IFR		; check interrupt sources
-		BNE ot_tone			; it is CA2, set tone
-		BVC ot_nvol			; it is NOT jiffy, thus spurious
-; *** otherwise it is the jiffy interrupt task ***
-; must acknowledge interrupt!!!!!!
-	INC VIA_J+IORA		; increase counters
-	LDA #%00011111		; set mask for tone ADC bits
-	BIT VIA_J+IORA		; check against current value
-	BNE ot_nvol			; volume bits did not change
-		BMI ot_nvol			; otherwise, check if we had set volume
+		BNE ot_pitch		; it is CA2, set pitch
+		BVC ot_vol			; it is jiffy
+; in case CA1 is used for volume interrupt...
+		
+		; otherwise it is spurious!
+; *** it is the jiffy interrupt task ***
+ot_vol:
+; must acknowledge interrupt!!!
+		INC VIA_J+IORA		; increase counters
+		NOP					; *** small delay allowing the OpAmp to rise its output!
+		LDA #%00011111		; set mask for pitch ADC bits
+		BIT VIA_J+IORA		; check against current value (respect 4uS delay from INC!!!)
+		BNE ot_exit			; volume bits will not change
+		BPL ot_exit			; or we have not detected current volume
+; PA7 went high, we must set volume the first time ONLY
+; this will be MUCH simpler if we use, say, positive-transition CA1 for volume interrupt instead of polling!
 			_PHX				; will use this
-			LDA VIA_J+IORA		; still scanning, get stored value as %0vvttttt
-			LSR					; shift as needed, now %00vvtttt (or CLC)
-			LSR					; %000vvttt (if ROL, %vvttttt0)
-			LSR					; %0000vvtt (if ROL, %vttttt00, C=v.h)
-			LSR					; %00000vvt (if ROL, %ttttt00v, C=v.l)
-			LSR					; %000000vv as needed (if ROL, %tttt00vv, C=t.h)
-;			AND #%00000011		; filter relevant (only needed with CLC/ROL)
+			LDA VIA_J+IORA		; still scanning, get stored value as %xvvttttt
+			
+			AND #%01111111		; must clear bit 7!
+			LSR					; shift as needed, now %00vvtttt
+			LSR					; %000vvttt
+			LSR					; %0000vvtt
+			LSR					; %00000vvt
+			LSR					; %000000vv as needed
 			TAX					; eeeeeeeeeeeeeek
 			LDA ot_patts, X		; get bit pattern for this volume
-			STA VIA_J+VSR		; set for output
+			STA VIA_J+VSR		; set for PWM control output
 			_PLX				; restore reg
-ot_nvol:
+; *** end of ISR ***
+ot_exit:
 	PLA					; restore reg
 	RTI					; and we are done
 
-; *** arrive here whenever CA2 is triggered
-; assume A is pushed into stack
-ot_tone:
+; *** arrive here whenever CA2 is triggered (pitch value is set)
+; assume A is already pushed into stack
+ot_pitch:
 	_PHX				; will be needed
-	LDA VIA_J+IORA		; get counter value... and clear CA2 interrupt source!
-	AND #%00011111		; filter tone bits
+; as CA2 was independent interrupt, must acknowledge source!
+	LDA VIA_J+IORA		; get counter value
+	AND #%00011111		; filter pitch bits
 	ASL					; twice...
 	TAX					; ...as index
-	LDA ot_notes, X		; get tone LSB
-	STA VIA_J+T2CL		; set T2 LSB
-;	LDA ot_notes+1, X	; same for MSB
-;	STA VIA_J+T2CH		; this will load upon next cycle
+	LDA ot_notes, X		; get pitch LSB
+	STA VIA_J+T1LL		; set T1 LSB
+	LDA ot_notes+1, X	; same for MSB
+	STA VIA_J+T1LH		; this will load upon next cycle
 	_PLX				; restore regs and exit
+; standard ISR exit, not worth reusing the above one
 	PLA
 	RTI
 
@@ -72,9 +87,9 @@ ot_tone:
 ; shifted bit patterns for diverse volume levels (hopefully!)
 ot_patts:
 	.byt	0			; 0 = MUTE
-	.byt	%10000000	; 1 = 25%
-	.byt	%11000000	; 2 = 50% (will not use 75% as per log ear response)
-	.byt	%11110000	; 3 = 100%, fully symmetric square wave
+	.byt	%10000000	; 1 = 12.5%
+	.byt	%10010010	; 2 = 37.5% (will not use 25 & 50% as per log ear response)
+	.byt	%11111111	; 3 = 100%, continuous PB7 output
 
 ; VIA T1 values for 32 chromatic notes
 ot_notes:
