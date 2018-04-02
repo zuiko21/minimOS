2257f0905171c4a82e574e876f72fa484d5b3455
Carlos Santisteban <zuiko21@gmail.com>
2018-04-02 13:16:07 +0200

    created fast API & ADMIN tables

---
+++ OS/adm_opt.h
@@ -0,0 +1,16 @@
+; minimOS 0.6rc1 FAST Firmware calls
+; (c) 2018 Carlos J. Santisteban
+; last modified 20180402-1314
+
+#ifdef	API_OPT
+#define		_ADMIN(GESTALT)		JSR gestalt
+#define		_ADMIN(SET_ISR)		JSR set_isr
+#define		_ADMIN(SET_NMI)		JSR set_nmi
+#define		_ADMIN(SET_DBG)		JSR set_dbg
+#define		_ADMIN(JIFFY)		JSR jiffy
+#define		_ADMIN(IRQ_SRC)		JSR irq_src
+#define		_ADMIN(POWEROFF)	JSR poweroff
+#define		_ADMIN(FREQ_GEN)	JSR freq_gen
+#define		_ADMIN(INSTALL)		JSR install
+#define		_ADMIN(PATCH)		JSR patch
+#endif
---
+++ OS/api_opt.h
@@ -0,0 +1,38 @@
+; minimOS 0.6rc1 FAST API calls
+; (c) 2018 Carlos J. Santisteban
+; last modified 20180402-1302
+
+#ifdef	API_OPT
+#define		_KERNEL(COUT)		JSR cout
+#define		_KERNEL(CIN)		JSR cin
+#define		_KERNEL(STRING)		JSR string
+#define		_KERNEL(READLN)		JSR readln
+#define		_KERNEL(BLOUT)		JSR blout
+#define		_KERNEL(BLIN)		JSR blin
+#define		_KERNEL(BL_CNFG)	JSR bl_cnfg
+#define		_KERNEL(BL_STAT)	JSR bl_stat
+#define		_KERNEL(OPEN_W)		JSR open_w
+#define		_KERNEL(CLOSE_W)	JSR close_w
+#define		_KERNEL(FREE_W)		JSR free_w
+#define		_KERNEL(UPTIME)		JSR uptime
+#define		_KERNEL(SHUTDOWN)	JSR shutdown
+#define		_KERNEL(LOADLINK)	JSR loadlink
+#define		_KERNEL(B_FORK)		JSR b_fork
+#define		_KERNEL(B_EXEC)		JSR b_exec
+#define		_KERNEL(B_SIGNAL)	JSR b_signal
+#define		_KERNEL(B_FLAGS)	JSR b_flags
+#define		_KERNEL(SET_HNDL)	JSR set_hndl
+#define		_KERNEL(B_YIELD)	JSR b_yield
+#define		_KERNEL(GET_PID)	JSR get_pid
+#define		_KERNEL(DR_INFO)	JSR dr_info
+#define		_KERNEL(AQ_MNG)		JSR aq_mng
+#define		_KERNEL(PQ_MNG)		JSR pq_mng
+#define		_KERNEL(DR_INST)	JSR dr_inst
+#define		_KERNEL(DR_SHUT)	JSR dr_shut
+#define		_KERNEL(MALLOC)		JSR malloc
+#define		_KERNEL(FREE)		JSR free
+#define		_KERNEL(MEMLOCK)	JSR memlock
+#define		_KERNEL(RELEASE)	JSR release
+#define		_KERNEL(TS_INFO)	JSR ts_info
+#define		_KERNEL(SET_CURR)	JSR set_curr
+#endif
