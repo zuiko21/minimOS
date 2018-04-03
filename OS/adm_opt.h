e27495be8068aaef864c929a63c21756d36d2c93
Carlos Santisteban <zuiko21@gmail.com>
2018-04-03 12:10:04 +0200

    stringifying API_OPT macros?

--- OS/adm_opt.h
+++ OS/adm_opt.h
@@ -1,21 +1,19 @@
 ; minimOS 0.6rc2 FAST Firmware calls
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180403-0959
+; last modified 20180403-1108
 
 
-#define	{	(
-#define	}	)
 #ifdef	API_OPT
-#define		_ADMIN{GESTALT}		JSR gestalt
-#define		_ADMIN{SET_ISR}		JSR set_isr
-#define		_ADMIN{SET_NMI}		JSR set_nmi
-#define		_ADMIN{SET_DBG}		JSR set_dbg
-#define		_ADMIN{JIFFY}		JSR jiffy
-#define		_ADMIN{IRQ_SRC}		JSR irq_src
-#define		_ADMIN{POWEROFF}	JSR poweroff
-#define		_ADMIN{FREQ_GEN}	JSR freq_gen
+#define		_ADMIN(#0)	JSR gestalt
+#define		_ADMIN(#2)	JSR set_isr
+#define		_ADMIN(#4)	JSR set_nmi
+#define		_ADMIN(#6)	JSR set_dbg
+#define		_ADMIN(#8)	JSR jiffy
+#define		_ADMIN(#10)	JSR irq_src
+#define		_ADMIN(#12)	JSR poweroff
+#define		_ADMIN(#14)	JSR freq_gen
 #ifndef	LOWRAM
-#define		_ADMIN{INSTALL}		JSR install
-#define		_ADMIN{PATCH}		JSR patch
+#define		_ADMIN(#16)	JSR install
+#define		_ADMIN(#18)	JSR patch
 #endif
 #endif
