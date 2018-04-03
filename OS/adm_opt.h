3f93617e356467b44a2750dd85745c637472ff3c
Carlos Santisteban <zuiko21@gmail.com>
2018-04-03 12:15:32 +0200

    neither does ##

--- OS/adm_opt.h
+++ OS/adm_opt.h
@@ -1,19 +1,19 @@
 ; minimOS 0.6rc2 FAST Firmware calls
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180403-1108
+; last modified 20180403-1114
 
 
 #ifdef	API_OPT
-#define		_ADMIN(#0)	JSR gestalt
-#define		_ADMIN(#2)	JSR set_isr
-#define		_ADMIN(#4)	JSR set_nmi
-#define		_ADMIN(#6)	JSR set_dbg
-#define		_ADMIN(#8)	JSR jiffy
-#define		_ADMIN(#10)	JSR irq_src
-#define		_ADMIN(#12)	JSR poweroff
-#define		_ADMIN(#14)	JSR freq_gen
+#define		_ADMIN(##0)	JSR gestalt
+#define		_ADMIN(##2)	JSR set_isr
+#define		_ADMIN(##4)	JSR set_nmi
+#define		_ADMIN(##6)	JSR set_dbg
+#define		_ADMIN(##8)	JSR jiffy
+#define		_ADMIN(##10)	JSR irq_src
+#define		_ADMIN(##12)	JSR poweroff
+#define		_ADMIN(##14)	JSR freq_gen
 #ifndef	LOWRAM
-#define		_ADMIN(#16)	JSR install
-#define		_ADMIN(#18)	JSR patch
+#define		_ADMIN(##16)	JSR install
+#define		_ADMIN(##18)	JSR patch
 #endif
 #endif
