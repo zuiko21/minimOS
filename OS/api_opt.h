468e87f6b96cd44b2179c5b0354ec323105062ae
Carlos Santisteban <zuiko21@gmail.com>
2018-04-05 13:18:01 +0200

    CPP compliant APIs

--- OS/api_opt.h
+++ OS/api_opt.h
@@ -1,43 +1,43 @@
-; minimOS 0.6rc4 FAST API calls
+; minimOS 0.6rc5 FAST API calls
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180404-0856
+; last modified 20180405-1309
 
 #ifdef	API_OPT
 #define		_KERNEL(a)	_KFAST_ ## a
 
-#define		_KFAST_0	JSR cout
-#define		_KFAST_2	JSR cin
-#define		_KFAST_4	JSR string
-#define		_KFAST_6	JSR readln
-#define		_KFAST_8	JSR blout
-#define		_KFAST_10	JSR blin
-#define		_KFAST_12	JSR bl_cnfg
-#define		_KFAST_14	JSR bl_stat
-#define		_KFAST_16	JSR open_w
-#define		_KFAST_18	JSR close_w
-#define		_KFAST_20	JSR free_w
-#define		_KFAST_22	JSR uptime
-#define		_KFAST_24	JSR shutdown
-#define		_KFAST_26	JSR loadlink
-#define		_KFAST_28	JSR b_fork
-#define		_KFAST_30	JSR b_exec
-#define		_KFAST_32	JSR b_signal
-#define		_KFAST_34	JSR b_flags
-#define		_KFAST_36	JSR set_hndl
-#define		_KFAST_38	JSR b_yield
-#define		_KFAST_40	JSR get_pid
-#define		_KFAST_42	JSR dr_info
-#define		_KFAST_44	JSR aq_mng
-#define		_KFAST_46	JSR pq_mng
-#define		_KFAST_48	JSR dr_inst
-#define		_KFAST_50	JSR dr_shut
+#define		_KFAST_COUT		JSR cout
+#define		_KFAST_CIN		JSR cin
+#define		_KFAST_STRING	JSR string
+#define		_KFAST_READLN	JSR readln
+#define		_KFAST_BLOUT	JSR blout
+#define		_KFAST_BLIN		JSR blin
+#define		_KFAST_BL_CNFG	JSR bl_cnfg
+#define		_KFAST_BL_STAT	JSR bl_stat
+#define		_KFAST_OPEN_W	JSR open_w
+#define		_KFAST_CLOSE_W	JSR close_w
+#define		_KFAST_FREE_W	JSR free_w
+#define		_KFAST_UPTIME	JSR uptime
+#define		_KFAST_SHUTDOWN	JSR shutdown
+#define		_KFAST_LOADLINK	JSR loadlink
+#define		_KFAST_B_FORK	JSR b_fork
+#define		_KFAST_B_EXEC	JSR b_exec
+#define		_KFAST_B_SIGNAL	JSR b_signal
+#define		_KFAST_B_FLAGS	JSR b_flags
+#define		_KFAST_SET_HNDL	JSR set_hndl
+#define		_KFAST_B_YIELD	JSR b_yield
+#define		_KFAST_GET_PID	JSR get_pid
+#define		_KFAST_DR_INFO	JSR dr_info
+#define		_KFAST_AQ_MNG	JSR aq_mng
+#define		_KFAST_PQ_MNG	JSR pq_mng
+#define		_KFAST_DR_INST	JSR dr_inst
+#define		_KFAST_DR_SHUT	JSR dr_shut
 #ifndef	LOWRAM
-#define		_KFAST_52	JSR malloc
-#define		_KFAST_54	JSR free
-#define		_KFAST_56	JSR memlock
-#define		_KFAST_58	JSR release
-#define		_KFAST_60	JSR ts_info
-#define		_KFAST_62	JSR set_curr
+#define		_KFAST_MALLOC	JSR malloc
+#define		_KFAST_FREE		JSR free
+#define		_KFAST_MEMLOCK	JSR memlock
+#define		_KFAST_RELEASE	JSR release
+#define		_KFAST_TS_INFO	JSR ts_info
+#define		_KFAST_SET_CURR	JSR set_curr
 	API_SIZE	=	64
 #else
 	API_SIZE	=	52
