68e8ac39505566fe43431ccbb12bdd6c787defc5
Carlos Santisteban <zuiko21@gmail.com>
2018-04-13 10:15:27 +0200

    new approach for minigaal

--- forge/minigaal.c
+++ forge/minigaal.c
@@ -1,12 +1,12 @@
 /*
  * miniGaal, VERY elementary HTML browser for minimOS
- * last modified 20180413-0826
+ * last modified 20180413-1014
  * */
 
 #include <stdio.h>
 
 // Global variables
-char tx[1000], c;		// the HTML input
+char tx[1000];			// the HTML input
 
 struct pila {
 	int		sp;
@@ -15,16 +15,16 @@ struct pila {
 
 char tags[] = "html*head*title*body*p*h1*br*hr*a*\0" ;	// recognised tags separated by asterisks!
 /*
- * TOKEN numbers (-1 is invalid)
- * 0 = html (do nothing)
- * 1 = head (expect for title at least)
- * 2 = title (show betweeen [])
- * 3 = body (do nothing)
- * 4 = p (print text, then a couple of CRs)
- * 5 = h1 (print text _with spaces between letters_)
- * 6 = br (print CR)
- * 7 = hr (print '------------------------------------')
- * 8 = a (link????)
+ * TOKEN numbers (0 is invalid) new base 20180413
+ * 1 = html (do nothing)
+ * 2 = head (expect for title at least)
+ * 3 = title (show betweeen [])
+ * 4 = body (do nothing)
+ * 5 = p (print text, then a couple of CRs)
+ * 6 = h1 (print text _with spaces between letters_)
+ * 7 = br (print CR)
+ * 8 = hr (print '------------------------------------')
+ * 9 = a (link????)
  * */
  
 // Several functions
@@ -45,63 +45,78 @@ char pop(void) {			// pop a tokenised tag from stack, or -1 if empty
 	}
 }
 
-int looktag(int pos) {
-	int start=pos;		// keep this position for retying
-	int lista=0;		// label-list scanning pointer
-	int found=0;		// will scan until found...
-	int ended=0;		// ...or the tag list ends
-	int token=0;		// token list counter
-	char del;			// found delimiter
+char tag(int pos) {		// detect tags and return token number (negative if CLOSING, zero if invalid)
+	int start=pos;			// keep this position for retrying
+	int cur=0;				// label-list scanning index
+	char token=1;			// token list counter
+	char del;				// found delimiter
 
-	while (!found && !ended) {
+	while (-1) {
+		// beware of closing tags
 		if (tx[pos] == '/') {	// it is a closing tag
-			token=pop();		// try to pull it from stack
-			printf("[CLOSE %d]", token);
-			return ++pos;
+			token=pop();			// try to pull token from stack
+			if (!token) {
+				printf("{EMPTY}");
+			}
+			else
+			{
+				printf("[CLOSE %d]", token);
+				// do something here?
+			}
+			return -token;			// report it and do not look further, NOTE SIGN
 		}
-		while (tx[pos++] == tags[lista++]) {
-			printf("%c...",tx[pos-1]);	// scan until end of label, either way, or any difference found
+		// find a matching substring
+		while (tx[pos++] == tags[cur++]) {
+			printf("%c...",tx[pos-1]);
 		}
-		del = tx[pos-1];						// check whatever ended the label
-		printf("\n(delimiter %c, lista %c)", del, tags[lista-1]);
-		if ((tags[--lista] == '*') && (del==' ' || del=='\t' || del=='\n' || del=='>' || del=='/')) {
+		// check whether there is a recognised tag, or a mismatch
+		del = tx[pos-1];			// check whatever ended the label
+		printf("\n(delimiter %c, lista %c)", del, tags[cur-1]);
+		if ((tags[--cur] == '*') && (del==' ' || del=='\t' || del=='\n' || del=='>' || del=='/')) {
 			// found a proper delimiter for this tag
-			found=1;							// we have found a label!
 			printf("OK\n");
-		} else {
+			return token;				// is this OK already?
+		} else {					// string did not match label
 			printf("<No>");
-			// different label, skip from list and try next one
-			if (tags[lista+1] == 0) {			// was it the last one?
+			if (tags[cur+1] == '\0') {	// was it the last one?
 				printf("{EOL}\n");
-				ended=1;						// no more to scan
+				return 0;					// no more to scan
 			} else {
-				pos=start;						// otherwise will try next, back where it started
-				while(tags[lista++]!='*')	printf("_%c", tags[lista-1]);		// skip current tag
+				// skip label from list and try next one
+				pos=start;					// otherwise will try next, back where it started
+				while(tags[cur++]!='*')	printf("_%c", tags[cur-1]);		// skip current tag
 				token++;
+				if (tags[cur] == '\0')		return 0;	// no more tags to scan!
 			}
 		}
-		if (found) {	// properly recognised tag, push it into stack!
-printf("[%d OK]", token);
-			if(push(token)==-1)	printf("{***OVERFLOW***}");		// try to push the token
-			
-		} else {
+	}
+}
+
+/*
+// no more here...
+		if (found) {
+			// properly recognised tag, push it into stack!
+			printf("[%d OK]", token);
+			token = push(token);	// try to push the token
+			if (!token)				printf("{***OVERFLOW***}");
+			// perhaps should check for effects here?
+		} else {	// *** I think it will NEVER reach here... ***
 			printf("{?}");
 			pos=start;
 		}
 	}
-	
-	return --pos;
-}
+ * */
 
 // *** main code ***
 int main(void)
 {
 	int pt=0, t;
+	char c;
 
 // init code
-	etiq.sp = 0;		// reset stack pointer!
-
-	fgets(tx, 1000, stdin);	// read keyboard
+	etiq.sp = 0;				// reset stack pointer!
+	printf("HTML input: ");
+	fgets(tx, 1000, stdin);		// read HTML from keyboard
 
 //if < is found, look for the label
 //	push it into stack
@@ -114,7 +129,48 @@ int main(void)
 		if (c=='<') {		// tag is starting
 		// should look for comments here
 			printf("\nTag ");
-			pt=looktag(pt);		// detect token and execute
+			t=tag(pt);			// detect token
+			// identify and execute the token
+			switch(t) {
+				case 1:				// <html> (do nothing)
+					break;
+				case 2:				// <head> (expect for title at least)
+					break;
+				case 3:				// <title> (show betweeen [])
+					break;
+				case 4:				// <body> (do nothing)
+					break;
+				case 5:				// <p> (print text, then a couple of CRs)
+					break;
+				case 6:				// <h1> (print text _with spaces between letters_)
+					break;
+				case 7:				// <br> (print CR)
+					break;
+				case 8:				// <hr> (print '------------------------------------')
+					break;
+				case 9:				// <a> (link????)
+					break;
+				// closing tags
+				case -1:			// <html> (do nothing)
+					break;
+				case -2:			// <head> (expect for title at least)
+					break;
+				case -3:			// <title> (show betweeen [])
+					break;
+				case -4:			// <body> (do nothing)
+					break;
+				case -5:			// <p> (print text, then a couple of CRs)
+					break;
+				case -6:			// <h1> (print text _with spaces between letters_)
+					break;
+				case -7:			// <br> (print CR)
+					break;
+				case -8:			// <hr> (print '------------------------------------')
+					break;
+				case -9:			// <a> (link????)
+					break;
+				default:
+			}
 			while ((tx[pt++] != '>') && (tx[pt-1]!='\0')) {
 				printf("%c>",tx[pt-1]);
 				if (tx[pt-1] == '/') {	// it is a closing tag
