35373702e7da3844b14f78c40859bff14fb9f12e
Carlos Santisteban <zuiko21@gmail.com>
2018-08-02 22:01:04 +0200

    keyboard driver will read from buffer

--- OS/drivers/asc_kbd.s
+++ OS/drivers/asc_kbd.s
@@ -1,7 +1,7 @@
 ; 64-key ASCII keyboard for minimOS!
 ; v0.6a1
 ; (c) 2012-2018 Carlos J. Santisteban
-; last modified 20180801-2308
+; last modified 20180802-2153
 
 ; VIA bit functions
 ; PA0...3	= input from selected column
@@ -19,7 +19,7 @@
 ; *** begins with sub-function addresses table ***
 	.byt	145		; physical driver number D_ID (TBD)
 	.byt	A_BLIN|A_POLL	; input driver, periodic interrupt-driven
-	.word	ak_read		; read at least 1 byte
+	.word	ak_read		; read from input buffer
 	.word	ak_err		; no output
 	.word	ak_init		; initialise 'device', called by POST only
 	.word	ak_poll		; periodic interrupt...
@@ -44,21 +44,32 @@ PB_MASK		= %01111111	; all used, PB7 free
 ; *** read key (only one byte of buffer will be filled at most ***
 ; ****************************************************************
 ak_read:
-#ifdef	SAFE
-	LDA bl_siz			; check remaining
-	ORA bl_siz+1
-		BEQ blck_end			; nothing to do
-#endif
-	JSR ak_get			; *** get one byte ***
-		BCS blck_end		; any error ends transfer!
-	LDA io_c			; received byte...
-	_STAY(bl_ptr)			; ...goes into buffer
-	DEC bl_siz			; one less to go
-	LDA bl_siz			; check whether wrapped
-	CMP #$FF
-	BNE blck_end			; no wrap, all done
-		DEC bl_siz+1			; ...or one page less
+	LDA bl_ptr+1			; save pointer MSB
+	PHA
+	LDY #0				; reset index
+ak_rloop:
+		LDA bl_siz			; check remaining
+		ORA bl_siz+1
+			BEQ blck_end			; nothing to do
+		JSR ak_get			; *** get one byte from buffer***
+			BCS blck_end		; any error ends transfer!
+		LDA io_c			; received byte...
+		STA (bl_ptr),Y			; ...goes into buffer
+		INY					; next byte, check carry
+		BNE ak_nw
+			INC bl_ptr+1
+ak_nw:
+		DEC bl_siz			; one less to go
+		LDA bl_siz			; check whether wrapped
+		CMP #$FF
+		BNE blck_end			; no wrap, all done
+			LDA bl_siz+1			; any page remaining?
+		BEQ blck_end			; no, exit
+			DEC bl_siz+1			; ...or one page less
+		_BRA ak_rloop
 blck_end:
+	PLA					; restore MSB
+	STA bl_ptr+1
 	RTS				; respect whatever error code
 
 
@@ -69,9 +80,9 @@ ak_init:
 	_DR_OK				; succeeded
 
 
-; *********************
-; *** read one byte ***
-; *********************
+; *********************************
+; *** read one byte from buffer ***
+; *********************************
 ak_get:
 	_DR_OK
 
