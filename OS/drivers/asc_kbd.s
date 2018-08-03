c173fab0e00421429420d8daca95adc8ad37c5c5
Carlos Santisteban <zuiko21@gmail.com>
2018-08-03 22:23:14 +0200

    keyboard driver has FIFO routines

--- OS/drivers/asc_kbd.s
+++ OS/drivers/asc_kbd.s
@@ -1,7 +1,7 @@
 ; 64-key ASCII keyboard for minimOS!
 ; v0.6a1
 ; (c) 2012-2018 Carlos J. Santisteban
-; last modified 20180802-2153
+; last modified 20180803-2159
 
 ; VIA bit functions
 ; PA0...3	= input from selected column
@@ -16,6 +16,7 @@
 ; ***********************
 #include "../usual.h"
 
+.(
 ; *** begins with sub-function addresses table ***
 	.byt	145		; physical driver number D_ID (TBD)
 	.byt	A_BLIN|A_POLL	; input driver, periodic interrupt-driven
@@ -23,7 +24,7 @@
 	.word	ak_err		; no output
 	.word	ak_init		; initialise 'device', called by POST only
 	.word	ak_poll		; periodic interrupt...
-	.word	1		; as fast as possible
+	.word	4		; 20ms scan seems fast enough
 	.word	ak_nreq		; D_ASYN does nothing
 	.word	ak_nreq		; no config
 	.word	ak_nreq		; no status
@@ -36,6 +37,7 @@ ak_info:
 	.asc	"ASCII keyboard v0.6", 0
 
 ; *** some definitions ***
+AF_SIZ		= 16		; buffer size (only 15 useable)
 
 PA_MASK		= %11110000	; PA0-3 as input, PA4-7 as output
 PB_MASK		= %01111111	; all used, PB7 free
@@ -53,7 +55,7 @@ ak_rloop:
 			BEQ blck_end			; nothing to do
 		JSR ak_get			; *** get one byte from buffer***
 			BCS blck_end		; any error ends transfer!
-		LDA io_c			; received byte...
+		LDA io_c			; received byte... *need?*
 		STA (bl_ptr),Y			; ...goes into buffer
 		INY					; next byte, check carry
 		BNE ak_nw
@@ -77,12 +79,51 @@ blck_end:
 ; *** initialise stuff ***
 ; ************************
 ak_init:
+; reset FIFO
+	_STZA ak_fi
+	_STZA ak_fo
+
 	_DR_OK				; succeeded
 
 
-; *********************************
-; *** read one byte from buffer ***
-; *********************************
+; *******************************
+; *** read one byte from FIFO *** A -> char, C = empty, uses X
+; *******************************
 ak_get:
+	LDX ak_fo			; get output position
+	CPX ak_fi			; is it empty?
+	BNE ak_some			; no, do extract
+		_DR_ERR(EMPTY)			; yes, do nothing
+ak_some:
+	LDA ak_buff, X		; extract char
+;	STA io_c			; *need?*
+	INX					; this is no more
+	CPX #AF_SIZ			; wrapped?
+	BNE ak_rnw				; no
+		LDX #0					; or yes, back to zero
+ak_rnw:
+	STX ak_fo			; eeeeeeeeeek
 	_DR_OK
 
+; *******************************
+; *** push one byte into FIFO *** A <- char, uses X
+; *******************************
+ak_push:
+	LDX ak_fi			; get input position
+	STA ak_buff, X		; insert char
+	INX					; go for first free position
+	CPX #AF_SIZ			; wrapped?
+	BNE ak_wnw				; no
+		LDX #0					; or yes, back to zero
+ak_wnw:
+	STX ak_fi			; update pointer
+	CPX ak_fo			; is it full?
+	BNE ak_room			; no, all OK
+		INC ak_fo			; yes, simply discard oldest byte
+		LDA ak_fo			; but check for wrap, too
+		CMP #AF_SIZ
+	BNE ak_room			; did not, all done
+		_STZA ak_fo			; or go back to zero
+ak_room:
+	_DR_OK
+.)
