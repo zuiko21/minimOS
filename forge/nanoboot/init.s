9f46b8c01c91c544600ba4c31c3eecf4e4d24e75
Carlos Santisteban <zuiko21@gmail.com>
2018-08-18 10:32:26 +0200

    timed init on nanoboot

--- forge/nanoboot/init.s
+++ forge/nanoboot/init.s
@@ -1,6 +1,6 @@
 ; startup nanoBoot for 6502
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180816-1816
+; last modified 20180818-1032
 
 ; *** needed zeropage variables ***
 ; nb_ptr (word) for initial address, will use as pointer
@@ -10,11 +10,11 @@
 ; nb_flag, sets bit 7 when a byte is ready (autoreset, or bit 7 clear)
 
 nb_init:
-	SEI					; make sure interrupts are off
-	LDY #1				; initial value
-	STY nb_rcv			; preset received value
-	STY nb_flag			; reset reception flag
-; *** set interrupt handlers ***
+	SEI					; make sure interrupts are off (2)
+	LDY #1				; initial value (2)
+	STY nb_rcv			; preset received value (3)
+	STY nb_flag			; reset reception flag (3)
+; *** set interrupt handlers *** (total 24)
 	LDX #<nb_irq
 	LDA #>nb_irq
 	STX fw_isr
@@ -25,46 +25,46 @@ nb_init:
 	STA fw_nmi+1
 ; *** wait for a valid nanoBoot link *** $4B, [start], [end]
 nb_wait:
-		BIT nb_flag			; received something?
-		BPL nb_wait
-	LDA nb_rcv			; check received
-	CMP #$4B			; valid nanoBoot link?
-	BNE nb_exit			; no, abort
+		BIT nb_flag			; received something? (3)
+		BPL nb_wait			; (3/2)
+	LDA nb_rcv			; check received (3)
+	CMP #$4B			; valid nanoBoot link? (2)
+	BNE nb_exit			; no, abort (2...)
 ; *** get nanoBoot header ***
-		LDX #0				; counter for bytes of header
-		STY nb_rcv			; preset value
-		STY nb_flag			; reset reception flag
+		LDX #0				; counter for bytes of header (2)
+		STY nb_rcv			; preset value (3)
+		STY nb_flag			; reset reception flag (3)
 nb_loop:
-				BIT nb_flag			; received something?
-				BPL nb_loop
-			LDA nb_rcv			; check received
-			STA nb_ptr, X			; store in variable
-			STY nb_rcv			; reset value
-			STY nb_flag			; clear bit 7
+				BIT nb_flag			; received something? (3)
+				BPL nb_loop			; (3/2)
+			LDA nb_rcv			; check received (3)
+			STA nb_ptr, X			; store in variable (4)
+			STY nb_rcv			; reset value (3)
+			STY nb_flag			; clear bit 7 (3)
 			INX
-			CPX #4				; all of header done?
-			BNE nb_loop
+			CPX #4				; all of header done? (2+2)
+			BNE nb_loop			; (3/2)
 ; prepare variables for transfer
-		LDX #1				; resetting value
-		LDY nb_ptr			; make a copy of initial address
+		LDX #1				; resetting value (2)
+		LDY nb_ptr			; make a copy of initial address (3+3)
 		LDA nb_ptr+1
-		STY nb_ex			; Y is offset already
+		STY nb_ex			; Y is offset already (3+3)
 		STA nb_ex+1
-		_STZA nb_ptr			; ready for indirect-indexed
+		_STZA nb_ptr			; ready for indirect-indexed (3 for CMOS)
 ; *** execute transfer ***
 nb_get:
-				BIT nb_flag			; received something?
-				BPL nb_get
-			LDA nb_rcv			; check received
-			STA (nb_ptr), Y		; store at destination
-			STX nb_rcv			; preset value
-			STX nb_flag			; clear bit 7
-			INY					; next
-			BNE nbg_nw			; check MSB too
+				BIT nb_flag			; received something? (3)
+				BPL nb_get			; (3/2)
+			LDA nb_rcv			; check received (3)
+			STA (nb_ptr), Y		; store at destination (5 or 6)
+			STX nb_rcv			; preset value (3)
+			STX nb_flag			; clear bit 7 (3)
+			INY					; next (2)
+			BNE nbg_nw			; check MSB too (3/7)
 				INC nb_ptr+1
 nbg_nw:
-			CPY nb_fin			; check whether ended
-			BNE nb_get			; no, continue
+			CPY nb_fin			; check whether ended (3)
+			BNE nb_get			; no, continue (3/11/10)
 				LDA nb_ptr+1			; check MSB too
 				CMP nb_fin+1
 			BNE nb_get			; no, continue
