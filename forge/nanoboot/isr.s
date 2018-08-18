c71ec8bccc4d3ebbf42c2954545a5955b242c5d2
Carlos Santisteban <zuiko21@gmail.com>
2018-08-18 10:22:25 +0200

    safer ISRs on nanoboot

--- forge/nanoboot/isr.s
+++ forge/nanoboot/isr.s
@@ -1,8 +1,14 @@
 ; nanoBoot ISR for 6502
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180816-1641
+; last modified 20180818-1021
 
 nb_irq:
 	LSR nb_rcv		; mimick IRQ level on least significant bit...
 	ASL nb_rcv		; ...without affecting other than state
+; not sure if while IRQ is held low, this will be retriggered upon return
+; although will affect A, it is safer to mask I into the saved status
+	PLA				; saved status...
+	ORA #%00000100	; ...now with I set
+	PHA				; restore all (A changed)
 	RTI			; total 16 clocks! if on zeropage
+; if I flag is to be set, takes 9 extra clocks (total 25)
--- forge/nanoboot/nmi.s
+++ forge/nanoboot/nmi.s
@@ -1,11 +1,14 @@
 ; nanoBoot NMI handler for 6502
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180816-1722
+; last modified 20180818-1016
 
 -nb_nmi:
 	SEC
 	ROL nb_rcv			; prepare for next bit, initially on (2+5)
 	CLI					; enable IRQ for a moment... (2)
-	SEI					; ...and disable it back! (2)
+;	SEI					; ...and disable it back! (2)
+; do not know if keeping IRQ low will retrigger the interrupt
+; perhaps ISR should alter saved status masking I, thus this SEI is not needed
+; will affect X, not sure if worth saving it
 	ROR nb_flag			; this will set bit 7 accordingly (5)
 	RTI					; (6) total 22 clocks + ISR
