da3c387abc05205494b464fb8af150a17c9c422c
Carlos Santisteban <zuiko21@gmail.com>
2018-08-18 16:46:49 +0200

    saving A in nanoBoot NMI

--- forge/nanoboot/nmi.s
+++ forge/nanoboot/nmi.s
@@ -1,15 +1,16 @@
 ; nanoBoot NMI handler for 6502
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180818-1037
+; last modified 20180818-1642
 
 nb_nmi:
 	SEC
 	ROL nb_rcv			; prepare for next bit, initially on (2+5)
+	PHA					; must save A before IRQ! (3)
 	CLI					; enable IRQ for a moment... (2)
-;	SEI					; ...and disable it back! (2)
+	SEI					; ...and disable it back! (2)
+	PLA					; restored (4)
 ; do not know if keeping IRQ low will retrigger the interrupt
-; perhaps ISR should alter saved status masking I, thus this SEI is not needed
-; will affect A, not worth saving it
+; perhaps ISR should alter saved status masking I, anyway this SEI is needed
+; will affect A, worth saving it here
 	ROR nb_flag			; this will set bit 7 accordingly (5)
-	RTI					; (6) total 22 clocks + ISR
-; with I-masking ISR, two clocks less, for a grand total of 20+7+25=52 clocks plus another 7 for NMI issuing
+	RTI					; (6) total 29 clocks + ISR
