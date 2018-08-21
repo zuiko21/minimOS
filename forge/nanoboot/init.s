6a3f270035ace801a558a9daa0b8c0ebf43e2d80
Carlos Santisteban <zuiko21@gmail.com>
2018-08-21 22:21:13 +0200

    making more compact nanoboot

--- forge/nanoboot/init.s
+++ forge/nanoboot/init.s
@@ -1,6 +1,6 @@
 ; startup nanoBoot for 6502
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180818-1750
+; last modified 20180821-2158
 
 ; *** needed zeropage variables ***
 ; nb_ptr (word) for initial address, will use as pointer
@@ -11,40 +11,33 @@
 
 nb_init:
 	SEI					; make sure interrupts are off (2)
-	LDY #1				; initial value (2)
-	STY nb_rcv			; preset received value (3)
-	STY nb_flag			; reset reception flag (3)
-; *** set interrupt handlers *** (total 24)
-	LDX #<nb_irq
-	LDA #>nb_irq
-	STX fw_isr
-	STA fw_isr+1
-	LDX #<nb_nmi
-	LDA #>nb_nmi
-	STX fw_nmi
-	STA fw_nmi+1
+	LDX #1				; initial value (2)
+	STX nb_rcv			; preset received value (3)
+	STX nb_flag			; reset reception flag (3)
+; *** set interrupt handlers ***
+; a loop plus table is 15b 53t, but needs consecutive fw vars
+; old code was 20b 24t
+	LDY #3				; copy bytes 0...3 (2)
+nb_svec:
+		LDA nv_tab, Y			; get origin from table (4)
+		STA fw_isr, Y			; and write for FW (4)
+		DEY					; next (2+3)
+		BPL nb_svec
 ; *** wait for a valid nanoBoot link *** $4B, [start], [end]
-nb_wait:
-		BIT nb_flag			; received something? (3)
-		BPL nb_wait			; (3/2)
-	LDA nb_rcv			; check received (3)
+; the wait loop, get rcv and reset flags are 30b
+; a subroutine can be 20b plus 2b saving, at some performance expense
+	JSR nb_grc			; wait for reception and reset flags ()
 	CMP #$4B			; valid nanoBoot link? (2)
 	BNE nb_exit			; no, abort (2...)
 ; *** get nanoBoot header ***
-		LDX #0				; counter for bytes of header (2)
-		STY nb_rcv			; preset value (3)
-		STY nb_flag			; reset reception flag (3)
+		LDY #0				; counter for bytes of header (2) X for savvyness if needed
 nb_loop:
-				BIT nb_flag			; received something? (3)
-				BPL nb_loop			; (3/2)
-			LDA nb_rcv			; check received (3)
-			STA nb_ptr, X			; store in variable (4)
-			STY nb_rcv			; reset value (3)
-			STY nb_flag			; clear bit 7 (3)
-			INX
-			CPX #4				; all of header done? (2+2)
+			JSR nb_grc			; wait for reception and reset flags ()
+			STA nb_ptr, Y			; store in variable (4)
+			INY
+			CPY #4				; all of header done? (2+2)
 			BNE nb_loop			; (3/2)
-; prepare variables for transfer
+; prepare variables for transfer ************ continue here
 		LDX #1				; resetting value (2)
 		LDY nb_ptr			; make a copy of initial address (3+3)
 		LDA nb_ptr+1
