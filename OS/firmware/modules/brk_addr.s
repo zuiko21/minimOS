59bd35fe113e2ccbffcce6719c127216c89bff12
Carlos Santisteban <zuiko21@gmail.com>
2018-09-05 17:31:09 +0200

    safer BRK presetting

--- OS/firmware/modules/brk_addr.s
+++ OS/firmware/modules/brk_addr.s
@@ -1,6 +1,6 @@
 ; firmware module for minimOS·65
 ; (c) 2017-2018 Carlos J. Santisteban
-; last modified 20180131-1419
+; last modified 20180905-1728
 
 ; *** preset default BRK handler *** temporarily set as default NMI
 ; expects std_nmi label, possibly from ROM file
@@ -10,3 +10,7 @@
 	LDY #<std_nmi
 	STY fw_dbg			; store default handler (4+4)
 	STA fw_dbg+1
+#ifndef	SAFE
+	STY fw_nmi			; store default handler (4+4)
+	STA fw_nmi+1
+#endif
--- OS/firmware/modules/brk_addr16.s
+++ OS/firmware/modules/brk_addr16.s
@@ -1,9 +1,12 @@
 ; firmware module for minimOS·16
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20180511-1050
+; last modified 20180905-1729
 
 ; *** preset default BRK handler *** temporarily set as default NMI
 ; expects std_nmi label, possibly from ROM file
 ; 65816 only, MUST enter in 16-bit memory!
-	LDA #std_nmi		; default like the standard NMI (2+2)
-	STA fw_dbg			; store default handler (4+4)
+	LDA #std_nmi		; default like the standard NMI (3)
+	STA fw_dbg			; store default handler (5)
+#ifndef	SAFE
+	STA fw_nmi			; store default handler (5)
+#endif
