e41a4cda46042a0515201dbf32afb1bd022fea8d
Carlos Santisteban <zuiko21@gmail.com>
2018-09-12 16:35:15 +0200

    added optional Neumann amend to RIAA

--- other/audio/riaa.c
+++ other/audio/riaa.c
@@ -43,27 +43,28 @@ double db(double g) {
 }
 
 double para(double r1, double r2) {
-	return r1*r2/(r1+r2);
+	return r1*r2/(r1+r2);			// two resistor in parallel (or capacitors in series)
 }
 
+/* simulte tolerances with random factors */
 double alea(void) {
-	return 0.5;//rand()*1.0/RAND_MAX;
+	return 0.5;//rand()*1.0/RAND_MAX;	// return 0.5 to disable randomness
 }
 
 double	c2(void) {
-	return 0.9+0.1*(alea()+alea());
+	return 0.9+0.1*(alea()+alea());		// 2 parallel 10% capacitors
 }
 
 double	c1(void) {
-	return 0.9+0.2*alea();
+	return 0.9+0.2*alea();			// single 10% capacitor
 }
 
 double	r2(void) {
-	return 0.95+0.05*(alea()+alea());
+	return 0.95+0.05*(alea()+alea());	// 2 parallel 5% resistors
 }
 
 double	r1(void) {
-	return 0.95+0.1*alea();
+	return 0.95+0.1*alea();			// single 5% resistor
 }
 
 /**********************/
@@ -100,12 +101,23 @@ double stage(double f, double cb, double rba, double rbd, double cl, double rla,
 	return polar(re, im);
 }
 
-double lowpass(double f, double c, double r) {
-	double re, im, zc;
+/* low-pass filter with optional Neumann high frquency rollover */
+double lowpass(double f, double c, double r, double ch) {
+	double re, im, zc, zi, zr;
 
+	if (ch>0) {
+		zi= 1/(pp*f*ch);
+		re= mulr(r, 0, 0, zi);
+		im= muli(r, 0, 0, zi);
+		zr= divr(re, im, r, zi);
+		zi= divi(re, im, r, zi);
+	} else {
+		zr= r;
+		zi= 0;
+	}
 	zc= 1/(pp*f*c);
-	re= divr(0, zc, r, zc);
-	im= divi(0, zc, r, zc);
+	re= divr(0, zc, zr, zc+zi);
+	im= divi(0, zc, zr, zc+zi);
 
 	return polar(re, im);
 }
@@ -126,13 +138,13 @@ double hipass(double f, double c, double r) {
 
 int main(void) {
 /* constants */
-	int freqs= 18;		// same as size of arrays below!!!
-	double hz[18]= {0.1, 1, 4, 12, 20, 31, 49, 79, 150, 270, 480,
-			1000, 1100, 2100, 3800, 7600, 12000, 21000};	// test frequencies
+	int freqs= 21;		// same as size of arrays below!!!
+	double hz[21]= {0.1, 1, 4, 12, 20, 31, 49, 79, 150, 270, 480,
+			1000, 1100, 2100, 3800, 7600, 12000, 21000, 31600, 50000, 100000};	// test frequencies
 /* if using ...20, 31, 49, 79, 150, 270, 480, 1k, 1k1, 2k1, 3k8, 7k6, 12k, 21k,
 	these are the IEC values for convenience */
-	double iec[18]= {0,0,0,0, 16.35, 17.09, 16.45, 14.4, 10.28, 6.23, 2.92,
-			0, -0.23, -2.73, -6.17, -11.39, -15.17, -19.95};
+	double iec[21]= {0,0,0,0, 16.35, 17.09, 16.45, 14.4, 10.28, 6.23, 2.92,
+			0, -0.23, -2.73, -6.17, -11.39, -15.17, -19.95, 0,0,0};
 
 	srand(time(NULL));			// prepare random numbers!
 /********************/
@@ -144,6 +156,8 @@ int main(void) {
 /* stage two and low pass filter */
 	double s3c=100e-6*c1(), s3a=150*r1(), s3d=560*r1(), s3l=1500*r1();
 	double lpr=para(68e3,68e3)*r2(), lpc=2.2e-9*c2();
+/* optional Neumann rollover on lowpass filter */
+	double nc= 0;//68e-12;			// 94pF for lpr=34k (50 kHz)
 /* input/output coupling */
 	double cin= 220e-9*c1(), zin= para(150e3, 330e3)*r2();		// effect of 220n input capacitor (was 68n)
 	double cout= 470e-9, zout= 47e3;	// effect of 470n output capacitor
@@ -172,14 +186,18 @@ int main(void) {
 		gain*= stage(hz[fr], s3c, s3a, s3d, 0, 0, s3l); // missing AC load
 
 /* RIAA low-pass filter */
-		gain*= lowpass(hz[fr], lpc, lpr);		// apply passive low-pass
+		gain*= lowpass(hz[fr], lpc, lpr, nc);		// apply passive low-pass
 
 /* apply output coupling effect */
 		gain*= hipass(hz[fr], cout, zout);
 
-		eq= db(gain)-38.8345;				// EQ dB for convenience
+		eq= db(gain)-38.8344;				// EQ dB for convenience
 /*** print results! ***/
-		printf("%f\t%f\t%f\t%f\n", hz[fr], gain, eq, eq-iec[fr]);
+		printf("%f\t%f\t%f", hz[fr], gain, eq);
+		if (iec[fr]!=0) {
+			printf("\t%f", eq-iec[fr]);
+		}
+		printf("\n");
 	}
 
 	return 0;
