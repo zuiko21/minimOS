885dd8cbaf46461dc518f669e15d4f1f9d9e3fdc
Carlos Santisteban <zuiko21@gmail.com>
2018-11-22 23:41:00 +0100

    added DYNKERN option

--- OS/firmware/modules/start.s
+++ OS/firmware/modules/start.s
@@ -1,14 +1,16 @@
 ; firmware module for minimOS·65
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20181121-1750
+; last modified 20181122-2339
 
 ; *** start a 65(C)02-based kernel ***
 ; no interface needed, uses fw_warm var
 ; * new, ready for dynamic kernels! *
 ; NMOS savvy
 
+#ifdef	DYNKERN
 	LDY #<sysvars		; note beginning of kernel space
 	LDA #>sysvars
 	STY sysptr		; store as obscure parameter, just in case
 	STA sysptr+1
+#endif
 	JMP (fw_warm)		; (6)
--- OS/firmware/modules/start16.s
+++ OS/firmware/modules/start16.s
@@ -1,14 +1,16 @@
 ; firmware module for minimOS·16
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20181121-1752
+; last modified 20181122-2340
 
 ; *** start a 65816-based kernel ***
 ; no interface needed, uses fw_warm var
 ; * new, ready for dynamic kernels! *
 
+#ifdef	DYNKERN
 	.al: REP #$20		; save a couple bytes by going 16-bit again
 	LDA #sysvars		; beginning of kernel space
 	STA sysptr		; obscure parameter, just in case
+#endif
 	SEC					; emulation mode for a moment (2+2)
 	XCE
 	.as
