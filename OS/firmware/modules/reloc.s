305cce3597f339249b3d59a0302f0093365c5743
Carlos Santisteban <zuiko21@gmail.com>
2018-12-10 11:11:23 +0100

    working on relocation module

--- OS/firmware/modules/reloc.s
+++ OS/firmware/modules/reloc.s
@@ -1,14 +1,47 @@
 ; firmware module for minimOS·65
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20181210-1042
+; last modified 20181210-1047
 
-; *** relocate a 65(C)02-based kernel ***
+; *** relocate 65(C)02-based code and data ***
 ; interface TBD
-; * new, ready for dynamic kernels! *
 
-#ifdef	DYNKERN
+#ifdef	LOWRAM
+.(
+; TODO TODO TODO TODO TODO
 	LDY #<sysvars		; note beginning of kernel space
 	LDA #>sysvars
 	STY 			; store as parameter
 	STA +1
+; *** this section may be used for code relocation too ***
+        LDY #D_DYN         ; get offset to relocation table
+        LDA (da_ptr), Y
+        CLC
+        ADC da_ptr         ; get absolute pointer
+        STA dyntab         ; use as local pointer
+        LDY #0             ; reset counter
+; all set, let us convert the variable references
+dyd_rel:
+            LDA (dyntab), Y    ; any more to convert?
+                BEQ dd_end         ; no, all done
+            CLC
+            ADC da_ptr         ; yes, compute actual location of address
+            STA tmptr          ; store temporary pointer
+            LDA (tmptr)        ; this is the generic address to be converted
+; generic data addresses may start at $4000 (up to 16K), while code relocation...
+; ...may just start from zero, as skipping the header will provide addresses over $100
+; in any case, 65xx jumps have no zeropage addressing anyway. 68xx may need...
+; ...to make sure early jumps (or references) are assembled as full 16-bit.
+; data relocation could start from $8000 as well, but $4000 gives it a chance to work...
+; ...on unaware 32K RAM systems!
+            EOR #$4000         ; *** assume generic addresses start @ $4000 and no more than 16k is used ***
+            CLC
+            ADC dynmem         ; the location of this driver's variables
+            STA (tmptr)        ; address is corrected!
+            INY                ; go for next offset (assume 16-bit indexes)
+            INY
+            BRA dyd_rel
+dd_end:
+.)
+#else
+	_DR_ERR(UNAVAIL)			; relocation not implemented
 #endif
--- OS/firmware/modules/reloc16.s
+++ OS/firmware/modules/reloc16.s
@@ -1,13 +1,51 @@
 ; firmware module for minimOS·16
 ; (c) 2018 Carlos J. Santisteban
-; last modified 20181210-1042
+; last modified 20181210-1111
 
-; *** start a 65816-based kernel ***
+; *** relocate 65816-based code and data ***
 ; interface TBD
-; * new, ready for dynamic kernels! *
+; ex_pt <- code start_kernel
 
-#ifdef	DYNKERN
+
+#ifdef	LOWRAM
+.(
 	.al: REP #$20		; save a couple bytes by going 16-bit again
 	LDA #sysvars		; beginning of kernel space
 	STA 		; set parameter
+; must check THREE relocation tables: code (all), data (usually kernels & drivers only) and banks (for 65816)
+; do NOT check headers here!!! use ZP parameters
+	LDA ex_pt			; get code location...
+	STA dynmem			; is the first 
+
+	SEC
+	SBC #256			; ...minus one page...
+	STA rtab_pt			; ...and set as local header pointer
+	LDY #REL_TAB		; offset for the first relocation table in headers
+rl_tab:
+		LDA (rtab_pt), Y	; get this table offset
+		CLC
+		ADC ex_pt			; get initial address
+		STA dyntab			; use as local pointer
+		PHY					; save for next table!
+; relocate all references from current table
+		LDY #0				; reset counter
+; all set, let us convert the references
+dyd_rel:
+			LDA (dyntab), Y		; any more to convert?
+				BEQ dd_end			; no, all done
+			CLC
+			ADC ex_pt			; yes, compute actual location of address
+			STA tmptr			; store temporary pointer
+			LDA (tmptr)			; this is the generic address to be converted
+			EOR d_mask			; will be $4000 for data and 0 otherwise!
+			CLC
+			ADC dynmem			; the destination of these references
+			STA (tmptr)			; address is corrected!
+			INY					; go for next offset (assume 16-bit indexes)
+			INY
+			BRA dyd_rel
+dd_end:
+.)
+#else
+	_DR_ERR(UNAVAIL)			; relocation not implemented
 #endif
