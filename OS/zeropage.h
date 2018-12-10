9516e1b3d23a197f859241c6cccef21809d9a05b
Carlos Santisteban <zuiko21@gmail.com>
2018-12-10 12:24:28 +0100

    revamping ZP for relocation

--- OS/zeropage.h
+++ OS/zeropage.h
@@ -1,6 +1,6 @@
 ; minimOS 0.6rc8 zero-page system variables
 ; (c) 2012-2018 Carlos J. Santisteban
-; last modified 20181210-1230
+; last modified 20181210-1324
 
 .zero
 * = 0
@@ -38,7 +38,7 @@ locals:								; old label for compatibility
 
 ; *** include aliases here for local1/locpt1 ***
 dr_aut: ma_ix: iol_dev:				; 8 bit (mm_sig seems of no use!)
-tmp_ktab:							; 16 bit (v_src seems of no use!)
+tmp_ktab: rl_mask:					; 16 bit (v_src seems of no use!)
 
 local1: locpt1	.dsb	4			; variables for kernel functions @ $E4
 
@@ -49,6 +49,7 @@ dq_ptr	= dr_aut+2					; 16b
 ; *** include aliases here for local2/locpt2 ***
 ma_lim: cio_of:						; 8 bit
 pfa_ptr: un_ptch: exec_p:			; 16 bit (is v_dest of any use?)
+bn_tab:								; 16/24 bit ***parameter***
 
 local2: locpt2	.dsb	4			; variables for kernel functions @ $E8
 
@@ -59,8 +60,9 @@ dr_id	= rl_dev					; 8b
 
 ; *** include aliases here for local3/locpt3 ***
 rl_cur: exe_sp:						; 8 bit
-dte_ptr:							; 16 bit
-rh_scan:							; 16/24 bit
+dte_ptr: tmptr:						; 16 bit
+rh_scan: 							; 16/24 bit
+sv_tab:								; 16/24 bit ***parameter***
 ; ex_wr no longer used
 
 local3: locpt3	.dsb	4			; variables for kernel functions @ $EC
@@ -74,7 +76,7 @@ local3: locpt3	.dsb	4			; variables for kernel functions @ $EC
 b_sig:								; 8 bit
 bl_ptr:								; 16/24 bit ptr
 ex_pt:								; 16/24 bit ptr
-ex_rlc:								; 16/24 bit ptr, base address for relocation
+ex_tab:								; 16/24 bit (pointer to code relocation table)
 
 z10:z10W:z10L:						; old labels for compatibility
 zpar3: zaddr3	.dsb	4			; up to 4 bytes, including older names @ $F0
@@ -82,32 +84,32 @@ zpar3: zaddr3	.dsb	4			; up to 4 bytes, including older names @ $F0
 k_ram	= b_sig+1					; 8b, Kernel RAM pages (0 = 128 byte system) changed for virtua6502 compatibility
 b_ram	= b_sig+3					; 8b, Banks of "high" memory (65816 only)
 ln_siz	= b_sig+3					; 8b, maximum READLN input! eeeeeeeeeeeek
-bnk_rl	= ex_rlc+2					; 8b, bank for relocation, part of the full address
 ; *********************************************
 
 ; *** include aliases here for zpar2/zaddr2 ***
 def_io: irq_hz: da_ptr: kerntab:	; 16 bit
 ma_pt: str_pt:						; 16/24 bit pointers
-sv_rlc:								; 16 bit pointer (sysvars relocation)
+ex_rlc:								; 16/24 bit ptr, base address for relocation
 
 z6:z6W:z6L:							; old labels for compatibility
 zpar2: zaddr2	.dsb	4			; up to 4 bytes, including older names @ $F4
 
 bl_siz	= da_ptr+2					; 16b *** was here
+bnk_rl	= ex_rlc+2					; 8b, bank for relocation, part of the full address
 ; *********************************************
 
 ; *** include aliases here for zpar/zaddr ***
 io_c:  cpu_ll:						; 8 bit
+sv_rlc:								; 16 bit pointer (sysvars relocation)
 ma_rs:								; 16/24 bit
 w_rect:	up_ticks:					; 32 bit
-rl_tab:								; 16/24 bit (block of pointers to relocation tables)
 
 z2:z2W:z2L:							; old labels for compatibility
-zpar: zaddr	.dsb	4				; up to 4 bytes, including older names @ $F8
+zpar: zaddr		.dsb	4			; up to 4 bytes, including older names @ $F8
 
 c_speed	= cpu_ll+1					; 16b ***might recheck alignment***
 up_sec	= up_ticks+1				; 24b, new source-compatible format
-ma_align	= ma_rs+3			; 8b, moved for virtua6502 compatibility
+ma_align	= ma_rs+3				; 8b, moved for virtua6502 compatibility
 ; *******************************************
 
 ; ***********************************************
