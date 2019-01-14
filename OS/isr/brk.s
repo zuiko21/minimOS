1e92b2e619c4a83c8c502375bf3d3bc83a9b747a
Carlos Santisteban <zuiko21@gmail.com>
2019-01-14 09:03:37 +0100

    fixed hex offset in BRK-8

--- OS/isr/brk.s
+++ OS/isr/brk.s
@@ -1,7 +1,7 @@
 ; minimOS BRK panic handler
-; v0.6b4
+; v0.6b5
 ; (c) 2016-2019 Carlos J. Santisteban
-; last modified 20190113-2057
+; last modified 20190114-0859
 
 #ifndef	HEADERS
 #include "../usual.h"
@@ -18,31 +18,31 @@ lda#'K':jsr$c0c2
 ; let us get the original return address
 ; *** think about a padding byte on any BRK call, would make life much simpler!
 	TSX				; current stack pointer
-	LDY $0111, X	; get MSB (note offset below)
-	LDA $0110, X	; get LSB+1
-	BNE brk_nw		; will not wrap upon decrement!
-		DEY				; otherwise correct MSB
+	LDY $010B, X		; get MSB (note offset below)
+	LDA $010A, X		; get LSB+1
+	BNE brk_nw			; will not wrap upon decrement!
+		DEY					; otherwise correct MSB
 brk_nw:
 	_DEC				; back to signature address
 ; Y/A points to beginning of string
-	STY sysptr+1	; prepare internal pointer, should it be saved for reentrancy?
+	STY sysptr+1		; prepare internal pointer, should it be saved for reentrancy?
 	STA sysptr
-	LDY #0			; eeeeeeeeeeeeeeeeeek
+	LDY #0				; eeeeeeeeeeeeeeeeeek
 brk_ploop:
-		_PHY			; save cursor
-		LDA (sysptr), Y	; get current char
-			BEQ brk_term	; if ended, finish printed line
-		JSR brk_out		; send out character! saves 6 bytes
-		_PLY			; restore counter
-		INY				; next in string
-		BNE brk_ploop	; this version will not print over 256 chars!
+		_PHY				; save cursor
+		LDA (sysptr), Y		; get current char
+			BEQ brk_term		; if ended, finish printed line
+		JSR brk_out			; send out character! saves 6 bytes
+		_PLY				; restore counter
+		INY					; next in string
+		BNE brk_ploop		; this version will not print over 256 chars!
 #ifdef	SAFE
-		BEQ brk_end		; string too long, should never arrive
+		BEQ brk_end			; string too long, should never arrive
 #endif
 brk_term:
-	PLA				; discard saved counter
+	PLA					; discard saved counter
 brk_end:
-	JSR brk_cr		; another newline
+	JSR brk_cr			; another newline
 ; we are done, should call debugger if desired, otherwise we will just lock
 lda#10:jsr$c0c2
 lda#'D':jsr$c0c2
@@ -50,18 +50,18 @@ lda#'i':jsr$c0c2
 lda#'e':jsr$c0c2
 lda#'!':jsr$c0c2
 
-	JMP lock		; let the system DIE
+	JMP lock			; let the system DIE
 ; if needed to return after BRK, skip panic message on stacked PC
-;	SEC				; Y is in A, get ready for addition, skipping NUL!
-;	ADC sysptr		; LSB...
-;	TAY				; ...is ready in Y
+;	SEC					; Y is in A, get ready for addition, skipping NUL!
+;	ADC sysptr			; LSB...
+;	TAY					; ...is ready in Y
 ;	LDA sysptr+1		; get MSB
-;	ADC #0			; fix MSB if needed, now in A
-;	TSX				; current stack pointer
+;	ADC #0				; fix MSB if needed, now in A
+;	TSX					; current stack pointer
 ;	STA $0110, X		; set LSB+1
-;	TYA				; as no STY abs,X...
+;	TYA					; as no STY abs,X...
 ;	STA $0111, X		; ...set MSB
-;	RTS				; *** otherwise let it finish the ISR
+;	RTS					; *** otherwise let it finish the ISR
 
 ; send a newline to default device
 brk_cr:
@@ -69,7 +69,25 @@ brk_cr:
 brk_out:
 jsr$c0c2
 rts
-;	LDY #0			; default
-;	STA io_c		; kernel parameter
-;	KERNEL(COUT)	; system call
+;	LDY #0				; default
+;	STA io_c			; kernel parameter
+;	KERNEL(COUT)		; system call
 ;	RTS
+
+debug_hex:
+pha
+lda#10:jsr$c0c2
+lda#'$':jsr$c0c2
+pla:pha
+lsr:lsr:lsr:lsr
+jsr dhx_prn
+pla
+and#$f
+dhx_prn:
+cmp#10
+bcc dhx_num
+adc#6
+dhx_num:
+clc:adc#'0'
+jsr$c0c2
+rts
