9d8af1e1a49c721a997c97b742c515b96ad70fa8
Carlos Santisteban <zuiko21@gmail.com>
2019-01-28 09:33:42 +0100

    fixed miniMoDA .s for NMOS

--- apps/pmap.s
+++ apps/pmap.s
@@ -162,7 +162,7 @@ pmap_end:
 
 
 ; *** useful routines ***
-
+	NOP			; *** *** *** temporary patch to avoid calling $c0bf!!!
 ; ** these will go into a pseudolibrary **
 ; * print binary in A as two hex ciphers *
 hex2char:
