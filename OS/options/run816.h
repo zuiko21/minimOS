5af162b9f6adae0b991c36a480f0d7afd4041e8b
Carlos Santisteban <zuiko21@gmail.com>
2019-01-30 13:16:45 +0100

    restored run816.h options!

--- OS/options/run816.h
+++ OS/options/run816.h
@@ -1,62 +1,51 @@
 ; default options for minimOS and other modules
 ; suitable for run816 BBC simulator
-; for 8-bit kernel testing!
 ; copy or link as options.h in root dir
 ; (c) 2017-2019 Carlos J. Santisteban
-; last modified 20190128-0956
+; last modified 20180901-2158
 
 ; *** set conditional assembly ***
 
 ; comment for optimized code without optional checks
-;#define		SAFE	_SAFE
+#define		SAFE	_SAFE
 
-#define		LOWRAM	_LOWRAM
-
-; *** these optimisations need the CPP preprocessor! ***
-;#define		FAST_API	_FAST_API
-;#define		FAST_FW		_FAST_FW
-
-; new option for mutable IDs, most likely mandatory!
-;#define		MUTABLE		_MUTABLE
-
-; enable in order to remove headers, not LOADLINK savvy!
+; enable in order to remove headers, not LOAD_LINK savvy!
 ;#define		NOHEAD	_NOHEAD
+; enable to allow 8-bit apps!
+#define		SUPPORT		_SUPPORT
+#define		MUTABLE		_MUTABLE
 ; enable multitasking support... hopefully! *** might be defined upon multitasking driver
 ;#define	MULTITASK	_MULTITASK
 
 ; *** machine specific info ***
 ; select type as on executable headers, B=generic 65C02, V=C816, N=NMOS 6502, R=Rockwell 65C02
-;#define		NMOS	_NMOS
-#ifdef	NMOS
-#define		CPU_TYPE	'N'
-#else
-#define		CPU_TYPE	'B'
-#endif
+#define		C816	_C816
+#define		CPU_TYPE	'V'
 
 ; *** machine hardware definitions ***
 ; Machine-specific ID strings, new 20150122, renamed 20150128, 20160120, 20160308
 
-#define		MACHINE_NAME	"BBC simulator '02"
-#define		MACHINE_ID		"run6502"
+#define		MACHINE_NAME	"BBC simulator"
+#define		MACHINE_ID		"run65816"
 
 ; Firmware selection, new 20160310, will pick up suitable template from firmware/
-#define		ARCH_s			"firmware/run02.s"
-#define		ARCH_h			"firmware/run02.h"
+#define		ARCH_h			"firmware/run816.h"
+#define		ARCH_s			"firmware/run816.s"
 
 ; Suitable driver package (add .h or .s as needed) in drivers/config/ folder, new 20160308
 ; may suit different configurations on a machine
-#define		DRIVER_PACK_s		"drivers/config/run02_std.s"
-#define		DRIVER_PACK_h		"drivers/config/run02_std.h"
+#define		DRIVER_PACK_h		"drivers/config/run816_std.h"
+#define		DRIVER_PACK_s		"drivers/config/run816_std.s"
 
 ; *** Default files ***
 ; default shell from folder
 #define		SHELL		"shell/minishell.s"
-; default firmware NMI
-#define		STD_NMI		"../forge/nanomon.s"
+; defaukt firmware NMI
+#define		STD_NMI		"../forge/nanomon16.s"
 ; default NMI, BRK etc TBD ***********
 
 ; ** start of ROM **
-ROM_BASE	=	$8000	; enough for package WITH EhBASIC
+ROM_BASE	=	$8000	; enough for package
 
 ; ** position of firmware, usually skipping I/O area **
 FW_BASE		=	$F800	; simple firmware expected on BBC simulator
@@ -65,7 +54,7 @@ FW_BASE		=	$F800	; simple firmware expected on BBC simulator
 ; ** I/O definitions **
 
 ; I/O base address, usually one page, new 20160308
-IO_BASE		=	$C000	; as per EhBASIC I/O
+IO_BASE		=	$C000	; ???????as per EhBASIC I/O
 
 ; missing hardware declarations...
 ; * VIA 65(C)22 Base address, machine dependent *
@@ -96,17 +85,13 @@ DEVICE	=	DEV_CNIO		; standard I/O device
 SRAM		=	128		; 32 KiB available as standard
 SPTR		=	$FF		; general case stack pointer, new name 20160308
 SYSRAM		=	$0200	; generic case system RAM after zeropage and stack, most systems with at least 1 kiB RAM
-#ifndef	C64
 ZP_AVAIL	=	$E1		; as long as locals start at $E4, not counting used_zp
-#else
-ZP_AVAIL	=	$DF		; two bytes less for the 6510
-#endif
 #else
 ; rare lowram version for testing purposes
 SRAM		=	0
 SPTR		=	$63		; (previously $75) MTE and other 128-byte RAM systems!
-SYSRAM		=	$20		; for 128-byte systems, reduced value 20150210 (should be $28)
-ZP_AVAIL	=	SYSRAM-3
+SYSRAM		=	$28		; for 128-byte systems, reduced value 20150210
+ZP_AVAIL	=	$25
 #endif
 
 ; *** speed definitions ***
