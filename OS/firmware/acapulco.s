a9ff6bd6700c10540b3ab7e4cd75355867db80de
Carlos Santisteban <zuiko21@gmail.com>
2019-02-03 14:18:28 +0100

    stub for Acapulco FW

---
+++ OS/firmware/acapulco.s
@@ -0,0 +1,387 @@
+; firmware for minimOS on Acapulco
+; v0.9.6b1
+; (c) 2019 Carlos J. Santisteban
+; last modified 20190203-1402
+
+#define		FIRMWARE 	_FIRMWARE
+
+#include "../usual.h"
+
+; already set at FW_BASE via rom.s
+
+.(
+#ifndef	NOHEAD
+; *** first some ROM identification *** new 20150612
+; this is expected to be loaded at an aligned address anyway
+fw_start:
+	.asc	0, "m", CPU_TYPE	; standard system file wrapper, new format 20161010, experimental type
+	.asc	"****", CR			; flags TBD
+	.asc	"boot", 0			; standard filename
+fw_splash:
+	.asc	"0.9.6 firmware for "	; machine description as comment
+fw_mname:
+	.asc	MACHINE_NAME, 0
+; advance to end of header
+	.dsb	fw_start + $F8 - *, $FF	; for ready-to-blow ROM, advance to time/date field
+
+; *** date & time in MS-DOS format at byte 248 ($F8) ***
+	.word	$4DA0				; time, 09.45
+	.word	$4C45				; date, 2018/2/5
+
+fwSize	=	fw_end - fw_start - 256	; compute size NOT including header!
+
+; filesize in top 32 bits NOT including header, new 20161216
+	.word	fwSize				; filesize
+	.word	0					; 64K space does not use upper 16-bit
+; *** end of standard header ***
+#else
+; if no headers, put identifying strings somewhere
+fw_splash:
+	.asc	"0.9.6 FW @ "
+fw_mname:
+	.asc	MACHINE_NAME, 0		; store the name at least
+#endif
+
+; *********************************
+; *********************************
+; *** administrative jump table *** changing
+; *********************************
+; *********************************
+fw_admin:
+; generic functions, esp. interrupt related
+	.word	gestalt		; GESTALT get system info (renumbered)
+	.word	set_isr		; SET_ISR set IRQ vector
+	.word	set_nmi		; SET_NMI set (magic preceded) NMI routine
+	.word	set_dbg		; SET_DBG set debugger, new 20170517
+	.word	jiffy		; JIFFY set jiffy IRQ speed
+	.word	irq_src		; IRQ_SOURCE get interrupt source in X for total ISR independence
+
+; pretty hardware specific
+	.word	poweroff	; POWEROFF power-off, suspend or cold boot
+	.word	freq_gen	; *** FREQ_GEN frequency generator hardware interface, TBD
+
+	.word	install		; INSTALL copy jump table
+	.word	patch		; PATCH patch single function (renumbered)
+	.word	conio		; *** CONIO basic console TO DO
+
+
+; ********************
+; ********************
+; *** cold restart ***
+; ********************
+; ********************
+
+; *** basic init *** unlikely to use a 65816
+reset:
+#include "modules/basic_init02.s"
+
+; ******************************
+; *** minimal hardware setup ***
+; ******************************
+
+; check for VIA presence and disable all interrupts
+#include "modules/viacheck_irq.s"
+
+; *********************************
+; *** optional firmware modules ***
+; *********************************
+
+; optional boot selector
+;#include "modules/bootoff.s"
+; try to select video mode *** TO DO ***
+; should initialise built-in video too *** TO DO ***
+
+; might check ROM integrity here
+;#include "modules/romcheck.s"
+; Acapulco has no ROM-in-RAM feature!
+
+; specific startup beep
+#include "modules/beep_pwm.s"
+
+; SRAM test, note that may affect VRAM
+#include "modules/ramtest.s"
+
+; ********************************
+; *** hardware interrupt setup ***
+; ********************************
+
+; VIA initialisation (and stop beeping)
+#include "modules/via_init.s"
+
+; ***********************************
+; *** firmware parameter settings ***
+; ***********************************
+
+; *** set default CPU type ***
+; just set expected default type as defined in options.h...
+;#include "modules/default_cpu.s"
+; ...or actually check for it!
+#include "modules/cpu_check.s"
+; do NOT include both files at once!
+
+; in case an NMOS CPU is used, make sure this was built for it
+#include "modules/nmos_savvy.s"
+
+; *** continue parameter setting ***
+; preset kernel start address
+#include "modules/kern_addr.s"
+
+; preset default BRK handler, might set a proper PANIC handler
+#include "modules/brk_addr.s"
+
+; no need to set NMI as it will be validated
+
+
+; preset jiffy irq frequency
+#include "modules/jiffy_hz.s"
+
+; reset jiffy count
+#include "modules/jiffy_rst.s"
+
+; reset last installed kernel (new)
+#include "modules/rst_lastk.s"
+
+; *** direct print splash string code comes here, include rainbow logo ***
+#include "modules/splash_acapulco.s"
+
+; *** optional network booting ***
+; might modify the contents of fw_warm
+;#include "modules/netboot.s"
+
+; ************************
+; *** start the kernel ***
+; ************************
+start_kernel:
+#include "modules/start.s"
+
+
+; ********************************
+; ********************************
+; ****** interrupt handlers ******
+; ********************************
+; ********************************
+
+; **********************************************
+; *** vectored NMI handler with magic number *** eeeeeeeeeeeeeek
+; **********************************************
+nmi:
+#include "modules/nmi_hndl.s"
+
+; ****************************
+; *** vectored IRQ handler ***
+; ****************************
+; nice to be here, but might go elsewhere in order to save space, like between FW interface calls
+irq:
+#include "modules/irq_hndl.s"
+
+; ***************************
+; *** minimOS BRK handler ***
+; ***************************
+brk_hndl:				; pointed from reserved vector
+#include "modules/brk_hndl.s"
+
+
+; ********************************
+; ********************************
+; *** administrative functions ***
+; ********************************
+; ********************************
+
+; *** generic functions ***
+
+; *********************************
+; GESTALT, get system info, API TBD
+; *********************************
+gestalt:
+#include "modules/gestalt.s"
+
+; ***********************
+; SET_ISR, set IRQ vector
+; ***********************
+set_isr:
+#include "modules/set_isr.s"
+
+; ********************************
+; SET_NMI, set NMI handler routine
+; ********************************
+set_nmi:
+#include "modules/set_nmi.s"
+
+; ********************************
+; SET_DBG, set BRK handler routine
+; ********************************
+set_dbg:
+#include "modules/set_dbg.s"
+
+; *** interrupt related ***
+
+; ***************************
+; JIFFY, set jiffy IRQ period
+; ***************************
+jiffy:
+#include "modules/jiffy.s"
+
+; ****************************************
+; IRQ_SRC, investigate source of interrupt
+; ****************************************
+; notice non-standard ABI, same module as 6502 version!
+irq_src:
+#include "modules/irq_src.s"
+
+; *** hardware specific ***
+
+; **********************
+; POWEROFF, shutdown etc *** TBD
+; **********************
+poweroff:
+#include "modules/poweroff.s"
+
+; ***********************************
+; FREQ_GEN, generate frequency at PB7 *** TBD
+; ***********************************
+freq_gen:
+;#include "modules/freq_gen16.s"
+	_DR_ERR(UNAVAIL)	; not yet implemented
+
+; *** other functions with RAM enough ***
+#ifndef		LOWRAM
+; **************************
+; INSTALL, supply jump table
+; **************************
+install:
+#include "modules/install.s"
+
+; ****************************
+; PATCH, patch single function
+; ****************************
+patch:
+#include "modules/patch.s"
+
+; **************************************
+; CONIO, basic built-in console ** TO DO
+; **************************************
+conio:
+	_DR_ERR(UNAVAIL)	; *** TO DO ***
+#endif
+
+
+; ***********************************
+; ***********************************
+; *** some firmware odds and ends ***
+; ***********************************
+; ***********************************
+
+; *** memory map, as used by gestalt, not sure what to do with it ***
+fw_map:					; TO BE DONE
+
+; *********************************************
+; *** experimental blinking of CapsLock LED ***
+; *********************************************
+led_lock:
+; make sure PB3 is output and device $Bx is selected, just in case
+; should toggle PB7 too with CB2 high, for a clicking sound****
+	LDA VIA+DDRB		; original direction
+	ORA #%11111001		; desired outputs
+	STA VIA+DDRB		; set on VIA
+; intial value selects $D8 device (LCD on keyboard, E down and LED on)
+	LDA #%11011000
+ll_tog:
+		STA VIA+IORB		; turn LED on, LCD will not disturb
+ll_loop:
+				INX			; inner loop (2)
+				NOP			; add some delay (2+2)
+				NOP
+				BNE ll_loop		; inner takes 2303t (3)
+			INY			; outer loop (2)
+			BNE ll_loop		; full loop is ~0.59s @ 1 MHz (3)
+		EOR #%00001000		; toggle PB3
+		_BRA ll_tog		; switch and continue forever
+
+; *** NMOS version needs large ADMIN call back here! ***
+#ifndef	FAST_FW
+#ifdef	NMOS
+nmos_adc:
+	_JMPX(fw_admin)		; takes a lot of clocks
+#endif
+#endif
+
+; ------------ only fixed addresses block remain ------------
+; filling for ready-to-blow ROM
+#ifdef		ROM
+	.dsb	kerncall-*, $FF
+#endif
+
+; ******************************************************************
+; ****** the following will come ALWAYS at standard addresses ****** last 64 bytes
+; ******************************************************************
+
+; *** minimOS function call primitive ($FFC0) ***
+* = kerncall
+	_JMPX(fw_table)	; macro for NMOS compatibility (6)
+
+; filling for ready-to-blow ROM
+#ifdef	ROM
+	.dsb	adm_appc-*, $FF	; eeeeeeeeeeeeeeeeeeeek
+#endif
+
+; *** administrative meta-kernel call primitive for apps ($FFD0) ***
+; not really needed on 6502 systems, but kept for the sake of binary compatibility
+; pretty much the same code at $FFD8, not worth more overhead
+* = adm_appc
+#ifndef	FAST_FW
+#ifndef	NMOS
+	_JMPX(fw_admin)		; takes 6 clocks with CMOS
+#else
+	JMP nmos_adc		; needed overhead as takes 10 bytes!
+#endif
+#endif
+
+; filling for ready-to-blow ROM
+#ifdef		ROM
+	.dsb	adm_call-*, $FF
+#endif
+
+; *** administrative meta-kernel call primitive ($FFD8) ***
+* = adm_call
+#ifndef	FAST_FW
+#ifndef	NMOS
+	_JMPX(fw_admin)		; takes 6 clocks with CMOS
+#else
+	JMP nmos_adc		; needed overhead as takes 10 bytes!
+#endif
+#endif
+
+; filling for ready-to-blow ROM
+#ifdef	ROM
+	.dsb	lock-*, $FF
+#endif
+
+; *** panic routine, locks at very obvious address ($FFE2-$FFE3) ***
+; alternatively, blink CapsLock LED!
+* = lock
+	SEI					; unified procedure
+; classic way
+	SEC
+panic_loop:
+	BCS panic_loop		; no problem if /SO is used
+; ** alternative way **
+;	JMP led_lock		; start CapsLock blinking!
+
+; filling for ready-to-blow ROM
+#ifdef	ROM
+	.dsb	$FFF6-*, $FF
+#endif
+
+; once again, CHIHUAHUA is very unlikely to use a 65816
+; but must store the BRK handler address!
+* = $FFF6
+	.word	brk_hndl		; new BRK	@ $FFF6
+	.word	nmi			; unsupported ABORT	@ $FFF8
+; *** 65(C)02 ROM vectors ***
+* = $FFFA				; just in case
+	.word	nmi			; NMI	@ $FFFA
+	.word	reset		; RST	@ $FFFC
+	.word	irq			; IRQ	@ $FFFE
+
+fw_end:					; for size computation
+.)
