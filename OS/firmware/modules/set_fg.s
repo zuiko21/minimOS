67e8ee417ebc489a669d652c7ac804c6673b67d9
Carlos Santisteban <zuiko21@gmail.com>
2019-02-06 08:51:31 +0100

    somewhat revised SET_FG modules

---
+++ OS/firmware/modules/set_fg.s
@@ -0,0 +1,52 @@
+; *** SET_FG, enable/disable frequency generator (Phi2/n) on VIA_FG *** revised 20150208...
+; (c) 2015-2019 Carlos J. Santisteban
+; last modified 20190206-0839
+
+; **************************************************
+; *** SET_FG, enable/disable frequency generator *** TO BE REVISED
+; **************************************************
+;		INPUT
+; zpar.w = dividing factor (times two?)
+;		OUTPUT
+; C = busy
+;
+; should be Phi2-rate independent... input as Hz, or 100uS steps?
+; zpar.W <- dividing factor (times two?), C -> busy
+; destroys A, X...
+
+; *******TO BE REVISED*********
+-set_fg:
+.(
+	LDA zpar			; check requested value
+	ORA zpar+1
+		BEQ fg_dis			; if zero, disable output
+	LDA VIA_FG+ACR		; get current configuration
+		BMI fg_busy			; already in use
+	LDX VIA_FG+T1LL		; get older T1 latch values
+	STX old_t1			; save them
+	LDX VIA_FG+T1LH
+	STX old_t1+1
+; *** TO_DO - should compare old and new values in order to adjust quantum size accordingly ***
+	LDX zpar			; get new division factor
+	STX VIA_FG+T1LL		; store it
+	LDX zpar+1
+	STX VIA_FG+T1LH
+	STX VIA_FG+T1CH		; get it running!
+	ORA #$C0			; enable free-run PB7 output
+	STA VIA_FG+ACR		; update config
+fg_none:
+	_DR_OK				; finish anyway
+fg_dis:
+	LDA VIA_FG+ACR		; get current configuration
+		BPL fg_none			; it wasn't playing!
+	AND #$7F			; disable PB7 only
+	STA VIA_FG+ACR		; update config
+	LDA old_t1			; older T1L_L
+	STA VIA_FG+T1LL		; restore old value
+	LDA old_t1+1
+	STA VIA_FG+T1LH		; it's supposed to be running already
+; *** TO_DO - restore standard quantum ***
+	_DR_OK				; all done
+fg_busy:
+	_DR_ERR(BUSY)		; couldn't set
+).
--- OS/firmware/modules/set_fg16.s
+++ OS/firmware/modules/set_fg16.s
@@ -1,18 +1,21 @@
+; *** SET_FG, enable/disable frequency generator (Phi2/n) on VIA_FG ***
+; (c) 2018-2019 Carlos J. Santisteban
+; last modified 20190206-0843
+
 ; **************************************************
 ; *** SET_FG, enable/disable frequency generator *** TO BE REVISED
 ; **************************************************
-; (c) 2018-2019 Carlos J. Santisteban
 ;		INPUT
 ; zpar.w = dividing factor (times two?)
 ;		OUTPUT
 ; C = busy
 ;
-; should use some firmware interface, just in case it doesn't affect jiffy-IRQ!
-; should also be Phi2-rate independent... input as Hz, or 100uS steps?
+; should be Phi2-rate independent... input as Hz, or 100uS steps?
 ; *** TO DO *** temporarily made 8-bit savvy
 ; no long addressing...
 
 set_fg:
+.(
 	.al: REP #$20		; *** 16-bit memory ***
 ; switch DBR as it accesses a lot of kernel data!
 	PHB					; eeeeeeeeek (3)
@@ -21,37 +24,38 @@ set_fg:
 ; proceed
 	LDA zpar			; take whole word
 		BEQ fg_dis			; if zero, disable output
-	LDX VIA+ACR			; get current configuration byte
+	LDX VIA_FG+ACR		; get current configuration byte
 		BMI fg_busy			; already in use
-	LDA VIA+T1LL		; get older T1 latch values
+	LDA VIA_FG+T1LL		; get older T1 latch values
 	STA old_t1			; save them
 ; *** TO_DO - should compare old and new values in order to adjust quantum size accordingly ***
 	LDA zpar			; get new division factor
-	STA VIA+T1LL		; store it
-	STA VIA+T1CH		; get it running!
+	STA VIA_FG+T1LL		; store it
+	STA VIA_FG+T1CH		; get it running!
 	.as: SEP #$20		; *** back to 8-bit memory ***
 	TXA					; process configuration
 	ORA #$C0			; enable free-run PB7 output
-	STA VIA+ACR			; update config
+	STA VIA_FG+ACR		; update config
 fg_none:
 	PLB					; restore!
-	_EXIT_OK			; finish anyway
+	_DR_OK				; finish anyway
 
 fg_dis:
 	.al:				; called from above
-	LDX VIA+ACR			; get current configuration
+	LDX VIA_FG+ACR		; get current configuration
 		BPL fg_none			; it wasn't playing!
 	TXA					; process configuration
 	AND #$007F			; disable PB7 only
 	TAX					; please respect 2nd byte!
-	STX VIA+ACR			; update config
+	STX VIA_FG+ACR		; update config
 	LDA old_t1			; older T1L
-	STA VIA+T1LL		; restore old value
+	STA VIA_FG+T1LL		; restore old value
 ; *** TO_DO - restore standard quantum ***
 	PLB					; restore!
-	_EXIT_OK
+	_DR_OK
 fg_busy:
-;	.al:
 	PLB					; restore!
-	_ERR(BUSY)			; couldn't set
+	_DR_ERR(BUSY)		; couldn't set
+.)
 
+.as:
