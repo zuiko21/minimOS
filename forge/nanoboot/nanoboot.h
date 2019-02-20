221f6d6d3187e36649db18a2550f43dea031be38
Carlos Santisteban <zuiko21@gmail.com>
2019-02-20 10:39:36 +0100

    fixed timeout variable for nanoboot

--- forge/nanoboot/nanoboot.h
+++ forge/nanoboot/nanoboot.h
@@ -1,11 +1,14 @@
-; zeropage variables for nanoBoot, v0.2a2
+; zeropage variables for nanoBoot, v0.2a3
 ; (c) 2018-2019 Carlos J. Santisteban
-; last modified 20190219-1422
+; last modified 20190220-1038
 
 .zero
 
 * = 2				; 6510-compatible (uz on minimOS)
 
+#ifdef	TIMEBOOT
+timeout	.word	0		; timeout counter
+#endif
 nb_rcv	.word	0		; received value
 nb_flag	.word	0		; a byte is ready if bit 7 is high (must be reset every byte)
 nb_ptr	.word	0		; initial address, will be used as pointer
@@ -13,7 +16,4 @@ nb_fin	.word	0		; final address (consecutive) after downloaded chunk
 nb_ex	.word	0		; copy of initial address
 ; will use three more bytes temporarily
 
-#ifdef	TIMEBOOT
-timeout	.word	0		; timeout counter
-#endif
 .text
