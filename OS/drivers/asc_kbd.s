2ac84e512850aa702868d291de04d94fe5858a52
Carlos Santisteban <zuiko21@gmail.com>
2019-03-04 13:39:44 +0100

    fixed CAPS LOCK LED on asc-kbd

--- OS/drivers/asc_kbd.s
+++ OS/drivers/asc_kbd.s
@@ -1,7 +1,7 @@
 ; 64-key ASCII keyboard for minimOS!
-; v0.6b4
+; v0.6b5
 ; (c) 2012-2019 Carlos J. Santisteban
-; last modified 20190212-0906
+; last modified 20190304-1336
 
 ; VIA bit functions
 ; PA0...3	= input from selected column
@@ -58,19 +58,19 @@
 ; ******************************
 
 ; *** begins with sub-function addresses table ***
-	.byt	145		; physical driver number D_ID (TBD)
+	.byt	145			; physical driver number D_ID (TBD)
 	.byt	A_BLIN|A_POLL	; input driver, periodic interrupt-driven
 	.word	ak_read		; read from input buffer
 	.word	ak_err		; no output
 	.word	ak_init		; initialise 'device', called by POST only
 	.word	ak_poll		; periodic interrupt...
-	.word	4		; 20ms scan seems fast enough
+	.word	4			; 20ms scan seems fast enough
 	.word	ak_nreq		; D_ASYN does nothing
 	.word	ak_err		; no config
 	.word	ak_err		; no status
 	.word	ak_exit		; shutdown procedure, leave VIA as it was...
 	.word	ak_info		; points to descriptor string
-	.word	0		; non-relocatable, D_MEM
+	.word	0			; non-relocatable, D_MEM
 
 ; *** driver description ***
 ak_info:
@@ -79,9 +79,9 @@ ak_info:
 ; *** some constant definitions ***
 AF_SIZ		= 16		; buffer size (only 15 useable) no need to be power of two
 AR_DEL		= 35		; 35×20 ms (0.7s) original delay
-AR_RATE		= 5		; 5×20 ms (1/10s) original repeat rate
+AR_RATE		= 5			; 5×20 ms (1/10s) original repeat rate
 PA_MASK		= %11110000	; PA0-3 as input, PA4-7 as output
-PB_KEEP		= %10000000	; keep PB7
+PB_KEEP		= %10001000	; keep PB7... and PB3 eeeeeeeeeeeek
 PB_MASK		= %00100101	; VIAport address
 
 ak_mk		= sysptr	; *** required zeropage pointer ***
@@ -90,16 +90,16 @@ ak_mk		= sysptr	; *** required zeropage pointer ***
 ; *** read from buffer to output block *** usual mandatory loop
 ; ****************************************
 ak_read:
-	LDA bl_ptr+1			; save pointer MSB
+	LDA bl_ptr+1		; save pointer MSB
 	PHA
 	LDY #0				; reset index
 ak_rloop:
 		LDA bl_siz			; check remaining
 		ORA bl_siz+1
-			BEQ blck_end			; nothing to do
+			BEQ blck_end		; nothing to do
 		JSR ak_get			; *** get one byte from buffer***
 			BCS blck_end		; any error ends transfer!
-		STA (bl_ptr),Y			; ...goes into buffer
+		STA (bl_ptr),Y		; ...goes into buffer
 		INY					; next byte, check carry
 		BNE ak_nw
 			INC bl_ptr+1
@@ -107,10 +107,10 @@ ak_nw:
 		DEC bl_siz			; one less to go
 		LDA bl_siz			; check whether wrapped
 		CMP #$FF
-		BNE blck_end			; no wrap, all done
-			LDA bl_siz+1			; any page remaining?
-		BEQ blck_end			; no, exit
-			DEC bl_siz+1			; ...or one page less
+		BNE blck_end		; no wrap, all done
+			LDA bl_siz+1		; any page remaining?
+		BEQ blck_end		; no, exit
+			DEC bl_siz+1		; ...or one page less
 		_BRA ak_rloop
 blck_end:
 	PLA					; restore MSB
@@ -118,7 +118,7 @@ blck_end:
 	RTS					; respect whatever error code
 
 ak_err:
-	_DR_ERR(NO_RSRC)		; cannot do this
+	_DR_ERR(NO_RSRC)	; cannot do this
 
 ; ************************
 ; *** initialise stuff ***
@@ -162,133 +162,133 @@ ak_poll:
 	LDA #PA_MASK		; prepare for this device
 	STA VIA_U+DDRA
 ; scan modifier column
-	LDX #15			; maximum column index (modifiers)
-	JSR ap_scol		; scan this column
-	_CRITIC			; will use zeropage interrupt space!
-	CMP ak_rmod		; any change on these?
-	BNE ap_eqm		; no, just scan the rest
-		STA ak_rmod		; update raw modifier combo...
-		LSR				; pressing caps lock?
-		BCC ap_selt		; no, just check other modifiers
+	LDX #15				; maximum column index (modifiers)
+	JSR ap_scol			; scan this column
+	_CRITIC				; will use zeropage interrupt space!
+	CMP ak_rmod			; any change on these?
+	BNE ap_eqm			; no, just scan the rest
+		STA ak_rmod			; update raw modifier combo...
+		LSR					; pressing caps lock?
+		BCC ap_selt			; no, just check other modifiers
 ; toggle caps lock status bit (A holds shifted modifiers)
-			LSR ak_cmod		; get older cpaps state
-			BCC ap_cup		; was off, turn it on...
-				CLC				; ...or turn off if was on
+			LSR ak_cmod			; get older cpaps state
+			BCC ap_cup			; was off, turn it on...
+				CLC					; ...or turn off if was on
 				BCC ap_cok
 ap_cup:
-			SEC				; this turns caps on
+			SEC					; this turns caps on
 ap_cok:
-			ROL				; reinsert new status together with other bits
-			STA ak_cmod		; update all bits
-			AND #1			; current caps lock status
-			TAY				; keep for later
+			ROL					; reinsert new status together with other bits
+			STA ak_cmod			; update all bits
+			AND #1				; current caps lock status
+			TAY					; keep for later
 ; and update status of caps lock LED
-			LDA VIA_U+IORB	; clear PB3, thus caps lock LED
+			LDA VIA_U+IORB		; clear PB3, thus caps lock LED
 			AND #%11110111
-			CPY #0			; is caps lock on?
-			BEQ ap_updc		; nope...
-				ORA #%00001000	; ...or yes
+			CPY #0				; is caps lock on?
+			BEQ ap_updc			; nope...
+				ORA #%00001000		; ...or yes
 ap_updc:
-			STA VIA_U+IORB	; update LED status
+			STA VIA_U+IORB		; update LED status
 ; get table address for this modifier combo
 ap_selt:
-		LDA ak_cmod		; retrieve modifier status
+		LDA ak_cmod			; retrieve modifier status
 #ifdef	DEADKEY
 ; *** check whether in deadkey mode for simplified modifier handling ***
-		LDX ak_dead		; will be modified by previous deadkey?
-			BNE ap_dset		; yeah
+		LDX ak_dead			; will be modified by previous deadkey?
+			BNE ap_dset			; yeah
 #endif
 ; standard table select
-		ASL				; table offsets need 9 bits!
-		TAX				; index for modifier combos
-		LDY ak_mods, X	; get pointer on main table for these modifiers
+		ASL					; table offsets need 9 bits!
+		TAX					; index for modifier combos
+		LDY ak_mods, X		; get pointer on main table for these modifiers
 		LDA ak_mods+1, X
 #ifdef	DEADKEY
 ; *** deadkey table handling ***
-		BNE ap_pset		; set this pointer (BRA)
+		BNE ap_pset			; set this pointer (BRA)
 ap_dset:
-			AND #%1001		; detect shift or caps ONLY
-			BEQ ap_dns		; unshifted...
-				INX				; ...or point to next table
+			AND #%1001			; detect shift or caps ONLY
+			BEQ ap_dns			; unshifted...
+				INX					; ...or point to next table
 				INX
 ap_dns:
-			LDY ak_dktb, X	; get pointer for deadkey-modified
+			LDY ak_dktb, X		; get pointer for deadkey-modified
 			LDA ak_dktb+1, X
 ap_pset:
 #endif
-		STY ak_mk		; save for later!
+		STY ak_mk			; save for later!
 		STA ak_mk+1
 ap_eqm:
-	LDX #14			; last regular column
+	LDX #14				; last regular column
 ap_sloop:
-		JSR ap_scol		; scan this one
-			BNE ap_kpr		; some key pressed
-		DEX				; next column
+		JSR ap_scol			; scan this one
+			BNE ap_kpr			; some key pressed
+		DEX					; next column
 		BPL ap_sloop
 	_STZA ak_scod		; clear previous scancode! eeeeeeeek
 ap_end:
-	_NO_CRIT		; eeeeeeeeek
-	RTS				; none pressed, all done
+	_NO_CRIT			; eeeeeeeeek
+	RTS					; none pressed, all done
 ; we have a raw, incomplete scancode, must convert it
 ap_kpr:
-	LDY #0			; clear row number (hopefully will stop)
+	LDY #0				; clear row number (hopefully will stop)
 ap_bshf:
-		LSR				; shift until A is clear
-	BEQ ap_scok		; Y is the highest row pressed!
-		INY				; next row
-		BNE ap_bshf		; will finish eventually
+		LSR					; shift until A is clear
+	BEQ ap_scok			; Y is the highest row pressed!
+		INY					; next row
+		BNE ap_bshf			; will finish eventually
 ap_scok:
-	TYA				; base row index
-	ORA col4, X		; include column index!
+	TYA					; base row index
+	ORA col4, X			; include column index!
 ; must check whether scancode is different from last poll
-	CMP ak_scod		; any changes?
-	BNE ap_char		; yes, get ASCII and put into buffer
+	CMP ak_scod			; any changes?
+	BNE ap_char			; yes, get ASCII and put into buffer
 #ifndef	REPEAT
-		BEQ ap_end		; do nothing if repeat is not implemented
+		BEQ ap_end			; do nothing if repeat is not implemented
 ap_char:
 #else
-		LDY ak_del		; already repeating?
-		BEQ ak_rpt		; go check its counter
-			DEC ak_del		; decrement delay counter...
-			BNE ap_end		; ...but do not repeat yet
+		LDY ak_del			; already repeating?
+		BEQ ak_rpt			; go check its counter
+			DEC ak_del			; decrement delay counter...
+			BNE ap_end			; ...but do not repeat yet
 ; delay counter has expired, start repeating at its rate
 ak_rpt:
-		DEC ak_rep		; rate counter...
-		BNE ap_end		; ...abort if not expired...
-	LDY ak_vrep		; ...or reload rate counter...
+		DEC ak_rep			; rate counter...
+		BNE ap_end			; ...abort if not expired...
+	LDY ak_vrep			; ...or reload rate counter...
 	STY ak_rep
-	BNE ap_dorp		; ...and send repeated char!
+	BNE ap_dorp			; ...and send repeated char!
 ; finish repeat (if active) and get ready for new char
 ap_char:
-	LDY ak_vdel		; preset repeat counters
+	LDY ak_vdel			; preset repeat counters
 	STY ak_del
 	LDY ak_vrep
 	STY ak_rep
 #endif
-	STA ak_scod		; save last detected! eeeeeeeeek
+	STA ak_scod			; save last detected! eeeeeeeeek
 ; get ASCII from compound scancode
 ap_dorp:
-	TAY				; use scancode as post-index
+	TAY					; use scancode as post-index
 	LDA (ak_mk), Y		; this is the ASCII code
-	_NO_CRIT		; zeropage is free
+	_NO_CRIT			; zeropage is free
 #ifdef	DEADKEY
-	LDX ak_dead		; check whether an actual deadkey-generated char
-		BNE ap_live		; yes, no further checking
-	CMP #$B4		; acute?
+	LDX ak_dead			; check whether an actual deadkey-generated char
+		BNE ap_live			; yes, no further checking
+	CMP #$B4			; acute?
 	BNE apd_b4
-		LDA #2			; first half table of dead keys
+		LDA #2				; first half table of dead keys
 		BNE ap_dead
 apd_b4:
-	CMP #$A8		; umlaut? last to be checked
+	CMP #$A8			; umlaut? last to be checked
 	BNE ap_live
-		LDA #6			; last of half-tables for deadkeys
+		LDA #6				; last of half-tables for deadkeys
 ap_dead:
-		STA ak_dead		; set deadkey mode and exit!
+		STA ak_dead			; set deadkey mode and exit!
 		RTS
 ap_live:
-	_STZA ak_dead		; no repeat for deadkeys, this far
+	_STZA ak_dead			; no repeat for deadkeys, this far
 #endif
-;	JMP ak_push		; goes into FIFO... and return to ISR
+;	JMP ak_push			; goes into FIFO... and return to ISR
 ; no need for the above if ak_push code follows!
 
 ; **************************
@@ -301,8 +301,8 @@ ak_push:
 	STA ak_buff, X		; insert char
 	INX					; go for first free position
 	CPX #AF_SIZ			; wrapped?
-	BNE ak_wnw				; no
-		LDX #0					; or yes, back to zero
+	BNE ak_wnw			; no
+		LDX #0				; or yes, back to zero
 ak_wnw:
 	STX ak_fi			; update pointer
 	CPX ak_fo			; is it full?
@@ -311,7 +311,7 @@ ak_wnw:
 		SEC					; notify error
 		LDY #FULL
 	BPL ak_room			; did not wrap, all done
-		LDA #AF_SIZ-1			; or just place it at the end
+		LDA #AF_SIZ-1		; or just place it at the end
 		STA ak_fi
 ak_room:
 	RTS					; no errors here
@@ -321,31 +321,31 @@ ak_get:
 	LDX ak_fo			; get output position
 	CPX ak_fi			; is it empty?
 	BNE ak_some			; no, do extract
-		_DR_ERR(EMPTY)			; yes, do nothing
+		_DR_ERR(EMPTY)		; yes, do nothing
 ak_some:
 	LDA ak_buff, X		; extract char
 	INX					; this is no more
 	CPX #AF_SIZ			; wrapped?
-	BNE ak_rnw				; no
-		LDX #0					; or yes, back to zero
+	BNE ak_rnw			; no
+		LDX #0				; or yes, back to zero
 ak_rnw:
 	STX ak_fo			; eeeeeeeeeek
 	_DR_OK
 
 ; *** get rows in A as selected in column X ***
 ap_scol:
-	LDA col4, X		; column times 4
-	ASL				; make it times 16 for port
+	LDA col4, X			; column times 4
+	ASL					; make it times 16 for port
 	ASL
 	STA VIA_U+IORA		; place output bits (select column)
 ; fastest machines may need some delay here
 	LDA VIA_U+IORA		; get row values back
-	AND #$0F		; just the low nibble
+	AND #$0F			; just the low nibble
 	RTS
 
 ; *** misc ***
 ak_nreq:
-	_NXT_ISR		; in case gets called, exit ASAP
+	_NXT_ISR			; in case gets called, exit ASAP
 
 ; *******************************
 ; *** diverse data and tables ***
---
+++ OS/firmware/modules/aca-vmode.s
@@ -0,0 +1,9 @@
+; Acapulco firmware module for minimOS
+; (c) 2019 Carlos J. Santisteban
+; last modified 20190304-1316
+
+; *** set Acapulco video mode at boot ***
+
+.(
+
+.)
