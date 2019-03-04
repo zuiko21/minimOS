eb73e3b9c2720695355aab64b9847ecba34f90da
Carlos Santisteban <zuiko21@users.noreply.github.com>
2019-03-04 14:11:17 +0100

    added safe CRTC setting to Acapulco FW

--- OS/firmware/modules/aca-vmode.s
+++ OS/firmware/modules/aca-vmode.s
@@ -1,10 +1,22 @@
 ; Acapulco firmware module for minimOS
 ; (c) 2019 Carlos J. Santisteban
-; last modified 20190304-1346
+; last modified 20190304-1410
 
 ; *** set Acapulco video mode at boot ***
 
 .(
+; *** it would be a good idea to set some safe values on CRTC registers ***
+	LDX #9				; last register to set
+afw_ci:
+		STX crtc_rs			; *** label from driver ***
+		LDA afw_si, X		; get safe value
+		STA crtc_da			; set register
+		DEX
+		BPL afw_ci			; until done
+	BMI afw_rm			; go read mode, no need for BRA
+afw_si:
+	.byt 47, 32, 37, 38, 32, 13, 30, 31, 0, 15		; *** revise safe values ***
+; *** this reads desired video mode from column 1 of ASCII-keyboard ***
 	_STZA VIA_U+DDRA	; safest way
 ; do I need to set DDRB?
 	LDA VIA_U+IORB		; get command
