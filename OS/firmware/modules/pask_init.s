ce60ac66f24c314b887e139ebf4c60f141b8622f
Carlos Santisteban <zuiko21@gmail.com>
2019-04-13 22:31:21 +0200

    negative CA1 on PASK init

--- OS/firmware/modules/pask_init.s
+++ OS/firmware/modules/pask_init.s
@@ -1,6 +1,6 @@
 ; firmware module for minimOS·65
 ; (c) 2019 Carlos J. Santisteban
-; last modified 20190408-1051
+; last modified 20190413-2230
 
 ; *** initialise PASK ***
 ; no interface needed
@@ -8,8 +8,8 @@
 .(
 	_STZA VIA_U+DDRA	; all input on PA
 	LDA VIA_U+PCR		; get current config
-	AND #%11110000		; keep unrelated bits (for actual PASK, might be removed)
-	ORA #%00001011		; set CA1 positive, CA2 pulse (for actual PASK, might use just 1 as no ACK is needed)
+	AND #%11110000		; keep unrelated bits (for actual PASK, might be $FE)
+	ORA #%00001010		; set CA1 negative, CA2 pulse (for actual PASK, might be removed as no ACK is needed)
 	STA VIA_U+PCR		; set handshake configuration
 	LDA VIA_U+ACR		; let us enable latching
 	ORA #1				; only needs to set bit 0
