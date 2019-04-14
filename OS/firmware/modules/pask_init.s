f18feee1d2ceb3ec5a52ee2ee87a381a1fda89e3
Carlos Santisteban <zuiko21@gmail.com>
2019-04-14 11:31:27 +0200

    fixes on PASK module

--- OS/firmware/modules/pask_init.s
+++ OS/firmware/modules/pask_init.s
@@ -1,6 +1,6 @@
 ; firmware module for minimOS·65
 ; (c) 2019 Carlos J. Santisteban
-; last modified 20190413-2230
+; last modified 20190414-1119
 
 ; *** initialise PASK ***
 ; no interface needed
@@ -8,14 +8,18 @@
 .(
 	_STZA VIA_U+DDRA	; all input on PA
 	LDA VIA_U+PCR		; get current config
+; full featured version
 	AND #%11110000		; keep unrelated bits (for actual PASK, might be $FE)
 	ORA #%00001010		; set CA1 negative, CA2 pulse (for actual PASK, might be removed as no ACK is needed)
+; simplified version for actual PASK
+;	AND #%11111110		; keep unrelated bits and set CA1 negative
 	STA VIA_U+PCR		; set handshake configuration
 	LDA VIA_U+ACR		; let us enable latching
 	ORA #1				; only needs to set bit 0
 	STA VIA_U+ACR		; ready for latched input
 ; the very basic FW drivers polls IFR until bit 1 is set
 ; otherwise, a suitable async interrupt task should put IORA on a FIFO for later CIN/BLIN read
-	LDA #2				; disable CA1 interrupt (unlike the full driver!)
+; but I do not thinks is really worth it! Polling should do fine.
+	LDA #2				; disable CA1 interrupt (unlike a full driver!)
 	STA VIA_U+IER		; all done
 .)
--- OS/firmware/modules/pask_read.s
+++ OS/firmware/modules/pask_read.s
@@ -1,6 +1,6 @@
 ; firmware module for minimOS·65
 ; (c) 2019 Carlos J. Santisteban
-; last modified 20190408-1057
+; last modified 20190414-1124
 
 ; *** read from polled PASK ***
 ; returns read code on Y, clear C
@@ -11,12 +11,12 @@
 	_STZA VIA_U+DDRA	; all input on PA
 #endif
 ; the very basic FW drivers polls IFR until bit 1 is set
-; otherwise, a suitable async interrupt task should put IORA on a FIFO for later CIN/BLIN read
-	LDA #2				; check CA1 interrupt (unlike the full driver!)
-	BIT VIA_U+IER		; was something latched?
+; otherwise, a suitable async interrupt task could put IORA on a FIFO for later CIN/BLIN read
+; ...but I do not think it is worth it!
+	LDA #2				; check CA1 interrupt (unlike a full driver!)
+	BIT VIA_U+IFR		; was something latched? eeeeeeek
 	BNE psk_no			; no, just report as empty
-		STA VIA_U+IER		; clear this interrupt source...
-		LDY VIA_U+IORA		; ...and read latched data
+		LDY VIA_U+IORA		; or clear interrupt and read latched data
 		CLC					; return with no error
 		BCC psk_end			; no need for BRA
 psk_no:
