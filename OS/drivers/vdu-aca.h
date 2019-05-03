2ea940056b5a8eb7e7dba6315298f3692ddb31fc
Carlos Santisteban <zuiko21@gmail.com>
2019-05-03 11:17:40 +0200

    notes on Acapulco GLOBAL variables

--- OS/drivers/vdu-aca.h
+++ OS/drivers/vdu-aca.h
@@ -1,8 +1,9 @@
 ; minimOS driver for Acapulco built-in VDU
 ; variables for v0.6
-; last modified 20190501-1824
+; last modified 20190503-1117
 
 ; surely needed variables
+; LOCAL data if using windows
 va_xor		.byt	0		; mask to be EORed with glyph data
 va_y		.byt	0		; current row
 va_x		.byt	0		; current column
@@ -10,10 +11,12 @@ va_attr		.byt	0		; current attributes, %ppppiiii
 va_col		.byt	0		; flag 0=normal, 16=binary mode, 18=wait for ink, 20=wait for paper, 23=wait for Y, 24=wait for X
 
 ; some variables for convenience
+; perhaps local?
 va_wdth		.byt	0		; copy number of columns here, may remove widths array too!
 va_hght		.byt	0		; ditto for number of rows
 
 ; newer variables for hardware scroll
+; GLOBAL data
 va_bi		.byt	0		; first element on circular pointer array
 va_lpl		.dsb	30, 0		; LSB of line pointers
 va_lph		.dsb	30, 0		; MSB of line pointers
