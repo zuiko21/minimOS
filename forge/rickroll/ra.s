0a0e0d46f023853d3320ff87eea43565fc0d9ff8
Carlos Santisteban <zuiko21@gmail.com>
2019-05-13 13:10:30 +0200

    cleanup on ra.s

--- forge/rickroll/ra.s
+++ forge/rickroll/ra.s
@@ -1,6 +1,6 @@
 ; RickRolling for Acapulco!
 ; (c) 2019 Carlos J. Santisteban
-; last modified 20190512-1745
+; last modified 20190513-1310
 
 .(
 ; *** minimOS header to be done ***
@@ -21,23 +21,22 @@
 #endif
 ; ** set audio interrupt handler **
 	SEI					; audio disabled by default
-	LDY #<au_isr			; ISR address
+	LDY #<au_isr		; ISR address
 	LDA #>au_isr
 	STY ex_pt			; set new ISR
 	STA ex_pt+1
 	_ADMIN(SET_ISR)
-; ** set 125 uS sampling rate ** TO DO **** TO DO **** TO DO ****
 	LDY #0				; reset counter...
 	STY vptr			; ...and indirect pointer
 ; ** must fill bitmap with checkered pattern **
-	LDA #$60			; start of VRAM
+	LDA #$60			; start page of VRAM
 	STA vptr+1
 ; new code is faster and takes 20b, was 26b
-	LDA #%01010101			; display pattern
+	LDA #%01010101		; display pattern
 vi_pat:
-	LDX #4					; pages per pattern change
+	LDX #4				; pages per pattern change
 vi_fill:
-		STA (vptr), Y			; write to screen
+		STA (vptr), Y		; write to screen
 		INY					; next
 		BNE vi_fill			; until page is done
 			INC vptr+1			; next page
@@ -56,24 +55,25 @@ vi_pb:
 ; SR free run (ACR, T2CL)
 #ifdef	SAFE
 ; save previous setup
-; T1 interrupt is likely to be enabled
-	LDA #%01111111			; disable previous interrupts
+
+; T1 interrupt is likely to be already enabled
+	LDA #%01111111		; disable previous interrupts
 	STA VIA_U+IER
-	LDA #%11000000			; enable T1 only
+	LDA #%11000000		; enable T1 only
 	STA VIA_U+IER
 #endif
-	LDA #%10010000			; no latch, free SR, T1 cont int.
+	LDA #%10010000		; no latch, free SR, T1 cont int.
 	STA VIA_U+ACR
 	LDX #$FF			; all output
 	STX VIA_U+DDRB
-	STA VIA_U+IORB			; set PB7 high for reset (PB4 irrelevant)
-	INX				; 0 => all input
+	STA VIA_U+IORB		; set PB7 high for reset (PB4 irrelevant)
+	INX					; 0 => all input
 	STX VIA_U+DDRA
-	STX VIA_U+IORB			; PB is ready to be used
-	STX VIA_U+T2CL			; maximum shift rate
+	STX VIA_U+IORB		; PB is ready to be used (reset ended)
+	STX VIA_U+T2CL		; 0 is maximum shift rate
 	LDA #190			; 125 uS sampling rate (minus 2)
-	STA VIA_U+T1CL			; latch this speed...
-	STX VIA_U+T1CH			; ...and start counting!
+	STA VIA_U+T1CL		; latch this speed...
+	STX VIA_U+T1CH		; ...and start counting!
 ; ** prepare for video play **
 	LDA #$5C			; start page of attribute area
 	STA vptr+1
@@ -83,33 +83,35 @@ vi_pb:
 	CLI					; enable audio and start playing!
 ; *** main loop *** usually 34t per byte, nearly 21 ms per frame, almost 28 incl overhead :-(
 vi_loop:
-		LDA VIA_U+IORA			; get video data... (4)
-		STA (vptr), Y			; ...into attribute area (5)
+		LDA VIA_U+IORA		; get video data... (4)
+		STA (vptr), Y		; ...into attribute area (5)
 		INY					; next byte (2)
 		BNE nx_vi			; check wrap or continue (3/15...)
 			INC vptr+1			; next page (5)
-			LDA vptr+1			; check attribute end (3+2)
+			LDA vptr+1			; check attribute area end (3+2)
 			CMP #$60
-			BNE nx_vi			; check end of frame (3...)
+			BNE nx_vi			; if frame is done (3...)
 				LDA #$5C			; (2+3) reset pointer
 				STA vptr+1
 				JSR vsync			; one frame already passed, wait for next one
-				DEC frames			; (5) fastest way up to 8s ~240 frames)
+				DEC frames			; (5) fastest way up to ~8s, 240 frames)
 				BEQ ra_end			; (2) until video is finished
 vi_nx:
-		LDA VIA_U+IORB			; get port status (4)
+		LDA VIA_U+IORB		; get port status (4)
 ; 6 lowest bits from PB1-6, rest on inboard counter pulsed by PB6 (total 7/8t)
 		CLC
-		ADC #2					; increase count on PB1-6 (2+2)
+		ADC #2				; increase count on PB1-6 (2+2)
 		BPL vi_plus			; PB7 did not change (3/4)
 			AND #$7F			; clear it otherwise
 vi_plus:
-		STA VIA_U+IORB			; set updated value (4)
+		STA VIA_U+IORB		; set updated value (4)
 		JMP vi_loop			; continue until aborted (3)
+
 ; *** clean up and finish ***
 ra_end:
 #ifdef	SAFE
 ; restore previous VIA configuration
+
 ; restore previous ISR!
 	PLA
 	STA ex_pt
@@ -117,18 +119,19 @@ ra_end:
 	STA ex_pt+1
 	_ADMIN(SET_ISR)
 #endif
+	_FINISH
 
 ; *****************
 ; *** audio ISR *** max. overhead is 43t+cur. opcode (not 37t because of vectored IRQ)
 ; *****************
-; at 1.536 MHz and 8 kHz sampling rate, overhead is ~30 uS/125 uS ~24%
+; at 1.536 MHz and 8 kHz sampling rate, overhead is ~30 uS/125 uS = 24%
 au_isr:
 	PHA					; only affected register (3)
-	LDA VIA_U+T1CL			; muat acknowledge interrupt eeeeek (4)
-	INC VIA_U+IORB			; put PB0 high (selects audio ROM) (6)
-	LDA VIA_U+IORA			; get audio PWM pattern... (4)
-	STA VIA_U+VSR			; ...into shift register (4)
-	DEC VIA_U+IORB			; put back PB0 low (advances audio counter and selects video ROM)
+	LDA VIA_U+T1CL		; must acknowledge interrupt! eeeeek (4)
+	INC VIA_U+IORB		; set PB0 high (selects audio ROM) (6)
+	LDA VIA_U+IORA		; get audio PWM pattern... (4)
+	STA VIA_U+VSR		; ...into shift register (4)
+	DEC VIA_U+IORB		; set PB0 back low (advances audio counter and selects video ROM)
 	PLA					; restore register and finish (4+6)
 	RTI
 .)
