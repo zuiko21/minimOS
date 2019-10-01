36b5ef02a1d0c44f075f573a65923ed0770e5198
Carlos Santisteban <zuiko21@users.noreply.github.com>
2019-10-01 12:38:06 +0200

    stub for dithering code

--- other/dither/24to8sg.c
+++ other/dither/24to8sg.c
@@ -1,8 +1,38 @@
 /*	24-bit dithering for 8-bit SIXtation palette
  *	(c) 2019 Carlos J. Santisteban
- *	last modified 20191001-1103 */
+ *	last modified 20191001-1219 */
 
 int main(void) {
+	char nombre[80];				/* string for filenames, plus read buffer */
+	char *pt;						/* pointer to dynamically allocated output buffer */
+	FILE *fi, *fo;					/* file handlers */
+	
+	prinf(".PPM file? ");			/* get input filename */
+	fgets(nombre, 80, stdin);		/* no longer scanf! */
+	fi=fopen(nombre, "r");			/* open input file */
+	if (fi==NULL) {
+		printf("NO FILE!\n");		/* error handling */
+		return -1;
+	}
+	strcat(nombre, ".SIX");			/* create output filename */
+	fo=fopen(nombre, "wb");			/* open output file */
+	if (fo==NULL) {
+		printf("CANNOT OUTPUT!\n");	/* error handling */
+		return -1;
+	}
+	
+/* expected format:
+P3
+#comments after pound sign
+#P3 means ASCII pixmap
+#width and height in pixels
+1360 768
+#max value
+255
+#actual data follows, no more than 76 chars per line
+#triplet order is R G B
+#whitespace is ignored
+*/
 	
 	return 0;
 }
