578876edbd0b8a48f98dbc6dc33a2e0f2d6fcc2b
Carlos Santisteban <zuiko21@gmail.com>
2019-10-15 11:07:10 +0200

    cleanup and stub for error diffusion

--- forge/palettes/dither/24to8sg.c
+++ forge/palettes/dither/24to8sg.c
@@ -1,6 +1,6 @@
 /*	24-bit dithering for 8-bit SIXtation palette
  *	(c) 2019 Carlos J. Santisteban
- *	last modified 20191014-2226 */
+ *	last modified 20191015-1106 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,47 +8,58 @@
 
 /* global variables */
 unsigned char levR[7]={18, 55, 91, 128, 164, 200, 237};
-unsigned char levG[8]={16, 48, 80, 112, 143, 175, 207, 239};
-unsigned char levB[4]={32, 96, 159, 223};
+unsigned char levG[8]=	{16, 48, 80, 112, 143, 175, 207, 239};
+unsigned char levB[4]=	{32, 96, 159, 223};
+unsigned char grey[16]={15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240};
 
 /***********************/
 /* auxiliary functions */
 /***********************/
-long coord(int x, int y, int sx) {
+long coord(int x, int y, int sx, int sy) {
 /* compute offset from coordinates */
-	return (long)sx*y+x;
+	if (x>=sx|y>=sy)	return -1;			/* negative offset means OUT of bounds! */
+	return (long)sx*y+x;					/* returns long in case int cannot handle one meg */
+}
+
+unsigned char byte(int v) {
+/* trim value to unsigned byte */
+	if (v<0)	return 0;
+	if (v>255)	return 255;
+	return (unsigned char)v;
 }
 
 unsigned char palR(int i) {
 /* get red value from standard palette */
-	if (i>31) {				/* user-defined colours */
+	if (i>31) {					/* user-defined colours */
 		return levR[((i&224)>>5)-1];
 	} else if (i<16) {			/* system colours */
 		return (i&4)?255:0;
-	} else {				/* system grayscale */
-		return (i-15)*15;
+	} else {					/* system grayscale */
+		return grey[i-16];
 	}
 }
 
 unsigned char palG(int i) {
 /* get green value from standard palette */
-	if (i>31) {				/* user-defined colours */
+	unsigned char g;
+	if (i>31) {					/* user-defined colours */
 		return levG[(i&15)>>1];
-	} else if (i<16) {			/* system colours */
-		return (((i&8)>>2)|((i&2)>>1))*85;
-	} else {				/* system grayscale */
-		return (i-15)*15;
+	} else if (i<16) {			/* system colours... a bit more difficult here as two bits for green */
+		g=((i&8)>>2)|((i&2)>>1);
+		return g|(g<<2)|(g<<4)|(g<<6);
+	} else {					/* system grayscale */
+		return grey[i-16];
 	}
 }
 
 unsigned char palB(int i) {
 /* get blue value from standard palette */
-	if (i>31) {				/* user-defined colours */
+	if (i>31) {					/* user-defined colours */
 		return levB[((i&16)>>3)|(i&1)];
 	} else if (i<16) {			/* system colours */
 		return (i&1)?255:0;
-	} else {				/* system grayscale */
-		return (i-15)*15;
+	} else {					/* system grayscale */
+		return grey[i-16];
 	}
 }
 
@@ -56,14 +67,14 @@ unsigned char palB(int i) {
 /* main program */
 /****************/
 int main(void) {
-	char nombre[80];			/* string for filenames, plus read buffer */
-	char *pt;				/* temporary pointer */
-	unsigned char *R, *G, *B, *I;		/* pointers to dynamically allocated buffers */
+	char nombre[80];				/* string for filenames, plus read buffer */
+	char *pt;						/* temporary pointer */
+	unsigned char *R, *G, *B, *I;	/* pointers to dynamically allocated buffers, may remove I! */
 	unsigned char r, g, b, i;		/* pixel values and index */
-	char dr, dg, db;			/* error diffusion variables */
-	int sx, sy, x, y;			/* coordinates and limits */
-	long xy;
-	FILE *fi, *fo;				/* file handlers */
+	int dr, dg, db;					/* error diffusion, best with extended range AND SIGNED */
+	int sx, sy, x, y;				/* coordinates and limits */
+	long xy;						/* complete array offset */
+	FILE *fi, *fo;					/* file handlers */
 
 /* get input file */
 	printf("PPM file? ");			/* get input filename */
@@ -77,13 +88,13 @@ int main(void) {
 /* swap extension on filename */
 	pt=strstr(nombre, ".ppm");		/* temporary pointer use */
 	if (pt==NULL) {				/* extension not found? */
-		pt=strstr(nombre, ".PPM");		/* perhaps in uppercase */
+		pt=strstr(nombre, ".PPM");		/* perhaps in uppercase? */
 		if (pt==NULL) {
 			printf("WRONG TYPE!\n");
 			return -1;
 		}
 	}
-	*pt='\0';				/* cut extension off */
+	*pt='\0';						/* cut extension off */
 	strcat(nombre, ".six");			/* create output filename */
 	printf("Output file:%s\n", nombre);
 
@@ -102,7 +113,7 @@ sy=1;//768;
 	R=(unsigned char*)malloc(sx*sy);
 	G=(unsigned char*)malloc(sx*sy);
 	B=(unsigned char*)malloc(sx*sy);
-	I=(unsigned char*)malloc(sx*sy);	/* indexed output */
+	I=(unsigned char*)malloc(sx*sy);		/* indexed output, might be removed! */
 	if(R==NULL||G==NULL||G==NULL||I==NULL) {
 		printf("OUT OF MEMORY!\n");
 		return -1;
@@ -122,10 +133,10 @@ P3
 */
 
 /* scan original file for error diffusion */
-	pt=0;					/* base offset */
 	for (y=0;y<sy;y++) {
 		for (x=0;x<sx;x++) {
-			xy=coord(x,y,sx);			/* current pixel */
+			xy=coord(x,y,sx,sy);	/* current pixel, no need to check bounds */
+//			if (xy>=0) {			/* not really needed here... */
 			r=R[xy];				/* component values */
 			g=G[xy];
 			b=B[xy];
@@ -134,11 +145,20 @@ i=31;/*placeholder*/
 			I[xy]=i;				/* set indexed pixel */
 			/* might be pushed directly into output file as well */
 /* compute error per channel */
-			dr=r-palR(i);
+			dr=r-palR(i);			/* these are signed! */
 			dg=g-palG(i);
 			db=b-palB(i);
 /* diffuse error */
-			
+			xy=coord(x+1,y,sx,sy);	/* pixel at right */
+			if (xy>=0) {
+				dr += R[xy];		/* add diffusion... */
+				R[xy]=byte(dr);		/* ...within bounds */
+				dg += G[xy];		/* add diffusion... */
+				G[xy]=byte(dg);		/* ...within bounds */
+				db += B[xy];		/* add diffusion... */
+				B[xy]=byte(db);		/* ...within bounds */
+			}
+//			}						/* ...in case of coordinates check */
 		}
 	}
 
