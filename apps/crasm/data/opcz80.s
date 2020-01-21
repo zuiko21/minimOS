d6f78dc78837f06c8c8428da556a79b7a32c3234
Carlos Santisteban <zuiko21@gmail.com>
2020-01-21 23:02:28 +0100

    stub for Z80 opcode list

---
+++ apps/crasm/data/opcz80.s
@@ -0,0 +1,291 @@
+; minimOS opcode list for (dis)assembler modules
+; (c) 2015-2020 Carlos J. Santisteban
+; last modified 20200121-2301
+
+; ***** for z80asm Z80 cross assembler *****
+; Z80 set, with 8085 mnemonics on comment
+; Opcode list as bit-7 terminated strings
+; @ expects single byte, & expects word
+; % expects RELATIVE addressing
+; *** need some special characters for prefixes *** TBD
+
+	.asc	"NO", 'P'+$80		; $00=NOP
+	.asc	"LD BC, ", '&'+$80	; $01=LXI B
+	.asc	"LD (BC), ", 'A'+$80	; $02=STAX B
+	.asc	"INC B", 'C'+$80	; $03=INX B
+	.asc	"INC ", 'B'+$80		; $04=INR B
+	.asc	"DEC ", 'B'+$80		; $05=DCR B
+	.asc	"LD B, ", '@'+$80	; $06=MVI B
+	.asc	"RLC", 'A'+$80		; $07=RLC
+	.asc	"EX AF, AF", $27+$80	; $08=EX AF, AF'	Z80 ONLY!
+	.asc	"ADD HL, B", 'C'+$80	; $09=DAD B
+	.asc	"LD A, (BC", ')'+$80	; $0A=LDAX B
+	.asc	"DEC B", 'C'+$80	; $0B=DCX B
+	.asc	"INC ", 'C'+$80		; $0C=INR C
+	.asc	"DEC ", 'C'+$80		; $0D=DCR C
+	.asc	"LD C, ", '@'+$80	; $0E=MVI C
+	.asc	"RRC", 'A'+$80		; $0F=RRC
+
+	.asc	"DJNZ, ", '@'+$80	; $10=DJNZ		Z80 ONLY!
+	.asc	"LD DE, ", '&'+$80	; $11=LXI D
+	.asc	"LD (DE), ", 'A'+$80	; $12=STAX D
+	.asc	"INC D", 'E'+$80	; $13=INX D
+	.asc	"INC ", 'D'+$80		; $14=INR D
+	.asc	"DEC ", 'D'+$80		; $15=DCR D
+	.asc	"LD D, ", '@'+$80	; $16=MVI D
+	.asc	"RL", 'A'+$80		; $17=RAL
+	.asc	"JR ", '%'+$80		; $18=JR		Z80 ONLY!
+	.asc	"ADD HL, D", 'E'+$80	; $19=DAD D
+	.asc	"LD A, (DE", ')'+$80	; $1A=LDAX D
+	.asc	"DEC D", 'E'+$80	; $1B=DCX D
+	.asc	"INC ", 'E'+$80		; $1C=INR E
+	.asc	"DEC ", 'E'+$80		; $1D=DCR E
+	.asc	"LD E, ", '@'+$80	; $1E=MVI E
+	.asc	"RR", 'A'+$80		; $1F=RAR
+
+	.asc	"JR NZ, ", '%'+$80	; $20=JR NZ		Z80 ONLY!
+	.asc	"LD HL, ", '&'+$80	; $21=LXI H
+	.asc	"LD (&), H", 'L'+$80	; $22=SHLD
+	.asc	"INC H", 'L'+$80	; $23=INX H
+	.asc	"INC ", 'H'+$80		; $24=INR H
+	.asc	"DEC ", 'H'+$80		; $25=DCR H
+	.asc	"LD H, ", '@'+$80	; $26=MVI H
+	.asc	"DA", 'A'+$80		; $27=DAA
+	.asc	"JR Z, ", '@'+$80	; $28=JR Z		Z80 ONLY!
+	.asc	"ADD HL, H", 'L'+$80	; $29=DAD H
+	.asc	"LD HL, (&", ')'+$80	; $2A=LDHL
+	.asc	"DEC H", 'L'+$80	; $2B=DCX H
+	.asc	"INC ", 'L'+$80		; $2C=INR L
+	.asc	"DEC ", 'L'+$80		; $2D=DCR L
+	.asc	"LD L, ", '@'+$80	; $2E=MVI L
+	.asc	"CP", 'L'+$80		; $2F=CMA
+
+	.asc	"JR NC, ", '%'+$80	; $30=JR NC		UNLIKE 8085
+	.asc	"LD SP, ", '&'+$80	; $31=LXI SP
+	.asc	"LD (&), ", 'A'+$80	; $32=STA
+	.asc	"INC S", 'P'+$80	; $33=INX SP
+	.asc	"INC (HL", ')'+$80	; $34=INR M
+	.asc	"DEC (HL", ')'+$80	; $35=DCR M
+	.asc	"LD (HL), ", '@'+$80	; $36=MVI M
+	.asc	"SC", 'F'+$80		; $37=STC
+	.asc	"JR C, ", '%'+$80	; $38=JR C		Z80 ONLY!
+	.asc	"ADD HL, S", 'P'+$80	; $39=DAD SP
+	.asc	"LD A, (&", ')'+$80	; $3A=LDA
+	.asc	"DEC S", 'P'+$80	; $3B=DCX SP
+	.asc	"INC ", 'A'+$80		; $3C=INR A
+	.asc	"DEC ", 'A'+$80		; $3D=DCR A
+	.asc	"LD A, ", '@'+$80	; $3E=MVI A
+	.asc	"CC", 'F'+$80		; $3F=CMC
+
+	.asc	"LD B, ", 'B'+$80	; $40=MOV B,B
+	.asc	"LD B, ", 'C'+$80	; $41=MOV B,C
+	.asc	"LD B, ", 'D'+$80	; $42=MOV B,D
+	.asc	"LD B, ", 'E'+$80	; $43=MOV B,E
+	.asc	"LD B, ", 'H'+$80	; $44=MOV B,H
+	.asc	"LD B, ", 'L'+$80	; $45=MOV B,L
+	.asc	"LD B, (HL", ')'+$80	; $46=MOV B,M
+	.asc	"LD B, ", 'A'+$80	; $47=MOV B,A
+	.asc	"LD C, ", 'B'+$80	; $48=MOV C,B
+	.asc	"LD C, ", 'C'+$80	; $49=MOV C,C
+	.asc	"LD C, ", 'D'+$80	; $4A=MOV C,D
+	.asc	"LD C, ", 'E'+$80	; $4B=MOV C,E
+	.asc	"LD C, ", 'H'+$80	; $4C=MOV C,H
+	.asc	"LD C, ", 'L'+$80	; $4D=MOV C,L
+	.asc	"LD C, (HL", ')'+$80	; $4E=MOV C,M
+	.asc	"LD C, ", 'A'+$80	; $4F=MOV C,A
+
+
+	.asc	"LD D, ", 'B'+$80	; $50=MOV D,B
+	.asc	"LD D, ", 'C'+$80	; $51=MOV D,C
+	.asc	"LD D, ", 'D'+$80	; $52=MOV D,D
+	.asc	"LD D, ", 'E'+$80	; $53=MOV D,E
+	.asc	"LD D, ", 'H'+$80	; $54=MOV D,H
+	.asc	"LD D, ", 'L'+$80	; $55=MOV D,L
+	.asc	"LD D, (HL", ')'+$80	; $56=MOV D,M
+	.asc	"LD D, ", 'A'+$80	; $57=MOV D,A
+	.asc	"LD E, ", 'B'+$80	; $58=MOV E,B
+	.asc	"LD E, ", 'C'+$80	; $59=MOV E,C
+	.asc	"LD E, ", 'D'+$80	; $5A=MOV E,D
+	.asc	"LD E, ", 'E'+$80	; $5B=MOV E,E
+	.asc	"LD E, ", 'H'+$80	; $5C=MOV E,H
+	.asc	"LD E, ", 'L'+$80	; $5D=MOV E,L
+	.asc	"LD E, (HL", ')'+$80	; $5E=MOV E,M
+	.asc	"LD E, ", 'A'+$80	; $5F=MOV E,A
+
+	.asc	"LD H, ", 'B'+$80	; $60=MOV H,B
+	.asc	"LD H, ", 'C'+$80	; $61=MOV H,C
+	.asc	"LD H, ", 'D'+$80	; $62=MOV H,D
+	.asc	"LD H, ", 'E'+$80	; $63=MOV H,E
+	.asc	"LD H, ", 'H'+$80	; $64=MOV H,H
+	.asc	"LD H, ", 'L'+$80	; $65=MOV H,L
+	.asc	"LD H, (HL", ')'+$80	; $66=MOV H,M
+	.asc	"LD H, ", 'A'+$80	; $67=MOV H,A
+	.asc	"LD L, ", 'B'+$80	; $68=MOV L,B
+	.asc	"LD L, ", 'C'+$80	; $69=MOV L,C
+	.asc	"LD L, ", 'D'+$80	; $6A=MOV L,D
+	.asc	"LD L, ", 'E'+$80	; $6B=MOV L,E
+	.asc	"LD L, ", 'H'+$80	; $6C=MOV L,H
+	.asc	"LD L, ", 'L'+$80	; $6D=MOV L,L
+	.asc	"LD L, (HL", ')'+$80	; $6E=MOV L,M
+	.asc	"LD L, ", 'A'+$80	; $6F=MOV L,A
+
+	.asc	"LD (HL), ", 'B'+$80	; $70=MOV M,B
+	.asc	"LD (HL), ", 'C'+$80	; $71=MOV M,C
+	.asc	"LD (HL), ", 'D'+$80	; $72=MOV M,D
+	.asc	"LD (HL), ", 'E'+$80	; $73=MOV M,E
+	.asc	"LD (HL), ", 'H'+$80	; $74=MOV M,H
+	.asc	"LD (HL), ", 'L'+$80	; $75=MOV M,L
+	.asc	"HAL", 'T'+$80		; $76=HLT
+	.asc	"LD (HL), ", 'A'+$80	; $77=MOV M,A
+	.asc	"LD A, ", 'B'+$80	; $78=MOV A,B
+	.asc	"LD A, ", 'C'+$80	; $79=MOV A,C
+	.asc	"LD A, ", 'D'+$80	; $7A=MOV A,D
+	.asc	"LD A, ", 'E'+$80	; $7B=MOV A,E
+	.asc	"LD A, ", 'H'+$80	; $7C=MOV A,H
+	.asc	"LD A, ", 'L'+$80	; $7D=MOV A,L
+	.asc	"LD A, (HL", ')'+$80	; $7E=MOV A,M
+	.asc	"LD A, ", 'A'+$80	; $7F=MOV A,A
+
+	.asc	"ADD A, ", 'B'+$80	; $80=ADD B
+	.asc	"ADD A, ", 'C'+$80	; $81=ADD C
+	.asc	"ADD A, ", 'D'+$80	; $82=ADD D
+	.asc	"ADD A, ", 'E'+$80	; $83=ADD E
+	.asc	"ADD A, ", 'H'+$80	; $84=ADD H
+	.asc	"ADD A, ", 'L'+$80	; $85=ADD L
+	.asc	"ADD A, (HL", ')'+$80	; $86=ADD M
+	.asc	"ADD A, ", 'A'+$80	; $87=ADD A
+	.asc	"ADC A, ", 'B'+$80	; $88=ADC B
+	.asc	"ADC A, ", 'C'+$80	; $89=ADC C
+	.asc	"ADC A, ", 'D'+$80	; $8A=ADC D
+	.asc	"ADC A, ", 'E'+$80	; $8B=ADC E
+	.asc	"ADC A, ", 'H'+$80	; $8C=ADC H
+	.asc	"ADC A, ", 'L'+$80	; $8D=ADC L
+	.asc	"ADC A, (HL", ')'+$80	; $8E=ADC M
+	.asc	"ADC A, ", 'A'+$80	; $8F=ADC A
+
+	.asc	"SUB ", 'B'+$80		; $90=SUB B
+	.asc	"SUB ", 'C'+$80		; $91=SUB C
+	.asc	"SUB ", 'D'+$80		; $92=SUB D
+	.asc	"SUB ", 'E'+$80		; $93=SUB E
+	.asc	"SUB ", 'H'+$80		; $94=SUB H
+	.asc	"SUB ", 'L'+$80		; $95=SUB L
+	.asc	"SUB (HL", ')'+$80	; $96=SUB M
+	.asc	"SUB ", 'A'+$80		; $97=SUB A
+	.asc	"SBC A, ", 'B'+$80	; $98=SBB B
+	.asc	"SBC A, ", 'C'+$80	; $99=SBB C
+	.asc	"SBC A, ", 'D'+$80	; $9A=SBB D
+	.asc	"SBC A, ", 'E'+$80	; $9B=SBB E
+	.asc	"SBC A, ", 'H'+$80	; $9C=SBB H
+	.asc	"SBC A, ", 'L'+$80	; $9D=SBB L
+	.asc	"SBC A, (HL", ')'+$80	; $9E=SBB M
+	.asc	"SBC A, ", 'A'+$80	; $9F=SBB A
+
+	.asc	"AND ", 'B'+$80		; $A0=ANA B
+	.asc	"AND ", 'C'+$80		; $A1=ANA C
+	.asc	"AND ", 'D'+$80		; $A2=ANA D
+	.asc	"AND ", 'E'+$80		; $A3=ANA E
+	.asc	"AND ", 'H'+$80		; $A4=ANA H
+	.asc	"AND ", 'L'+$80		; $A5=ANA L
+	.asc	"AND (HL", ')'+$80	; $A6=ANA M
+	.asc	"AND ", 'A'+$80		; $A7=ANA A
+	.asc	"XOR ", 'B'+$80		; $A8=XRA B
+	.asc	"XOR ", 'C'+$80		; $A9=XRA C
+	.asc	"XOR ", 'D'+$80		; $AA=XRA D
+	.asc	"XOR ", 'E'+$80		; $AB=XRA E
+	.asc	"XOR ", 'H'+$80		; $AC=XRA H
+	.asc	"XOR ", 'L'+$80		; $AD=XRA L
+	.asc	"XOR (HL", ')'+$80	; $AE=XRA M
+	.asc	"XOR ", 'A'+$80		; $AF=XRA A
+
+	.asc	"OR ", 'B'+$80		; $B0=ORA B
+	.asc	"OR ", 'C'+$80		; $B1=ORA C
+	.asc	"OR ", 'D'+$80		; $B2=ORA D
+	.asc	"OR ", 'E'+$80		; $B3=ORA E
+	.asc	"OR ", 'H'+$80		; $B4=ORA H
+	.asc	"OR ", 'L'+$80		; $B5=ORA L
+	.asc	"OR (HL", ')'+$80	; $B6=ORA M
+	.asc	"OR ", 'A'+$80		; $B7=ORA A
+	.asc	"CP ", 'B'+$80		; $B8=CMP B
+	.asc	"CP ", 'C'+$80		; $B9=CMP C
+	.asc	"CP ", 'D'+$80		; $BA=CMP D
+	.asc	"CP ", 'E'+$80		; $BB=CMP E
+	.asc	"CP ", 'H'+$80		; $BC=CMP H
+	.asc	"CP ", 'L'+$80		; $BD=CMP L
+	.asc	"CP (HL", ')'+$80	; $BE=CMP M
+	.asc	"CP ", 'A'+$80		; $BF=CMP A
+
+	.asc	"RET N", 'Z'+$80	; $C0=RNZ
+	.asc	"POP B", 'C'+$80	; $C1=POP B
+	.asc	"JP NZ, ", '&'+$80	; $C2=JNZ
+	.asc	"JP ", '&'+$80		; $C3=JMP
+	.asc	"CALL NZ, ", '&'+$80	; $C4=CNZ
+	.asc	"PUSH B", 'C'+$80	; $C5=PUSH B
+	.asc	"ADD A, ", '@'+$80	; $C6=ADI
+	.asc	"RST 00", 'H'+$80	; $C7=RST 0
+	.asc	"RET ", 'Z'+$80		; $C8=RZ
+	.asc	"RE", 'T'+$80		; $C9=RET
+	.asc	"JP Z, ", '&'+$80	; $CA=JZ
+	.asc	"?", ' '+$80		; $CB=**BITS**		** Z80 PREFIX **
+	.asc	"CALL Z, ", '&'+$80	; $CC=CZ
+	.asc	"CALL ", '&'+$80	; $CD=CALL
+	.asc	"ADC A, ", '@'+$80	; $CE=ACI
+	.asc	"RST 08", 'H'+$80	; $CF=RST 1
+
+	.asc	"RET N", 'C'+$80	; $D0=RNC
+	.asc	"POP D", 'E'+$80	; $D1=POP D
+	.asc	"JP NC, ", '&'+$80	; $D2=JNC
+	.asc	"OUT (@), ", 'A'+$80	; $D3=OUT
+	.asc	"CALL NC, ", '&'+$80	; $D4=CNC
+	.asc	"PUSH D", 'E'+$80	; $D5=PUSH D
+	.asc	"SUB ", '@'+$80		; $D6=SUI
+	.asc	"RST 10", 'H'+$80	; $D7=RST 2
+	.asc	"RET ", 'C'+$80		; $D8=RC
+	.asc	"EX", 'X'+$80		; $D9=EXX		Z80 ONLY!
+	.asc	"JP C, ", '&'+$80	; $DA=JC
+	.asc	"IN A, (@", ')'+$80	; $DB=IN
+	.asc	"CALL C, ", '&'+$80	; $DC=CC
+	.asc	"? ", '&'+$80		; $DD=**IX+D**		** Z80 PREFIX **
+	.asc	"SBA A, ", '@'+$80	; $DE=SBI
+	.asc	"RST 18", 'H'+$80	; $DF=RST 3
+
+	.asc	"RET P", 'O'+$80	; $E0=RPO
+	.asc	"POP H", 'L'+$80	; $E1=POP H
+	.asc	"JP PO, ", '&'+$80	; $E2=JPO
+	.asc	"EX (SP), H", 'L'+$80	; $E3=XTHL
+	.asc	"CALL PO, ", '&'+$80	; $E4=CPO
+	.asc	"PUSH H", 'L'+$80	; $E5=PUSH H
+	.asc	"AND ", '@'+$80		; $E6=ANI
+	.asc	"RST 20", 'H'+$80	; $E7=RST 4
+	.asc	"RET P", 'E'+$80	; $E8=RPE
+	.asc	"JP (HL", ')'+$80	; $E9=PCHL
+	.asc	"JP PE, ", '&'+$80	; $EA=JPE
+	.asc	"EX DE, H", 'L'+$80	; $EB=XCHG
+	.asc	"CALL PE, ", '&'+$80	; $EC=CPE
+	.asc	"?", ' '+$80		; $ED=**EXTD**		** Z80 PREFIX **
+	.asc	"XOR ", '@'+$80		; $EE=XRI
+	.asc	"RST 28", 'H'+$80	; $EF=RST 5
+
+	.asc	"RET ", 'P'+$80		; $F0=RP
+	.asc	"POP A", 'F'+$80	; $F1=POP PSW
+	.asc	"JP P, ", '&'+$80	; $F2=JP
+	.asc	"D", 'I'+$80		; $F3=DI
+	.asc	"CALL P, ", '&'+$80	; $F4=CP
+	.asc	"PUSH A", 'F'+$80	; $F5=PUSH PSW
+	.asc	"OR ", '@'+$80		; $F6=ORI
+	.asc	"RST 30", 'H'+$80	; $F7=RST 6
+	.asc	"RET ", 'M'+$80		; $F8=RM
+	.asc	"LD SP, H", 'L'+$80	; $F9=SPHL
+	.asc	"JP M, ", '&'+$80	; $FA=JM
+	.asc	"E", 'I'+$80		; $FB=EI
+	.asc	"CALL M, ", '&'+$80	; $FC=CM
+	.asc	"? ", '&'+$80		; $FD=**IY+D**		** Z80 PREFIX **
+	.asc	"CP ", '@'+$80		; $FE=CPI
+	.asc	"RST 38", 'H'+$80	; $FF=RST 7
+
+; *** BIT instructions ($CB prefix) ***
+
+; *** IX+d indexed instructions ($DD prefix) ***
+
+; *** extended instructions ($ED prefix) ***
+
+; *** IY+d indexed instructions ($FD prefix) ***
