9cf6d31b6f122b97261b86b200c146ef7d3e4f47
Carlos Santisteban <zuiko21@users.noreply.github.com>
2020-05-05 09:40:32 +0200

    thinking about corner positions on VGA autoconfig

--- forge/gc_config/vga_t_set.s
+++ forge/gc_config/vga_t_set.s
@@ -18,4 +18,21 @@ vs_cls:
 		BNE vs_cls
 			INC z_pt+1		; next page
 		BPL vs_cls		; fortunately, it's until the last "positive" address!
-		
+; *******************************************************************************
+; *** now let's place some patterns at the corners of every screen resolution ***
+; *******************************************************************************
+; 170 for left side, 85 for the right (via LSR/ASL)
+; $6000 is upper left on ALL modes
+; add 31, 35 or 39 for upper right ($601F, $6023 & $6027)
+; bottom is much more difficult... base scanline offset is 7168 (7 Ki) or $8C00
+; but must add (rows-1)*cols for the left, adding (cols-1) for the right
+
+	LDA #%10101010	; leftmost pattern
+	STA $6000		; upper left is always the same address
+					; store at bottom left places
+	LSR				; makes %01010101 for rightmost pattern
+	STA $601F		; store at upper right positions
+	STA $6023
+	STA $6027
+					; store at bottom right places
+	
