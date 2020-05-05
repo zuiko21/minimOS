3034856b7d331a4cbec819da1054935742995eab
Carlos Santisteban <zuiko21@users.noreply.github.com>
2020-05-05 10:03:58 +0200

    describe modes for VGA auto

--- forge/gc_config/vga_t_set.s
+++ forge/gc_config/vga_t_set.s
@@ -18,6 +18,7 @@ vs_cls:
 		BNE vs_cls
 			INC z_pt+1		; next page
 		BPL vs_cls		; fortunately, it's until the last "positive" address!
+
 ; *******************************************************************************
 ; *** now let's place some patterns at the corners of every screen resolution ***
 ; *******************************************************************************
@@ -29,10 +30,27 @@ vs_cls:
 
 	LDA #%10101010	; leftmost pattern
 	STA $6000		; upper left is always the same address
-					; store at bottom left places
+	STA $7FA0		; store at bottom left places (f/32, 40 & 36)
+	STA $7FC0
+	STA $7FCC
 	LSR				; makes %01010101 for rightmost pattern
-	STA $601F		; store at upper right positions
+	STA $601F		; store at upper right positions (32, 36 & 40)
 	STA $6023
 	STA $6027
-					; store at bottom right places
-	
+	STA $7FBF		; store at bottom right places (32, 40 & 36)
+	STA $7FE7
+	STA $7FEF
+
+; with suitable patterns on screen, make a 10s timeout
+; if CR is pressed, keep current mode and go on
+; if timeout expired, set safe mode (36-D) and go on
+; every time SPC is pressed, cycle between modes and reset timer
+; suggested order is:
+; 40 (40x25, VGA industry standard timing)
+; 36 (36x28, standard timing)
+; 32-L (32x30, VSYNC set for Leading edge)
+; 32-T (32x30, VSYNC set for Trailing edge)
+; 40-DS (40x25 on 24.576 MHz dotclock and shorter sync)
+; 36-D (36x28 on 24.576, SAFEST mode)
+; 32-DL (32x30 on 24.576, VSYNC set for Leading edge)
+; 32-DT (32x30 on 24.576, VSYNC set for Trailing edge)
