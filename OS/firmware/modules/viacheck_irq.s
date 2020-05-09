bb66db7447e8ceb039e138273b46ea2ff4eaf936
Carlos Santisteban <zuiko21@gmail.com>
2020-05-09 15:00:26 +0200

    1 byte saved on VIA-check

--- OS/firmware/modules/viacheck_irq.s
+++ OS/firmware/modules/viacheck_irq.s
@@ -1,6 +1,6 @@
 ; firmware module for minimOS·65
 ; (c) 2017-2020 Carlos J. Santisteban
-; last modified 20171221-1302
+; last modified 20200509-1434
 
 ; *** check for VIA and disable all interrupt sources ***
 ; NMOS and 65816 savvy
@@ -8,13 +8,14 @@
 
 .(
 ; disable all interrupt sources
-	LDA #$7F		; disable all interrupts (2+4)
-	STA VIA_J + IER	; *** this is for single VIA systems ***
+	LDX #$7F		; disable all interrupts (2+4)
+	STX VIA_J + IER	; *** this is for single VIA systems ***
+; could be repeated for multi-VIA systems
 
 ; and optionally check for VIA presence
 #ifdef	SAFE
-	LDA VIA_J + IER	; check VIA presence, NEW 20150118 (4)
-	CMP #$80		; should read $80 (2)
+	INX				; now X holds $80, as IER should read... (2)
+	CPX VIA_J + IER	; check VIA presence, NEW 20150118 (4)
 	BEQ via_ok		; panic otherwise! (slight modification 20150121 and 0220) (3/2)
 		JMP lock		; no other way to tell the world... (3)
 via_ok:
