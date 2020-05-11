3e371e5d5bf301526c1104a5b80079dde41eff3d
Carlos Santisteban <zuiko21@users.noreply.github.com>
2020-05-11 12:43:35 +0200

    Create 6502-128.s

---
+++ other/PassingCars/6502-128.s
@@ -0,0 +1,33 @@
+; Simple 65(C)02 code for the Passing Cars exercise
+; https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars
+; (c) 2020 Carlos J. Santisteban
+
+; *** CAVEATS ***
+; No execution limit
+; Array up to 128 elements
+; Array elements are bytes, containing either zero or any non-zero value
+
+; *** memory use ***
+total	.byt	0		; single-byte counter
+array	.dsb	128, 0	; array
+
+; ************
+; *** CODE ***
+; ************
+	LDX #length-1		; start from the LAST element, going backwards
+	LDY #0				; reset partial counter (Y)
+	STY total			; reset total too
+loop:
+		LDA array, X	; get element from array (zero or otherwise)
+		BEQ zero		; if not zero...
+			INY			; ...increment partial counter
+			BRA next	; NMOS 6502 could use BNE as well
+zero:
+			TYA			; else add partial counter...
+			CLC
+			ADC total	; ...to current total
+			STA total
+next:
+		DEX				; go for next element
+		BPL loop
+; ************
