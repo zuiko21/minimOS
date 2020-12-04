dded236935c639ae5c3cff2caf0d941db1e7c835
Carlos Santisteban <zuiko21@gmail.com>
2020-12-04 23:30:25 +0100

    RAMless version of A5 blinkç

---
+++ other/blink_ramless.s
@@ -0,0 +1,68 @@
+; ***   A5 blinking LED mini-test    ***
+; *** ***   RAM-less version!    *** ***   
+; *** (c) 2020 Carlos J. Santisteban ***
+; *** last modified 20201204-2326    ***
+
+		.text
+
+; ****************************************
+; *** delay loop, first issue (A5 low) ***
+; ****************************************
+
+		* = $FFC0			; whole code fits into the last 64-byte chunk
+
+start:
+		SEI					; just in case
+		CLD
+blink1:
+			JSR ex_rts		; just some suitable delay
+			INX
+			BNE blink1
+				INY
+			BNE blink1
+		JMP blink2			; that will turn A5 on! 14 bytes this chunk
+
+; ********************************************
+; *** filling until the next 32-byte chunk ***
+; ********************************************
+
+		.dsb	blink2-*, $FF
+
+; ***********************************
+; *** second delay loop (A5 high) ***
+; ***********************************
+
+		* = $FFE0			; A5 is now on
+
+blink2:
+			JSR ex_rts		; just some suitable delay
+			INX
+			BNE blink2
+				INY
+			BNE blink2
+		JMP blink1			; and turn A off again, this chunk is 14 bytes plus 6 for the vectors
+
+; *************************
+; *** auxiliary opcodes ***
+; *************************
+
+ex_rts:	RTS					; *** label for delay via JSR/RTS  ***
+ex_rti:	RTI					; *** label for unexpected IRQ/BRK ***
+
+; *************************************
+; *** filling until the ROM vectors ***
+; *************************************
+
+		vectors = $FFFA
+
+		.dsb	vectors-*, $FF
+
+; *************************************
+; *** *** 6502 hardware vectors *** ***
+; *************************************
+
+		* = vectors
+
+		.word	start		; *** without monitor or any IO, will just acknowledge NMI as cycle restart ***
+		.word	start		; *** only functionality of this device ***
+		.word	ex_rti		; *** will ignore unexpected IRQs or BRKs ***
