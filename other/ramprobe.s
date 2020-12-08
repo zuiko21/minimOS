2796aaae6ef9f388a4fd6ee7841681bf06b7af82
Carlos Santisteban <zuiko21@gmail.com>
2020-12-08 16:48:15 +0100

    ramprobe detects 128-byte mirrored as 512

--- other/ramprobe.s
+++ other/ramprobe.s
@@ -1,6 +1,6 @@
-; *** RAM check for 65(C)02          ***
+; *** RAM probe for 65(C)02 v2       ***
 ; *** (c) 2020 Carlos J. Santisteban ***
-; *** last modified 20201208-1106    ***
+; *** last modified 20201208-1635    ***
 
 	.zero
 ; *** zeropage declarations ***
@@ -39,7 +39,7 @@ bytchk:
 			BCC bytchk		; try both patterns
 		BCS good			; that byte is OK, thus try to add another (less significant) bit
 bad:
-; if arrived here, the proposed bit goes beyond addressable RAM and must be removed
+; the proposed bit goes beyond addressable RAM and must be removed
 			LDA chkmsk		; offending bit
 			EOR #$FF		; mask for removing
 			AND chkptr+1	; previous pointer minus offending bit
@@ -47,62 +47,63 @@ bad:
 good:
 		LSR chkmsk			; try a lower bit now
 		BCC try
-; if arrived here, chkptr.H is the highest addressable RAM byte (mirrored or not)
+; chkptr+1 is the highest addressable RAM page (mirrored or not)
 	LDX chkptr+1			; more than 256 bytes?
 	BNE mirror				; as expected, investigate mirroring
+; here we have no more than 256 bytes of RAM, thus check them all
 zpchk:
 		LDA #$AA			; test pattern
 		DEX					; initial value was zero, now $FF
-		CPX #1				; for 6510-savvyness, but cannot fully check down to zero, and a 2-byte RAM system would be quite useless anyway
-		BEQ *				; *** locked (no valid byte over $0002) ***
+		CPX #7				; cannot fully check down to zero, such a system would be quite useless anyway
+		BEQ *				; *** locked (no more than 8 bytes of RAM) ***
 patchk:
 			STA 0, X		; try writing it
 			CMP 0, X		; wrote OK?
 				BNE zpchk	; try another if it did not
 			LSR				; go for next pattern ($55 or exit)
 			BCC patchk		; try both patterns
-		BCS top				; that byte is OK, thus it must bit the RAMtop (in zeropage!)
-	STX chkptr				; if arrived here, X points to the highest byte (MSB expected to be zero)
+		STX chkptr			; if arrived here, X points to the highest byte (MSB expected to be zero)
 ; *** at this point, chkptr points to the highest addressable RAM byte, mirrored or not ***
 mirror:
 	TXA						; if X=0, we have more than 256 bytes of RAM, thus check for page mirroring
-	BEQ page_mr
-zp_bw:
-			TXA				; reload byte number
-			STA 0, X		; try to store it
-			DEX				; previous byte
-			CPX #1			; must be 6510-savvy
-			BNE zp_bw
-		LDX chkptr			; retrieve stored pointer
-zp_mr:
-			TXA				; there should be the byte number itself
-			CMP 0, X		; matches read value?
-				BEQ zp_nm	; found highest real byte!
-			DEX				; or try previous byte, as usual
-			CPX #1
-			BNE zp_mr
-zp_nm:
- 		STX chkptr			; non-mirrored zeropage last byte!
- 		BEQ end				; as per previous comparisons, no need for BRA
-page_mr:
-	STX chkpag				; (re)set pointers
-	LDA chkptr+1
-	STA chkpag+1
+	BNE no_page				; otherwise just check zeropage
+; check pages for mirroring *** first write page number on every available page
+		STX chkpag			; (re)set pointers, it was zero
+		LDA chkptr+1
+		STA chkpag+1
 pgw_loop:
-		LDA chkpag+1		; reload page number
-		STA (chkpag), Y		; store page number
-		DEC chkpag+1		; previous page
-		BNE pgw_loop
-	LDA chkptr+1			; let's scan pages again
-	STA chkpag+1
+			LDA chkpag+1	; reload page number
+			STA (chkpag), Y	; store page number
+			DEC chkpag+1	; previous page
+			BNE pgw_loop
+		TXA					; write a zero...
+		STA (chkpag), Y		; on what should be chkpag @ zeropage, in case it's mirrored!
+; now have a look at the stored values, whether they're read at their respective pages
+		LDA chkptr+1		; let's scan pages again
+		STA chkpag+1
 pgr_loop:
-		LDA chkpag+1		; reload page number
-		CMP (chkpag), Y		; compare with stored page number
-			BEQ pg_found	; if match, there is no mirroring
-		DEC chkpag+1		; otherwise try previous page
-		BNE pgr_loop
-	BEQ *					; trap, cannot arrive here!
-pg_found:
+			LDA chkpag+1	; reload page number
+			CMP (chkpag), Y	; compare with stored page number
+				BEQ pg_found	; if they match, there is no mirroring
+			DEC chkpag+1	; otherwise try previous page
+			BNE pgr_loop
+; if arrived here, there's only zeropage mirrored elsewhere
+no_page:
 	STA chkptr+1			; now that's the real last byte
-end:
+zp_bw:
+		TXA					; reload byte number
+		STA 0, X			; try to store it
+		DEX					; previous byte
+		CPX #7				; do not touch first 8 bytes
+		BNE zp_bw
+	LDX chkptr				; retrieve stored count
+zp_mr:
+		TXA					; there should be the byte number itself
+		CMP 0, X			; matches read value?
+			BEQ zp_nm		; found highest real byte!
+		DEX					; or try previous byte, as usual
+		CPX #1
+		BNE zp_mr
+zp_nm:
+ 	STX chkptr				; non-mirrored zeropage last byte!
 ; *** chkptr holds the last real address of non-mirrored RAM ***
