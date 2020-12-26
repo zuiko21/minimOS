c85de250d5b5d250c2b946564b2ff680dd5eb90c
Carlos Santisteban <zuiko21@gmail.com>
2020-12-26 16:06:13 +0100

    sound test on ROM and SMC versions

---
+++ forge/test/a14s_rom.s
@@ -0,0 +1,62 @@
+; sound test (speaker on *A13*)
+; *** ROMmable version ***
+; (c) 2020 Carlos J. Santisteban
+; last modified 20201226-1557
+
+	.text
+
+; *** addresses configuation ***
+	*	= $C000				; 27C128 savvy
+
+alt		= $FFC0				; needs 12 kiB RAM! change and use another address line if needed (A14...A6)
+
+; since * is %1100000000000000
+; and alt is $1111111111000000
+; A13 to A6 will do
+
+; *** sound execution ***
+init:
+	LDY #170				; initial value for lower freq (1=max)
+; *** delay loop for both locations ***
+d_st:
+		TYA
+		TAX					; set initial X
+d_loop:
+			DEX
+			BNE d_loop		; initial delay, depending on initial X
+d_last:
+			DEX
+			BNE d_last		; last, fixed delay (as X is 0)
+		DEY					; now a bit higher freq
+		BNE cont
+lock:
+			JMP lock		; lock here
+cont:
+	JMP alt					; execute alternate copy *** this will change on copy ***
+
+; ****************************************************************
+	.dsb	alt-*, $FF		; *** padding until alternate copy ***
+; ****************************************************************
+
+		TYA
+		TAX					; set initial X
+a_loop:
+			DEX
+			BNE a_loop		; initial delay, depending on initial X
+a_last:
+			DEX
+			BNE a_last		; last, fixed delay (as X is 0)
+		DEY					; now a bit higher freq
+		BNE back
+lock2:
+			JMP lock2		; lock here
+back:
+	JMP d_st				; return to original
+
+; *************************************************************
+	.dsb	$FFFA-*, $FF	; *** ROM padding until vectors ***
+; *************************************************************
+
+	.word	lock
+	.word	init			; interrupts will lock at different addresses
+	.word	lock2
---
+++ forge/test/a14sound.s
@@ -0,0 +1,45 @@
+; sound test (speaker on A14)
+; *** downloadable version ***
+; (c) 2020 Carlos J. Santisteban
+; last modified 20201226-1554
+
+	.text
+
+; *** addresses configuation ***
+	*	= $0400				; most firmware savvy
+
+alt		= $5FC0				; needs 32 kiB RAM! change and use another address line if needed (A14...A6)
+
+start:
+; *** copy delay loop in alternate location ***
+	LDX #end-d_st-1			; bytes to copy
+copy:
+		LDA d_st, X			; get source
+		STA alt, X			; put on alternate location
+		DEX
+		BPL copy			; until all code is copied
+; *** modify alternate copy for return to original ***
+	LDX #>d_st				; pick original return address
+	LDY #<d_st
+	STX alt+cont+2-d_st		; set on alternate copy
+	STY alt+cont+1-d_st
+; *** sound execution ***
+init:
+	LDY #170				; initial value for lower freq (1=max)
+; *** delay loop for both locations ***
+d_st:
+		TYA
+		TAX					; set initial X
+d_loop:
+			DEX
+			BNE d_loop		; initial delay, depending on initial X
+d_last:
+			DEX
+			BNE d_last		; last, fixed delay (as X is 0)
+		DEY					; now a bit higher freq
+		BNE cont
+lock:
+			JMP lock		; lock here
+cont:
+	JMP alt					; execute alternate copy *** this will change on copy ***
+end:
