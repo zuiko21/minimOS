28a4cdf032521356279117941f65fcb1106c3f14
Carlos Santisteban <zuiko21@gmail.com>
2020-12-27 18:21:47 +0100

    C nanoboot server ready for RPi

--- forge/nanoboot/server.c
+++ forge/nanoboot/server.c
@@ -1,10 +1,15 @@
 /* nanoBoot server for Raspberry Pi! *
  * (c) 2020 Carlos J. Santisteban    *
- * last modified 20201227-1719       */
+ * last modified 20201227-1820       */
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <wiringPi.h>
+#include <wiringPi.h>
+
+/* definición de pines, 36-38-40 en el conector, BCM 16-20-21 */
+#define	CB1		27
+#define	CB2		28
+#define	STB		29
 
 /* prototipos */
 void dato(int x);
@@ -15,6 +20,12 @@ int main(void) {
 	int		i, c, fin, ini;
 	char	nombre[80];
 
+/* preparar GPIO */
+	wiringPiSetup();
+	digitalWrite(CB1, 1);
+	pinMode(CB1, OUTPUT);
+	pinMode(CB2, OUTPUT);
+	pinMode(STB, OUTPUT);
 /* abrir archivo original */
 	printf("Archivo: ");
 	scanf("%s", nombre);
@@ -38,6 +49,7 @@ int main(void) {
 	rewind(f);
 	printf("¡¡¡TOMA!!! ¡Todo pa´dentro, maricón!\n");
 	for (i=ini; i<fin; i++) {
+		if (i&255 == 0)		delay(1);
 		c = fgetc(f);
 		dato(c);
 	}
@@ -53,11 +65,13 @@ void dato(int x) {
 	
 	while(i>0) {
 		bit = x & 1;
-		// poner bit y pulsar reloj
-		printf("%d", bit);//placeholder
+		digitalWrite(CB2, bit);
+		digitalWrite(CB1, 0);
+		digitalWrite(CB1, 1);
+//		printf("%d", bit);//placeholder
 		x >>= 1;
 		i--;
 	}
-//	delay(1);
-	printf(" ");//placeholder
+	delay(1);
+//	printf(" ");//placeholder
 }
