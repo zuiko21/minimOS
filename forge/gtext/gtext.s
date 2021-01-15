7f349d8ed8b01f1c16cddecb077cc167d1e5e732
Carlos Santisteban <zuiko21@gmail.com>
2021-01-15 11:11:36 +0100

    stub for planar gtext without SMC

--- forge/gtext/gtext.s
+++ forge/gtext/gtext.s
@@ -1,7 +1,7 @@
 ; print text on arbitrary pixel boundaries
 ; 65(C)02-version
 ; (c) 2020-2021 Carlos J. Santisteban
-; last modified 20210114-1422
+; last modified 20210115-1111
 
 ; assume MAXIMUM 32x32 pixel font, bitmap VRAM layout (might be adapted to planar as well)
 ; supports variable width fonts!
@@ -28,7 +28,7 @@
 f_ptr	.dsb	2			; indirect pointer for font reading
 v_ptr	.dsb	2			; indirect pointer for screen writing
 
-; *** required pointers *** actually parameters to be set by init
+; *** required pointers *** parameters to be set by init or when selecting a font
 
 font	.dsb	2			; font definition start pointer
 wdth	.dsb	2			; font widths pointer
@@ -50,17 +50,20 @@ l_byt	.dsb	1			; last font byte for each scanline (number of bytes minus 1)
 l_msk	.dsb	1			; last mask byte for each scanline (number of bytes minus 1, usually l_byt+1 but not always)
 
 ; *** variables not necessarily in ZP ***
-; actual printing parameters
+; actual printing parameters, to be set every character!
 x_pos	.dsb	2			; 16-bit x-position, fixed-point (5b LSB first, then 8b MSB)
 ; that MUST be equalized so the "low" part is 3b ONLY, incrementing column as needed
 ; systems with "wide" hardware chars (e.g. SIXtation) would take that into account when computing addresses
 y_pos	.dsb	2			; 16-bit y-position, fixed-point (5b LSB first, then 8b MSB)
 char	.dsb	1			; ASCII to be printed
-hght	.dsb	1			; font height
 
-; local variables
+hght	.dsb	1			; font height *** to be set at font change, no big deal on performance
+
+; offset tables are to be generated at init or screen resizing
 off_l	.dsb	64			; LSB of offsets for twice the max scanlines
 off_h	.dsb	64			; MSB of the above
+
+; local variables
 count	.dsb	1			; RAM variable for loops, allowing free use of X
 
 ; ****************************************************************************
@@ -85,7 +88,7 @@ init:
 	RTS						; anything else to do?
 
 ; ********************
-; *** actual stuff *** give or take, worst case is ~15000t
+; *** actual stuff *** give or take, worst case is ~t
 ; ********************
 
 print:
@@ -215,21 +218,55 @@ is_l:
 ; *** now read from VRAM, AND with 'mask' and OR with glyph data at 'scan' [takes up to 3104t]
 v_draw:
 		LDY l_msk			; get last byte for drawing!
+vd_init:
+#ifdef	PLANAR
+; *** some init is needed before entering the blit loop *** TO DO *** TO DO
+; essentially a plane counter, perhaps having previously created a combo index array!
+; %00000fb0 for every plane, to be set before any character
+; loop should use X, the scanline counter may be back to variable as matters little performance-wise
+#endif
 blit:
 ; this is for a bitmapped B/W screen
 ; in case of planar screens, the ORA op will change according to the fg & bg bits
 ; if both are 0, do nothing (skip the ORA, actually)
-; if both are 1, ORA imsk, Y -- which is an INVERTED copy of the ORIGINAL mask, before applying glyph!
-; if only fg is 1, proceed normally
 ; if only bg is 1, ORA iscn, Y -- which is an (INVERTED copy of shifted *scan*) AND mask
+; if only fg is 1, proceed normally
+; if both are 1, ORA imsk, Y -- which is an INVERTED copy of the ORIGINAL mask, before applying glyph!
 			LDA (v_ptr), Y	; get screen data (5)*s*b
 			AND mask, Y		; clear where the glyph goes *** note for 65816 (4)*s*b
+#ifdef	PLANAR
+; let's try something
+			LDA optab, X	; get pointer to appropriate op for that bit combo
+			PHX				; eeeeek! no good for speed
+			TAX
+			JMP (vd_op, X)	; do as appropriate
+; operations pointer table 
+vd_op:
+				.word	vd_plan	; %00 = skip ORA (clears place)
+				.word	vd_inv	; %01 = place inverted scan
+				.word	vd_ora	; %10 = normal operation
+				.word	vd_set	; %11 = set inverted mask
+; alternative operations
+vd_inv:
+			ORA scan, Y		; set inverted glyph bits *** ditto for 65816
+			BRA vd_plan
+vd_set:
+			ORA scan, Y		; set all bits *** ditto for 65816
+			BRA vd_plan
+#endif
 ; *** *** this op is to be changed for planar screens *** ***
+vd_ora:
 			ORA scan, Y		; set glyph pixels *** ditto for 65816 (4)*s*b
-; *** *** ******************************************* *** ***
+vd_plan:
+; *** *** actually asking for Self-Modifying Code...  *** ***
 			STA (v_ptr), Y	; update screen (6+2+3')*s*b
-			DEY
+#ifdef	PLANAR
+; loop for every plane
+			DEX
 			BPL blit
+#endif
+			DEY
+			BPL vd_init		; in case of planar-like init, otherwise same as blit
 ; now prepare for the next scanline! [up to 1439t, including all overhead except mask creation]
 		LDA f_ptr+1			; get font pointer MSB (3)*s
 		CLC					; will jump to next scanline, note planar-like format (2)*s
