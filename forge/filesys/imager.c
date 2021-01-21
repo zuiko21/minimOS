e0300fcd85d6d46811415fc8212798fd8ea1b7d6
Carlos Santisteban <zuiko21@gmail.com>
2021-01-21 19:46:28 +0100

    fixed close in imager.c

--- forge/filesys/imager.c
+++ forge/filesys/imager.c
@@ -1,6 +1,6 @@
 /* minimOS disk imager v0.2       *
  * (c) 2021 Carlos J. Santisteban *
- * last modified 20210121-1426    *
+ * last modified 20210121-1943    *
  */
 
 #include <stdio.h>
@@ -17,73 +17,70 @@ typedef	struct imagen*	PIM;
 
 /* function prototypes */
 int		menu(void);
-PIM		create(long tama);
-PIM		open(char *nom);
-ERR		dir(PIM fs);
-ERR		close(PIM fs, char* nom);
-ERR		insert(PIM fs, char* nom);
-ERR		delete(PIM fs, char* nom);
-ERR		extract(PIM fs, char* nom);
-
-/* global variables */
-	FILE*	im;
-	char*	rd = NULL;
-	long	siz = 0, kb;
+ERR		create(PIM ptr, long tama);
+ERR		open(PIM ptr, char *nom);
+ERR		dir(PIM ptr);
+ERR		close(PIM ptr, char* nom);
+ERR		insert(PIM ptr, char* nom);
+ERR		delete(PIM ptr, char* nom);
+ERR		extract(PIM ptr, char* nom);
 
 /* main loop */
 int main(void) {
-	int		opc, err;
-	char	nom[80];
+	int				opc, err;
+	long			kb;
+	char			nom[80];
 	struct imagen	rd;
 
-	printf("\nminimOS disk image manager v0.1");
+	rd.byte = NULL;			/* object intialisation */
+	rd.size = 0;			/* just in case */
+
+	printf("\nminimOS disk image manager v0.2");
 	printf("\n===============================\n");
 	do {
 		opc=menu();
 		switch(opc) {
 			case 1:			/* create image in RAM */
-				if (rd != NULL) {
+				if (rd.byte != NULL) {
 					printf("*** Error: one image is open ***\n");
 				} else {
 					printf("How many kiB? ");
-					scanf("%ld", &kb);
-					rd = create(kb<<10);	/* multiply by 1024 */
-					if (rd == NULL) {
+					scanf("%ld", &kb);	/* will be multiplied by 1024 */
+					if (create(&rd, kb<<10)) {
 						printf("*** Error: not enough memory ***\n");
 					}
 				}
 				break;
 			case 2:			/* open image from disk */
-				if (rd != NULL) {
+				if (rd.byte != NULL) {
 					printf("*** Error: one image is open ***\n");
 				} else {
 					printf("Image file: ");
 					scanf("%s", nom);
-					rd = open(nom);
-					if (rd == (struct imagen *)-1) {
+					err = open(&rd, nom);
+					if (err == -1) {
 						printf("*** Error: no such file ***\n");
-						rd = NULL;
-					} else if (rd == NULL) {
+					} else if (err == -2) {
 						printf("*** Error: not enough memory ***\n");
-					}
-					if (rd != NULL) {
-						printf("Loaded %ld bytes\n", rd->size);
+					} else {
+						printf("Loaded %ld bytes\n", rd.size);
 					}
 				}
 				break;
 			case 3:			/* display image contents */
-				if (dir(rd))	printf("*** Error: no image in use ***\n");
+				if (dir(&rd))	printf("*** Error: no image in use ***\n");
 				break;
 			case 4:			/* save image to file */
-				if (rd != NULL) {
+				if (rd.byte != NULL) {
 					printf("Save to image file: ");
 					scanf("%s", nom);
-					err = close(rd, nom);
-					if (rd == (struct imagen *)-2) {
+					err = close(&rd, nom);
+					if (err == -3) {
 						printf("*** Error: couldn't save image ***\n");
 					} else {
-						free(rd);
-						rd = NULL;
+						free(rd.byte);
+						rd.byte = NULL;
+						rd.size = 0;
 					}
 				} else {
 					printf("*** Error: no image to save ***\n");
@@ -92,17 +89,17 @@ int main(void) {
 			case 5:			/* insert file into image */
 				printf("File to insert: ");
 				scanf("%s", nom);
-				insert(rd, nom);
+				err = insert(&rd, nom);
 				break;
 			case 6:			/* delete file from image */
 				printf("Delete from image, which file? ");
 				scanf("%s", nom);
-				delete(rd, nom);
+				err = delete(&rd, nom);
 				break;
 			case 7:			/* extract file from image */
 				printf("Extract from image, which file? ");
 				scanf("%s", nom);
-				extract(rd, nom);
+				err = extract(&rd, nom);
 		}
 	} while (opc != 0);
 
@@ -111,7 +108,7 @@ int main(void) {
 
 /* function definitions */
 int menu(void) {
-	int x=-1;
+	int x = -1;
 	
 	printf("\n1.Create image\n");
 	printf("2.Open image\n");
@@ -125,59 +122,75 @@ int menu(void) {
 		printf(">");
 		scanf("%d",&x);
 	}
-	
+
 	return x;
 }
 
-PIM	create(long tama) {
-	PIM	ptr;
+ERR	create(PIM ptr, long tama) {
 
 	ptr->byte = (char*)malloc(tama);	/* allocate RAM */
-	if (ptr->byte == NULL)				return NULL;
+	if (ptr->byte == NULL)	return -2;	/* ** not enough memory ** */
 	ptr->size = tama;
 
-	return	ptr;
+	return	0;
 }
 
-PIM	open(char *nom) {
-	PIM	ptr;
-	FILE*			im;
-	long			tama;
+ERR	open(PIM ptr, char *nom) {
+	FILE*	im;
+	long	tama;
+	ERR		err;
 
 	im = fopen(nom, "rb");
 	if (im == NULL) {
-		return (struct imagen *)-1;		/* ** no file ** */
+		return -1;				/* ** no file ** */
 	}
-	fseek(im, 0, SEEK_END);				/* check file length */
+	fseek(im, 0, SEEK_END);		/* check file length */
 	tama = ftell(im);
 	rewind(im);
-	ptr = create(tama);					/* allocate RAM */
-	if (ptr == NULL) {
+	err = create(ptr, tama);	/* allocate RAM */
+	if (err) {
 		fclose(im);
-		return ptr;						/* ** not enough memory ** */
+		return err;				/* ** not enough memory ** */
 	}
 	fread(ptr->byte, sizeof(char), tama, im);	/* load whole file into RAMdisk */
 	fclose(im);
 
-	return ptr;
-}
-
-ERR				dir(PIM fs) {
 	return 0;
 }
 
-ERR				close(PIM fs, char* nom) {
+ERR	dir(PIM ptr) {
 	return 0;
 }
 
-ERR				insert(PIM fs, char* nom) {
+ERR	close(PIM ptr, char* nom) {
+	FILE*	im=NULL;
+	char	chk[80];
+
+	im = fopen(nom, "r");
+	fclose(im);
+	if (im != NULL) {			/* file exists */
+		printf("-- File exists. Overwrite? (y/n): ");
+		scanf("%s", chk);
+		if ((chk[0]|32) != 'y') {
+			return -3;			/* ** not overwriting ** */
+		}
+	}
+	im = fopen(nom, "wb");
+	if (im == NULL)		return -3;	/* ** couldn't write ** */
+	fwrite(ptr->byte, sizeof(char), ptr->size, im);	/* save whole file into RAMdisk */
+	fclose(im);
+
 	return 0;
 }
 
-ERR				delete(PIM fs, char* nom) {
+ERR	insert(PIM ptr, char* nom) {
 	return 0;
 }
 
-ERR				extract(PIM fs, char* nom) {
+ERR	delete(PIM ptr, char* nom) {
+	return 0;
+}
+
+ERR	extract(PIM ptr, char* nom) {
 	return 0;
 }
