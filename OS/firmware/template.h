f3920650842dd7a692ed3029e12be0fd55ab6e00
Carlos Santisteban <zuiko21@gmail.com>
2021-02-18 12:13:28 +0100

    template.h is nanoBoot savvy

--- OS/firmware/template.h
+++ OS/firmware/template.h
@@ -1,13 +1,9 @@
 ; generic firmware variables for minimOS·65
-; v0.6a5
+; v0.6a6
 ; (c) 2015-2021 Carlos J. Santisteban
-; last modified 20180219-0833
+; last modified 20210218-1212
 
 -sysram:
-#ifndef	LOWRAM
-fw_table	.dsb	API_SIZE, $0	; more efficient usage 171114, NOT available in 128-byte systems
-fw_lastk	.word	0				; address of last installed kernel jump table! new 20180116
-#endif
 fw_isr		.word	0				; ISR vector
 fw_nmi		.word	0				; NMI vector, fortunately checks for integrity
 fw_dbg		.word	0				; BRK (debug) vector, eeeeeeeeeeeek
@@ -17,3 +13,7 @@ himem		.byt	0				; number of available 'kernel-RAM' pages, 0 means 128-byte RAM
 ; should add some high ram and rom shadowing info
 irq_freq	.word	200				; jiffys per second
 old_t1		.word	0				; keep old T1 latch value for FREQ_GEN
+#ifndef	LOWRAM
+fw_table	.dsb	API_SIZE, $0	; more efficient usage 171114, NOT available in 128-byte systems
+fw_lastk	.word	0				; address of last installed kernel jump table! new 20180116
+#endif
