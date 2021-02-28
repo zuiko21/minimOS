1f52b5d3b133d8e2f50bb057e89177f081343891
Carlos Santisteban <zuiko21@gmail.com>
2021-02-28 20:01:56 +0100

    right sprite move and placeholders for the rest

--- apps/pacman/pacman.h
+++ apps/pacman/pacman.h
@@ -1,6 +1,6 @@
 ; variables for PacMan
 ; (c) 2021 Carlos J. Santisteban
-; last modified 20210228-1751
+; last modified 20210228-1829
 
 ; **************************
 ; *** zeropage variables ***
@@ -14,8 +14,8 @@
 ; note new order, where pacman is just a srpite like the ghosts
 sprite_x	.dsb	5, 0	; sprite coordinates (in pixels), array for pacman [0] + ghosts [1...4]
 sprite_y	.dsb	5, 0
-sprite_d	.dsb	5, 0	; sprite direction is 0=right, 1=down, 2=left, 3=up
-sprite_s	.dsb	5, 0	; sprite status is 0=scatter, 1=chase, 2=frightened, 3=eaten (makes no sense for pacman)
+sprite_d	.dsb	5, 0	; sprite direction is 0=right, 2=down, 4=left, 6=up
+sprite_s	.dsb	5, 0	; sprite status is 0=scatter, 2=chase, 4=frightened, 6=eaten (makes no sense for pacman)
 sel_gh		.byt	0		; temporarily selected ghost (index for arrays above), also temporary use
 score		.word	0		; score (a tenth of the original score, thus up to 655350 in the arcade)
 lives		.byt	0		; remaining lives
@@ -26,7 +26,7 @@ draw_s		.byt	0
 
 ; *** these MUST reside in zeropage ***
 map_pt	.word	0			; pointer to descriptor map
-spr_pt	.word	0			; pointer to sprite enry
+spr_pt	.word	0			; pointer to sprite entry
 org_pt	.word	0			; pointer to 'clean', sprite-less screen
 dest_pt	.word	0			; VRAM pointer
 
--- apps/pacman/pacman.s
+++ apps/pacman/pacman.s
@@ -1,7 +1,7 @@
 ; PacMan for Tommy2 breadboard computer!
 ; hopefully adaptable to other 6502 devices
 ; (c) 2021 Carlos J. Santisteban
-; last modified 20210228-1635
+; last modified 20210228-2001
 
 ; can be assembled from this folder
 
@@ -28,12 +28,19 @@ start:
 	STX $FFF0				; turn off Tommy2 display
 
 ; perhaps is time to init data...
+	INX						; gets a zero
+	STX score				; reset score
+	STX score+1
+	LDA #5					; initial lives
+	STA lives
 
+; initial screen setup, will be done every level as well
 	JSR newmap				; reset initial map
 	JSR screen				; draw initial field (and current dots)
 	JSR positions			; reset initial positions
 ; now coordinates are set, draw all ghosts and pacman on screen... *** TBD
 
+; **********************************************************
 ; screen is ready, now play the tune... that started it all!
 	STX sel_gh				; reset cursor (temporary use, X known to be zero **** sure? **** at this point)
 m_loop:
@@ -70,7 +77,7 @@ m_next:
 		INC sel_gh			; advance cursor to next note
 		BNE m_loop
 m_end:
-
+; **********************************************************
 ; music finished, now start the game!
 level:
 
@@ -141,6 +148,7 @@ sc_loop:
 #endif
 		BPL sc_loop			; (usually 3, just 8 times)
 ; now place dots according to current map *** TBD
+	
 	RTS
 
 ; reset initial positions
@@ -153,26 +161,144 @@ ip_loop:
 		BNE ip_loop
 	RTS
 
-; draw one sprite... TBD
+; *** draw one sprite... ***
 draw:
 	LDX draw_d				; check direction
 	JMP (sp_dir, X)			; *** CMOS only *** execute appropriate code
-
 sp_dir:
 	.word	sd_right		; table of pointers for sprite drawing routines
 	.word	sd_down
 	.word	sd_left
 	.word	sd_up
 
-; sample routine for sprite drawing, towards right
+; sample routine for sprite drawing, towards right ***
 sd_right:
-	
+	LDY draw_x
+	INY						; try one pixel to the right
+	TYA
+	LDX draw_y
+	JSR chk_map				; check status of suggested tile
+	BMI sr_abort
+		;***check dot/pill (perhaps in chk_map)
+		INC draw_x			; one pixel to the right
+		LDA draw_x
+		AND #7				; bit within byte
+		BNE nowrap
+; perhaps could clear here the leftmost column
+			LDY org_pt		; if wrapped, advance one byte
+			INY
+			STY org_pt
+			STY dest_pt
+			BNE sr_nb		; page boundary crossing
+				INC org_pt+1
+				INC dest_pt+1
+sr_nb:
+#ifdef	IOSCREEN
+		LDY dest_pt+1		; this needs to be ready always
+		STY $8000
+#endif
+		ASL					; each pixel displacement takes 16 bytes of sprite file
+		ASL
+		ASL
+		ASL
+		CLC
+		ADC spr_pt			; add to selected sprite file base address
+		STA spr_pt
+		BCC sr_now			; check for wrapping in sprite file
+			INC spr_pt+1
+sr_now:
+		LDY #0				; reset sprite byte counter
+sr_loop:
+			LDA (org_pt), Y		; get clean data
+			ORA (spr_pt), Y		; put sprite data on it
+			STA (dest_pt), Y	; and place it on screen
+#ifdef	IOSCREEN
+			LDX dest_pt			; eeeeeeeek must get this pointer
+			STX $8001			; latch low address, high byte was already done
+			STA $8003			; copy data on screen!
+#endif
+			INY					; advance to adjacent byte in both sprite and screen
+			LDA (org_pt), Y		; ditto with this second byte, get clean data
+			ORA (spr_pt), Y		; put sprite data on it
+			STA (dest_pt), Y	; and place it on screen
+#ifdef	IOSCREEN
+			LDX dest_pt			; eeeeeeeek must get this pointer
+			STX $8001			; latch low address, high byte was already done
+			STA $8003			; copy data on screen!
+#endif
+			INY					; prepare for next entry
+			LDA org_pt			; advance screen pointers... backing off a bit as the index increases!
+			CLC
+			ADC #14				; must subtract the two processed entries on sprite file
+			STA org_pt
+			STA dest_pt			; VRAM pointer too
+			BCC sr_nw
+				INC org_pt+1	; page wrapping
+				INC dest_pt+1
+#ifdef	IOSCREEN
+				LDX dest_pt+1
+				STX $8000
+#endif
+			CPY #16				; until sprite file is done
+			BNE sr_loop
+sr_abort:
 	RTS
 ; *** remaining sprite drawing functions TBD ***
 sd_down:
-sd_left:
-sd_up:
+	LDA draw_x
+	LDX draw_y
+	INX						; try one pixel down
+	JSR chk_map				; check status of suggested tile
+	BMI sd_abort
+
+sd_abort:
 	RTS
+
+sd_left:
+	LDY draw_x
+	DEY						; try one pixel to the left
+	TYA
+	LDX draw_y
+	JSR chk_map				; check status of suggested tile
+	BMI sl_abort
+
+sl_abort:
+	RTS
+
+sd_up:
+	LDA draw_x
+	LDX draw_y
+	DEX						; try one pixel up
+	JSR chk_map				; check status of suggested tile
+	BMI sr_abort
+
+sr_abort:
+	RTS
+
+; compute map data from pixel coordinates
+chk_map:
+; input is A=suggested draw_x, X=suggested draw_y
+	LSR
+	LSR
+	TAY						; Y=map column (one each 4 pixels)
+	TXA						; this gets suggesteddraw_y
+	LSR						; will get map row from A (one each 4 pixels)
+	LSR
+	STZ map_pt				; clear temporary variable *** CMOS ***
+	LSR						; each y advances 32 bytes in table, thus divide MSB by 8 (256/8=32)
+	ROR map_pt
+	LSR
+	ROR map_pt
+	LSR
+	ROR map_pt				; this is the LSB
+	STA map_pt+1			; save this MSB
+	LDA #<d_map				; add LSB to offset.low
+	ADC map_pt				; C known to be clear (ROR of a reset value)
+	STA map_pt
+	LDA #>d_map				; MSB too
+	ADC map_pt+1
+	STA map_pt+1			; pointer is ready
+	LDA (map_pt), Y			; map entry for that position
 
 ; ****************************
 ; ****************************
@@ -192,13 +318,11 @@ init_p:
 	.byt	 0,  0,  0,  0,  0	; ghosts initial state (nonsense for pacman)
 
 ; valid X values in current system (+2 offset)
-; 2, 10, 22, 34, 46, (52 the base), 58, 70, 82, 94, 102
-; which +2 become
-; 4, 12, 24, 36, 48, (54), 60, 72, 84, 96, 104
+; 4, 12, 24, 36, 48, (54 for base), 60, 72, 84, 96, 104
 ; which fit into these (half)byte offsets
 ; 0.5, 1.5, 3, 4.5, 6, (6.75), 7.5, 9, 10.5, 12, 13
 ; since the base exit is for rising ghosts only, it might be an special case
-; but some ghosts inside the base jump at X=44+2 and X=60+2 (5.75 & 7.75)
+; but some ghosts inside the base jump at X=46 and X=62 (5.75 & 7.75)
 
 ; not sure if i will use an offset table for the map
 
