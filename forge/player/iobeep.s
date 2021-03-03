1075942d9f7d3d510e1fdd12992001e56ec09c93
Carlos Santisteban <zuiko21@gmail.com>
2021-03-03 18:12:38 +0100

    creating sound effects for pacman

--- forge/player/iobeep.s
+++ forge/player/iobeep.s
@@ -1,6 +1,6 @@
 ; music player for breadboard!
 ; (c) 2021 Carlos J. Santisteban
-; last modified 20210227-0012
+; last modified 20210303-1638
 
 ; *** required variables (not necessarily in ZP) ***
 
@@ -23,6 +23,63 @@ cur		.byt	0			; current score position
 
 	STX $FFF0				; turn off display, just in case
 
+; *** experimntal code ***
+; sweep for pacman eating ghost ** OK
+/*	LDA #0
+	STA cur
+sweep:
+		LDX #8
+		JSR mt_beep
+		DEC cur
+		DEC cur
+		DEC cur
+		DEC cur
+		LDA cur
+		CMP #16
+		BNE sweep
+*/
+; pacman death
+	LDA #100	; initial freq
+	LDY #86		; top freq
+	LDX #36		; length
+	JSR squeak
+	LDA #118	; initial freq
+	LDY #104	; top freq
+	LDX #32		; length
+	JSR squeak
+	LDA #127
+	LDY #111
+	LDX #28
+	JSR squeak
+	LDA #136
+	LDY #117
+	LDX #24
+	JSR squeak
+	LDA #144
+	LDY #124
+	LDX #22
+	JSR squeak
+; last two sweeps ** OK
+	LDA #2
+	STA cur+1	; iteration
+d_rpt:
+	LDA #255
+	STA cur
+send:
+		LDX #10
+		JSR mt_beep
+		LDA cur
+		SEC
+		SBC #24
+		STA cur
+		CMP #15
+		BCS send
+	JSR ms74
+; next iteration
+	DEC cur+1
+	BNE d_rpt
+	JMP end
+; ************************
 	INX						; now it's zero
 	STX cur					; reset cursor (or use STZ)
 loop:
@@ -33,21 +90,7 @@ loop:
 		LDA note, Y			; get note period (10A+20 t) from its array
 		BEQ rest			; if zero, no sound!
 
-; *****************************
-; *** ** beeping routine ** *** inlined
-; *** X = length, A = freq. ***
-; *** tcyc = 10 A + 20      ***
-; *****************************
-mt_beep:
-			TAY				; determines frequency (2)
-			STX $BFF0		; send X's LSB to beeper (4)
-rb_zi:
-				DEY			; count pulse length (y*2)
-				BNE rb_zi	; stay this way for a while (y*3-1)
-			DEX				; toggles even/odd number (2)
-			BNE mt_beep		; new half cycle (3)
-		STX $BFF0			; turn off the beeper!
-; *****************************
+		JSR mt_beep			; call beep! (no longer inlined)
 
 		BEQ next			; go for next note
 
@@ -72,6 +115,59 @@ end:
 	LDA #%01100101			; put dashes on display
 	STA $FFF0
 	BNE end					; *** repeats at the end ***
+
+; support routines
+; *****************************
+; *** ** beeping routine ** *** no longer inlined
+; *** X = length, A = freq. ***
+; *** tcyc = 10 A + 20      ***
+; *****************************
+mt_beep:
+		TAY				; determines frequency (2)
+		STX $BFF0		; send X's LSB to beeper (4)
+rb_zi:
+			DEY			; count pulse length (y*2)
+			BNE rb_zi	; stay this way for a while (y*3-1)
+		DEX				; toggles even/odd number (2)
+		BNE mt_beep		; new half cycle (3)
+	STX $BFF0			; turn off the beeper!
+	RTS
+; *****************************
+
+; ~74 ms delay
+ms74:
+	LDY #198
+delay:
+		INX
+		BNE delay
+		INY
+		BNE delay
+	RTS
+
+; squeak, get higher then lower
+; cur=current, cur+1=initial, cur+2=final, cur+3=length
+squeak:
+	STA cur+1
+	STA cur		; and current
+	STY cur+2
+	STX cur+3
+peak1:
+		LDX cur+3
+		JSR mt_beep
+		DEC cur
+		DEC cur
+		LDA cur
+		CMP cur+2
+		BNE peak1
+peak:
+		LDX cur+3
+		JSR mt_beep
+		INC cur
+		INC cur
+		LDA cur
+		CMP cur+1
+		BNE peak
+	RTS
 
 ; *******************
 ; *** music score ***
