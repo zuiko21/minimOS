4f659a945dd6c8e88e3c3ab2c0a830e4bf1d626b
Carlos Santisteban <zuiko21@users.noreply.github.com>
2021-05-11 12:07:14 +0200

    Update iox-kbd.c

--- hard/peripheral/iox-kbd.c
+++ hard/peripheral/iox-kbd.c
@@ -2,6 +2,7 @@
  * for VIAport2 keyboard
  * includes two EPROMS, one for ASCII codes, one for control signals
  * (c) 2021 Carlos J. Santisteban
+ * THIS CODE GENERATES THE EPROM CONTENTS
  */
 
 #include <stdio.h>
@@ -9,7 +10,7 @@
 int main(void) {
 	FILE*	rom;
 	int		i, j;
-	unsigned char	mat[16][16][16];	/* row, col, modifiers */
+	unsigned char	mat[16][16][16];	/* modifiers, col, row */
 /* Character ROM
  * A0..A3 = row
  * A4..A7 = column
@@ -19,11 +20,20 @@ int main(void) {
  * A11 = ALT
  */
 	
+/* ***********************
+ * *** character EPROM ***
+ * *********************** */
+/* unshifted but CAPS LOCK on */
+	
+	mat[0][0]={  0,  0,  0,  0,  0,  0,  0,'º',  0,  0,  9,  0,'<',' '};
+	
 	rom=fopen("iox-char.bin", "wb");
 	if (rom==NULL) {
 		printf("*** Can't write to character ROM ***\n\n");
 		return -1;
 	}
+	
+	fclose(rom);
 	
 	return 0;
 }
