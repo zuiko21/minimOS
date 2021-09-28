fa2bdf024aa54fff31a6aa2b1b52f85d3013cb38
Carlos Santisteban <zuiko21@gmail.com>
2021-09-28 13:52:23 +0200

    test code for Durango IO

---
+++ forge/test/iotest.s
@@ -0,0 +1,75 @@
+; I/O test of Durango-X (downloadable version)
+; (c) 2021 Carlos J. Santisteban
+; last modified 20210928-1231
+
+; ****************************
+; *** standard definitions ***
+	fw_irq	= $0200
+	fw_nmi	= $0202
+	test	= 0
+	posi	= $FB			; %11111011
+	systmp	= $FC			; %11111100
+	sysptr	= $FD			; %11111101
+	himem	= $FF			; %11111111
+	IO8lh	= $DF80			; will become $DF80
+	IOAen	= $DFA0			; will become $DFA0
+	IOBeep	= $DFB0			; will become $DFB0
+; ****************************
+
+* = $400					; downloadable start address
+
+reset:
+	SEI						; usual 6502 stuff
+	CLD
+	LDX #$FF
+	TXS
+; Durango-X specific stuff
+	STX IOAen				; disable hardware interrupt
+	LDA #$30				; flag init
+	STA IO8lh				; set colour mode
+; clear the rest of the screen, just for aesthetics
+	LDX #$60
+	LDY #0
+	STY sysptr
+	STX sysptr+1
+	LDA #$BB				; pink!
+clear:
+			STA (sysptr), Y
+			INY
+			BNE clear
+		INC sysptr+1
+		BPL clear
+/*
+crap:
+;	STA IOAen, Y
+	STY $DFB0
+	INY
+	CPY #16
+	BNE crap
+	LDY #0
+	BRA crap
+*/
+; **********************
+; *** test  routines ***
+; **********************
+; 5 kHz beep while IRQ LED blinks
+khz:
+	SEC
+	LDA #%01111111			; single blink per cycle (note C is set in test!)
+fi_1:
+		LDY #29
+fi_2:
+			DEY
+			BNE fi_2		; delay 155t (~101 µs, ~5 kHz)
+		INX
+		PHA
+		AND #$0F
+		TAY
+		PLA
+		STA IOAen, Y		; update pattern, more than needed
+		STX IOBeep			; set buzzer output
+		BNE fi_1			; 256 times is ~26 ms
+	ROL						; keep rotating pattern (cycle ~0.23 s)
+	BNE fi_1				; A is NEVER zero
+suite_end:
+
