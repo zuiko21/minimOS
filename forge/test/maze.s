f64a5d7ad4652f4ec89631bd17597ee05d0cea9e
Carlos Santisteban <zuiko21@gmail.com>
2021-10-02 22:29:51 +0200

    4bpp maze test

---
+++ forge/test/maze.s
@@ -0,0 +1,13 @@
+; test of Durango-X video output (colour mode)
+; (c) 2021 Carlos J. Santisteban 
+
+* = $6000					; VRAM start
+
+IO8lh	= $DF80				; will change to $DF80
+
+	LDA #$30				; colour, non-inverted, 24K, interrupts off
+	STA IO8lh
+	BRA $6000				; must lock here as will be exectuted!
+
+; binary data from file, offset 7 as it's a raw binary
+	.bin	*-$6000, 8185, "../../other/data/maze4.sv"
