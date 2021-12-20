5d40ef8a8c0fd8e2927b293c9a5f79f566060fb3
Carlos Santisteban <zuiko21@gmail.com>
2021-12-20 18:30:15 +0100

    invaders sound test

---
+++ forge/test/invaders.s
@@ -0,0 +1,58 @@
+; Space Invaders sound test for Durango
+; (c) 2021 Carlos J. Santisteban
+
+; must play 494, 440, 392 and 370 Hz brief notes
+; that's semicycles of 1555t, 1745t, 1959t and 2076t
+; for 9t loops is ~173, 194, 218 and 231
+; like every 0.6 s?
+
+*		= $400				; standard download
+
+	SEI						; just in case
+play:
+	LDA #173				; B (494 Hz)
+	JSR note
+	JSR delay
+	LDA #194				; A (440 Hz)
+	JSR note
+	JSR delay
+	LDA #218				; G (392 Hz)
+	JSR note
+	JSR delay
+	LDA #231				; Gb (370 Hz)
+	JSR note
+	JSR delay
+	JMP play
+
+note:
+	LDY #15					; number of semicycles
+n_loop:
+		TAX					; get loop value
+hi_loop:
+			STY $DFB0		; beeper output
+			DEX
+			BNE hi_loop
+		DEY					; toggle output
+		TAX					; get loop value again
+lo_loop:
+			STY $DFB0		; beeper output
+			DEX
+			BNE lo_loop
+		DEY					; toggle output
+		BPL n_loop			; good for a few cycles
+	RTS
+
+; 64K * 14t
+delay:
+	LDA #0					; useful to disable output
+	TAX						; reset counters
+	TAY
+d_loop:
+			STA $DFB0
+			STA 0			; some extra delay
+			NOP
+			INX
+			BNE d_loop
+		INY
+		BNE d_loop
+	RTS
