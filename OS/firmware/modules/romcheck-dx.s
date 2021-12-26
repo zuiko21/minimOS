50505901ee0a08fc29113dd1aad7229b9bddc3c7
Carlos Santisteban <zuiko21@gmail.com>
2021-12-27 00:38:43 +0100

    fix romcheck interface

--- OS/firmware/modules/romcheck-dx.s
+++ OS/firmware/modules/romcheck-dx.s
@@ -1,9 +1,19 @@
 ; firmware module for minimOS
 ; Durango-X ROMcheck interface
 ; (c) 2021 Carlos J. Santisteban
-; last modified 20211226-1640
+; last modified 20211227-0037
 
 .(
+	LDX #>ROM_BASE			; begin of test
+#ifndef	DOWNLOAD
+	LDY #0
+#else
+	LDY #$60				; VRAM start
+#endif
+	LDA #>IO_BASE			; page to be skipped (if in ROM)
+	STX st_pg
+	STY af_pg
+	STA skipg
 	JSR chk_sum				; direct firmware call, of course
 	BCC f16ok				; ROM checked out OK...
 		LDA #1				; or turn LED off once, cyclically
