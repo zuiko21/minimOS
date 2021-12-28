ef345bffb175b532f6754f09067172bbc1e79b27
Carlos Santisteban <zuiko21@gmail.com>
2021-12-28 12:26:41 +0100

    fix PASK for RPi, modi not working

--- forge/nanoboot/pask.c
+++ forge/nanoboot/pask.c
@@ -1,6 +1,6 @@
 /* IO9 keyboard emulation              *
  * (c) 2021      Carlos J. Santisteban *
- * last modified 20211228-1205         */
+ * last modified 20211228-1211         */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,7 +21,7 @@
 #define	D7		10
 
 /* Global variables */
-int	ctl=64, alt=0;
+int	ctl=255, alt=0;
 
 /* prototypes */
 void salida(int x);
@@ -53,14 +53,17 @@ int main(void) {
 	pinMode(D7, OUTPUT);
 /* main loop */
 	while(1) {
-		c = getchar_unlocked();
+		c = getchar();
 		if (c==10)	c=13;	/* minimOS CR */
 		if (c==1) {
 			modi();			/* ^A sets modifiers */
 			c=0;
 		}
-		if (c)		salida(c&ctl|alt);
-		ctl=64; alt=0;		/* reset modifiers */
+		if (c) {
+			salida(c&ctl|alt);
+			printf("(%d)", c&ctl|alt);
+			ctl=255; alt=0;		/* reset modifiers */
+		}
 	}
 
 	return 0;
@@ -76,7 +79,7 @@ void salida(int x) {
 	digitalWrite(D5, x&32);
 	digitalWrite(D6, x&64);
 	digitalWrite(D7, x&128);
-	wait(20);					/* 20 ms wait should be enough */
+	delay(20);					/* 20 ms wait should be enough */
 	digitalWrite(D0, 0);		/* reset port to allow repeated keys */
 	digitalWrite(D1, 0);
 	digitalWrite(D2, 0);
@@ -85,17 +88,17 @@ void salida(int x) {
 	digitalWrite(D5, 0);
 	digitalWrite(D6, 0);
 	digitalWrite(D7, 0);
-	wait(10);
+	delay(10);
 }
 
 void modi() {
-	int n;
+	int n=0;
 
 	printf("\n\n[MODIFIERS]1=CTL, 2=ALT, 3=both: ");
-	while (!n)	n=getchar_unlocked();
-	if (n=='1')		ctl=0;	/* control key */
-	if (n=='2')		alt=128;	/* alt key */
-	if (n=='3')		ctl=0; alt=128;	/* both keys */
-	printf("%d\n\n", n);
+	while (!n)	n=getchar();
+	if (n=='1')		ctl=159; alt=0;		/* control key */
+	if (n=='2')		ctl=255; alt=128;	/* alt key */
+	if (n=='3')		ctl=159; alt=128;	/* both keys */
+	printf("%c\n\n", n);
 }
 
