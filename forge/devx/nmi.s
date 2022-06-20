41a1a822e2c1889a2937769401ff9a083a4a5b08
Carlos Santisteban <zuiko21@gmail.com>
2022-06-20 16:30:28 +0200

    fix and improve devX NMI handler

--- forge/devx/nmi.s
+++ forge/devx/nmi.s
@@ -1,7 +1,12 @@
 ; devX cartridge NMI handler
 ; 128K RAM + 64K ROM
 ; (c) 2022 Carlos J. Santisteban
-; last modified 20220620-0758
+; last modified 20220620-1624
+
+#include "../../OS/macros.h"
+#include "../../OS/zeropage.h"
+
+#define	DEBOUNCE	_DEBOUNCE
 
 ; *** I/O addresses ***
 	IO8attr	= $DF80			; resolution settings as always (D7=HIRES, D6=INVERT, D5-4=SCREEN, D3=RGB)
@@ -9,18 +14,23 @@
 	IOCbank	= $DFFF			; bankswitching register (D7=!ROM, D5-4=high bank $C000-$FFFF (+4), D2-0=low bank $8000-$BFFF
 
 ; *** variables ***
-	IOCold	= $0100			; storage for original bankswitching status *** KLUDGE
-;	nmi_deb	= place to store a magic number ($65) to prevent NMI-button bounce
+	nmi_deb	= $0100			; place to store a magic number ($65) to prevent NMI-button bounce
+	IOCold	= $0101			; storage for original bankswitching status *** KLUDGE
 
 ; *** parameters needed ***
-;	oldat	= original gameplay screen mode (usually $38)
-;	newat	= debugger screen mode (usually $B0)
+	oldat	= sys_sp		; address of original gameplay screen mode (usually stores $38), ununsed otherwise
+	newat	= $B0			; debugger screen mode
 
 ; *******************
 ; *** NMI handler ***
 ; *******************
+
+	*	= $C000				; placeholder address
+
 dx_nmi:
-; usual status save *** common NMI handler in all ROM banks ***
+; *******************************************
+; *** common NMI handler in all ROM banks *** over $C000
+; usual status saving
 	PHA						; easiest way to keep original stack frame
 #ifdef	DEBOUNCE
 	LDA #$65				; look for magic constant in memory
@@ -31,34 +41,34 @@ dx_nmi:
 do_nmi:
 	STA nmi_deb				; this will prevent calling another NMI on top of this one
 #endif
-	_PHX
-	_PHY					; continue saving status
-
-; save extended status *** TBD
-
 ; EEEEEEK! must store bankswitching register, try using an *unused* stack address in order to retain the standard stack frame
 	LDA IOCbank				; switch banks...
 	STA IOCold				; ...hopefully $0100 is not used... EEEEK
-	AND #%10001000			; ...to zero (may use ORA afterwards)
+	AND #%00001000			; ...to lowest of both ROM[4] and RAM[0] (may use ORA afterwards)
+;	AND #%11111000			; use this instead if NMI code is present in all (or current) hi-banks
 	STA IOCbank
 ; *** code afterwards doesn't need to be in all ROM banks ***
+; ***********************************************************
+
+dx_cont:
+	_PHX
+	_PHY					; continue saving status
+
+; as this will save the WHOLE ZP, no need for saving extended status!
 
 ; *** make a full memory dump on bankswitched RAM ***
 ; first goes specific zeropage code, won't need any pointers
-	LDX #0					; reset index (will actually waste a few cycles for extended status)
+	LDX #0					; reset index (will include extended status)
 zp_save:
 		LDA 0, X
 		STA $8000, X		; copy ZP at low-bank bottom
 		INX
 		BNE zp_save
 
-; may store SP at *stored* sys_sp ($80FF), really needed?
-	TSX
-	STX $80FF				; copy of SP, must retrieve that upon return
-
+; no actual need to store SP
 ; we may dispose of actual ZP freely now
 	LDX #1					; first page for source pointer
-	STX sys_sp				; unusual location!
+	STX sys_sp				; unusual location! won't disturb oldat
 	LDX #$81				; first page for destination, will remain in register
 hi_blk:
 		STX sysptr+1		; use savvy interrupt pointer
@@ -84,9 +94,8 @@ saved:
 
 ; store old (gameplay) and set new (debugging) video mode
 	LDA IO8attr				; get current mode
-	STA oldat				; store for resolution switcher!
-	LDA #$B0				; new debug screen mode (usually HIRES)
-	STA newat				; store to be retrieved
+	STA oldat				; store for resolution switcher! sys_sp no longer in use
+	LDA #newat				; new debug screen mode (usually HIRES)
 	STA IO8attr				; and set new mode for debugging
 
 ; ***********************
@@ -95,11 +104,11 @@ saved:
 ; may initialise monitor *** TBD
 db_loop:
 		JSR switch			; try to display top half of the screen in colour, bottom in HIRES
-		JSR debug			; invoke (on inline) miniMoDA or similar monitor
+		JSR debug			; invoke (on inline) miniMoDA or similar monitor (hopefully less than 11.7 ms)
 		BCC db_loop			; debug until returns with C set (for instance)
 
 	LDA oldat				; restore old video mode (firmware tweak will be undone by memory restore)
-	STA IO8attr
+	STA IO8attr				; sys_sp is now free for the loops
 
 ; ******************************************
 ; *** exit debugger, restore full status ***
@@ -108,11 +117,12 @@ db_loop:
 	LDX #1					; first page for now DEST pointer, skipping ZP
 	STX sys_sp				; unusual location!
 	LDX #$81				; first page for SOURCE, will remain in register
+	LDY #0					; byte index, always zero
+	STY sysptr				; complete pointers
+	STY systmp				; note unusual DESTINATION address location
+	STY IOCbank				; EEEEEEEEEEK
 hi_rst:
-		STX sysptr+1		; use savvy interrupt pointer
-		LDY #0				; byte index, always zero
-		STY sysptr			; complete pointers
-		STY systmp			; note unusual DESTINATION address location
+		STX sysptr+1		; update savvy interrupt pointer
 mem_rst:
 			LDA (sysptr), Y	; copy one byte FROM bankswitched RAM
 			STA (systmp), Y
@@ -128,28 +138,29 @@ mem_rst:
 		BNE hi_rst			; no need for BRA
 restored:
 
-; may retrieve SP from *stored* sys_sp ($80FF)
-	LDX $80FF				; copy of SP, must retrieve that upon return
-	TXS						; does this make ANY sense?
-
 ; finally goes specific zeropage code, won't need any pointers
-	LDX #0					; reset index (will actually waste a few cycles for extended status)
+	LDX #0					; reset index
+	STX IOCbank				; EEEEEEEEK
 zp_rst:
 		LDA $8000, X		; copy from low-bank bottom to ZP
 		STA 0, X
 		INX
 		BNE zp_rst
 
-; restore extended state *** TBD
-
 ; memory is restored, now the registers
-	LDA IOCold				; retreive old banks...
-	STA IOCbank
-; *** this must be at a common address in all banks ***
 	_PLY					; usual end of restore sequence
 	_PLX
+	LDA IOCold				; retrieve old banks...
+	STA IOCbank
+
+dx_exit:
+; *****************************************************
+; *** this must be at a common address in all banks ***
 #ifdef	DEBOUNCE
 	INC nmi_deb				; ugly, but will allow next NMI invocation, may use STZ as well
 #endif
-	_PLA
+	PLA
 	RTI
+; *** end of common NMI handler ***
+; *********************************
+dx_end:
--- forge/devx/switcher.s
+++ forge/devx/switcher.s
@@ -1,6 +1,6 @@
 ; split-screen resolution switcher for devX development suite
 ; (c) 2022 Carlos J. Santisteban
-; last modified 20220619-1804
+; last modified 20220620-1549
 
 
 ; *** I/O addresses ***
@@ -8,8 +8,9 @@
 	IO8sync = $DF88			; blanking signals (D7=LINE, D6=FRAME)
 
 ; *** parameters needed ***
-;	oldat	= original gameplay screen mode (usually $38)
-;	newat	= debugger screen mode (usually $B0)
+;	oldat	= address of original gameplay screen mode (usually stores $38)
+	newat	= $B0			; debugger screen mode
+	raster	= 127			; number of rasters displayed in original mode... minus one?
 
 ; routine for switching resolution on the top half of the screen
 switch:
@@ -23,7 +24,7 @@ wait:
 		BVS wait
 ; a new frame is about to be displayed, thus count 128 raster lines (half the screen height)
 ; *** not sure if the BMI/BPL sequence below does lose one raster, but will switch faster
-	LDX #127				; number of lines to be displayed in original mode***
+	LDX #raster				; number of lines to be displayed in original mode***
 blank:
 			BIT IO8sync		; wait for the end of H-blanking, if still on it
 			BMI blank
@@ -33,6 +34,6 @@ line:
 		DEX					; one less line to go
 		BNE blank
 ; switch ASAP to HIRES for the debugger
-	LDA newat				; set debugger screen mode
+	LDA #newat				; set debugger screen mode
 	STA IO8attr				; hope to return on time (<11.7 ms)
 	RTS
