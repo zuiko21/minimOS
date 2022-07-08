aad8e4be8b4c0dda2b58528e4964ec59db181743
emiliollbb <emiliollbb@gmail.com>
2022-07-08 14:14:17 +0200

    Durango demos. Add delay to filler

--- forge/test/filler.s
+++ forge/test/filler.s
@@ -1,4 +1,4 @@
-*=$FF00
+*=$C000
 
 begin:
     ; Set video mode
@@ -6,50 +6,61 @@ begin:
     STA $df80
 
     ; Store at $10 video memory pointer
-    LDA #$60	; *** this is done at loop3, thus not needed here
+    LDA #$60
     STA $11
-    LDA #$00	; *** may use Y instead, so it will be reset as well
+    LDA #$00
     STA $10 ; Current video memory position
 
     ; Store at $12 current color
     LDA #$11
-    STA $12		; *** no actual need as modified code won't affect A
+    STA $12
 
 loop3:
     ; Init memory position
-    LDA #$60	; *** if using X instead, will keep A intact, and LDX could be before loop3 as will not change, but not really worth
+    LDA #$60
     STA $11
 
 loop2:
     ; Load color into accumulator
-    LDA $12		; *** no need if using X above
+    LDA $12
 
     ; Iterate over less significative memory address
-    LDY #$00	; *** the loop below exits when zero, no need for this as long as it's reset when setting the pointer
+    LDY #$00
 loop:
-    STA ($10), Y	; *** MAIN LOOP, takes 256*11-1 clocks, done 32 times is ~90 kt or ~59 ms (about 3 fields)
+    STA ($10), Y
     INY
     BNE loop
-; *** remaining code adds little overhead as will be executed 0.4% of the time
+
     ; Iterate over more significative memory address
-    LDA $11 ; Increment memory pointer Hi address using accumulator *** can be simplified, see below
+    LDA $11 ; Increment memory pointer Hi address using accumulator
     CLC
     ADC #$1
     STA $11
     CMP #$80; Compare with end memory position
-    BNE loop2	; *** BPL is best, CMP is not needed and neither is A, as INC $11 will suffice!
+    BNE loop2
 
-    LDA $12		; *** not needed if using modifications
+    LDA $12
     CLC
     ADC #$11
-    CMP #$10	; *** when it wraps, Carry is set, thus remove CMP and use BCC instead
-    BNE store	; *** alternatively, branch to loop3
+    CMP #$10
+    BNE store
     LDA #$00
 
 store:
-    STA $12		; *** not really needed
+    STA $12
 
-    JMP loop3	; *** could use BRA (CMOS) or even BCS/BEQ (as A was reset)
+
+    LDX #$20
+wait_vsync_end:
+    BIT $DF88
+    BVS wait_vsync_end
+wait_vsync_begin:
+    BIT $DF88
+    BVC wait_vsync_begin   
+    DEX
+    BNE wait_vsync_end
+
+    JMP loop3
 
     .dsb    $fffa-*, $ff    ; filling
 
