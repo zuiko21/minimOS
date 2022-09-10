4c9627819d912dc935ea37e98f45b63118614134
Carlos Santisteban <zuiko21@gmail.com>
2022-09-10 22:06:36 +0200

    first try of pacman intro anim

--- apps/pacman/intro.s
+++ apps/pacman/intro.s
@@ -1,6 +1,6 @@
 ; PacMan intro
 ; (c) 2022 Carlos J. Santisteban
-; last modified 20220910-1859
+; last modified 20220910-2205
 
 #include "../../OS/macros.h"
 
@@ -9,12 +9,14 @@ IO8attr	= $DF80
 IO8sync	= $DF88
 IOAen	= $DFA0
 screen3	= $6000
-ban_pos	= $6D00				; line 52 looks nicer
+ban_pos	= $6CC0				; line 51 looks nicer, and makes animation simpler!
 ctl_pos	= $7440				; bottom of the screen
+sp_buf	= $1000				; sprite buffer
 
 ; *** zeropage usage *** placeholder
 orig	= $F0
 dest	= $F2
+tmp		= $F4
 rle_src	= orig				; pointer to compressed source
 rle_ptr	= dest				; pointer to screen output
 
@@ -59,7 +61,41 @@ cl_l:
 	LDA #14					; ~3 sec delay
 	JSR delay
 ; pacman animation!
-
+	LDX #>sp_buf
+	LDY #<sp_buf
+	STY dest				; this will decompress into free RAM
+	STX dest+1
+	LDX #>anim
+	LDY #<anim
+	STY orig				; set compressed data source
+	STX orig+1
+	JSR rle					; decompress!
+	LDA #$D1				; byte LSB to initial pacman position
+	STA tmp
+; the fun begins!
+an_loop:
+		LDA #$6C			; position MSB
+		STA dest+1
+		LDA tmp				; current LSB
+		STA dest
+		AND #7				; mod 8
+		TAX
+		LDA af_ind, X		; get frame index (MSB of sprite file)
+		STA orig+1
+		STZ orig
+		JSR frame			; draw!
+		INC tmp				; next position
+		BNE an_loop
+; decompress banner again
+	LDX #>ban_pos
+	LDY #<ban_pos
+	STY dest				; set banner screen position
+	STX dest+1
+	LDX #>banner
+	LDY #<banner
+	STY orig				; set compressed data source
+	STX orig+1
+	JSR rle					; decompress!
 ; decompress controls
 	LDX #>ctl_pos
 	LDY #<ctl_pos
@@ -70,7 +106,6 @@ cl_l:
 	STY orig				; set compressed data source
 	STX orig+1
 	JSR rle					; decompress!
-	
 
 end: JMP end
 
@@ -86,6 +121,41 @@ delay:
 		BNE delay
 	RTS
 
+; * draw animation frame 22x20 *
+; set orig and dest (with byte offset) as desired
+frame:
+	LDX #20					; raster counter
+fr_ras:
+		LDY #10				; max offset for 22-pix wide *** must be less if close to the edge
+fr_loop:
+			LDA (orig), Y	; get sprite data
+			STA (dest), Y	; put on screen
+			DEY
+			BPL fr_loop
+		LDA orig			; add 11 to orig
+		CLC
+		ADC #11
+		STA orig
+;		BCC o_now			; manage possible carry *** not possible if frames (220-byte) are page-aligned
+;			INC orig+1
+;o_now:
+		LDA dest			; add 64 to dest
+		CLC
+		ADC #64
+		STA dest
+		BCC d_now			; manage possible carry
+			INC dest+1
+d_now:
+		DEX					; next raster
+		BNE fr_ras
+	RTS
+
+; **************************
+; *** animation sequence *** get MSB from here (index = byte offset % 8)
+; **************************
+af_ind:
+	.byt	$13, $10, $11, $12, $11, $10, $13, $14
+
 ; **********************
 ; *** included files ***
 ; **********************
@@ -97,3 +167,6 @@ banner:
 
 controls:
 	.bin	0, 0, "../../other/data/controls.rle"
+
+anim:
+	.bin	0, 0, "../../other/data/ipac.rle"
