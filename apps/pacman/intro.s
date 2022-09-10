1f69f8c1a0d72695a4e8bd357f34713b2847f6e3
Carlos Santisteban <zuiko21@gmail.com>
2022-09-10 18:56:08 +0200

    update intro

--- apps/pacman/intro.s
+++ apps/pacman/intro.s
@@ -1,6 +1,6 @@
 ; PacMan intro
 ; (c) 2022 Carlos J. Santisteban
-; last modified 20220910-1706
+; last modified 20220910-1829
 
 #include "../../OS/macros.h"
 
@@ -10,6 +10,7 @@ IO8sync	= $DF88
 IOAen	= $DFA0
 screen3	= $6000
 ban_pos	= $6D00				; line 52 looks nicer
+ctl_pos	= $7440				; bottom of the screen
 
 ; *** zeropage usage *** placeholder
 orig	= $F0
@@ -55,6 +56,27 @@ cl_l:
 	STY orig				; set compressed data source
 	STX orig+1
 	JSR rle					; decompress!
+; some delay
+	LDA #15
+delay:
+				INX
+				BNE delay
+			INY
+			BNE delay
+		DEC
+		BNE delay
+
+; decompress controls
+	LDX #>ctl_pos
+	LDY #<ctl_pos
+	STY dest				; set banner screen position
+	STX dest+1
+	LDX #>controls
+	LDY #<controls
+	STY orig				; set compressed data source
+	STX orig+1
+	JSR rle					; decompress!
+	
 
 end: JMP end
 
@@ -66,3 +88,6 @@ rle:
 
 banner:
 	.bin	0, 0, "../../other/data/title_c.rle"
+
+controls:
+	.bin	0, 0, "../../other/data/controls.rle"
