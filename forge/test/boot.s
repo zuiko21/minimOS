debed35b1b1adfc23489c8f51f027dcbac40edee
Carlos Santisteban <zuiko21@gmail.com>
2022-11-02 17:28:45 +0100

    add multiboot test

---
+++ forge/test/boot.s
@@ -0,0 +1,16 @@
+; bootloader test
+; *** must be signed above $C000 but BELOW $FF80 ***
+#define	MULTIBOOT
+
+; first ROM
+*	= $8000
+
+#include "../../apps/pacman/intro.s"
+
+	.dsb	$C000-*, $FF	; filling until next bank
+
+#include "durango-test.s"
+
+	.dsb	$FF80-*, $FF	; filling until boot ROM
+
+#include "../bootloader.s"
