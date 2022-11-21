18ce5ee92efbeb7f75a8d6ae9955296c81b50c81
Carlos Santisteban <zuiko21@gmail.com>
2022-11-21 19:18:53 +0100

    measure & time HEX2BCD code

--- forge/div10aj.s
+++ forge/div10aj.s
@@ -1,4 +1,6 @@
-; Convert an 16 bit binary value to BCD
+; from 6502.org/source/integers/hex2dec-more.htm
+
+; Convert a 16 bit binary value to BCD
 ;
 ; This function converts a 16 bit binary value into a 24 bit BCD. It
 ; works by transferring one bit a time from the source and adding it
@@ -6,39 +8,58 @@
 ; arithmetic is being done in BCD the result is a binary to decimal
 ; conversion. All conversions take 915 clock cycles.
 ;
-; See BINBCD8 for more details of its operation.
-;
 ; Andrew Jacobs, 28-Feb-2004
 
-		.ORG $0200
-
-BINBCD16:	SED		; Switch to decimal mode
-		LDA #0		; Ensure the result is clear
-		STA BCD+0
-		STA BCD+1
-		STA BCD+2
-		LDX #16		; The number of source bits
-
-CNVBIT:		ASL BIN+0	; Shift out one bit
+; will take 79b, ~0.583 ms in Durango-X
+BINBCD16:
+		SED			; Switch to decimal mode (2)
+;		LDA #0		; Ensure the result is clear (3*3)
+		STZ BCD+0
+		STZ BCD+1
+		STZ BCD+2
+		LDX #16		; The number of source bits (2)
+CNVBIT:				; whole loop is 671t in NMOS, 719 in CMOS (assuming ZP)
+		ASL BIN+0	; Shift out one bit (16*5*2)
 		ROL BIN+1
-		LDA BCD+0	; And add into result
+		LDA BCD+0	; And add into result (16*3*3+16)
 		ADC BCD+0
 		STA BCD+0
-		LDA BCD+1	; propagating any carry
+		LDA BCD+1	; propagating any carry (16*3*3+16)
 		ADC BCD+1
 		STA BCD+1
-		LDA BCD+2	; ... thru whole result
+		LDA BCD+2	; ... thru whole result (16*3*3+16)
 		ADC BCD+2
 		STA BCD+2
-		DEX		; And repeat for next bit
-		BNE CNVBIT
-		CLD		; Back to binary
+		DEX			; And repeat for next bit (16*2)
+		BNE CNVBIT	; (16*3-1)
+		CLD			; Back to binary (2)
+; above code is 37b, 734t (+14 if data not in ZP)
+; string-generating code below is 42b, 161t (assuming all ZP but str)
+; *** create string, for uniformity sake ***
+		LDX #0		; reset index (2+3)
+		LDA BCD+2
+		JSR prndec	; only LSN is printed (6+18)
+		LDA BCD+1
+		JSR prnbcd	; first MSN, then LSN (3+56)
+		LDA BCD
+		JSR prnbcd	; last one (needed for the terminator) (3+56)
+		LDA #0
+		STA str, X	; terminate string (2+6+6)
+		RTS
 
-		BRK		; All Done.
+prnbcd:				; all this takes 38+18=56t
+		TAY			; save value for LSN (make sure Y is intact!)
+		LSR
+		LSR
+		LSR
+		LSR			; MSN only, over 16
+		JSR prndec	; print this number 0-9
+		TYA			; retrieve value
+		AND #15		; LSN only
 
-; A test value to be converted
-
-		.ORG $0300
-
-BIN		.DW  12345
-BCD		.DS  3
+prndec:				; all this takes 18t
+		CLC
+		ADC #'0'	; BCD to ASCII
+		STA str, X	; place char
+		INX			; next
+		RTS
--- forge/div10be.s
+++ forge/div10be.s
@@ -4,37 +4,39 @@
 ; output = mod10 (.w?)
 ; string = message
 
+; assume value & mod10 in ZP, string in regular RAM
+; 73b, 847t PER DIVISION ~0.55 ms
 divide:
 ; intitialise reminder to zero
-	STZ mod10
+	STZ mod10				; (10 in CMOS per division)
 	STZ mod10+1
 	CLC
 
 	LDX #16
 divloop:
 ; rotate quotient and reminder
-		ROL value
+		ROL value			; (16* 5*4)
 		ROL value+1
 		ROL mod10
 		ROL mod10+1
 
 ; AY = dividend-divisor
-		SEC
+		SEC					; (16* (2+3+2+2+3+2))
 		LDA mod10
 		SBC #10
 		TAY					; save low byte
 		LDA mod10+1
 		SBC #0
-		BCC ignore_result	; branch if dividend < divisor
+		BCC ignore_result	; branch if dividend < divisor (either 16* 3 or 16* (2+3+3)) 
 			STY mod10
 			STA mod10+1
 ignore_result:
-		DEX
+		DEX					; (16* (2+3)-1)
 		BNE divloop
-	ROL value				; shift in the last bit of quotient
+	ROL value				; shift in the last bit of quotient (2*5)
 	ROL value+1
 
-lda mod10
+	LDA mod10				; (3+2+2+6+*+3+3+3)
 	CLC
 	ADC #'0'
 	; print...
@@ -45,20 +47,22 @@ lda mod10
 	ORA value+1
 	BNE divide
 ; *** end, print string ***
+	RTS
+
 
 ; *** routines ***
-; add char in A to beginning of string
+; add char in A to beginning of string (20+n*31-1) ~96t
 push_char:
 	PHA						; first char onto stack
 	LDY #0
 char_loop:
 		LDX message, Y
 		PLA
-		STA message, Y			; pull char and add it to the string
+		STA message, Y		; pull char and add it to the string
 		INY
 		TXA
-		PHA						; push char from string onto stack
+		PHA					; push char from string onto stack
 		BNE char_loop
 	PLA
-	STA message, Y				; pull NULL from stack and terminate string
+	STA message, Y			; pull NULL from stack and terminate string
 	RTS
