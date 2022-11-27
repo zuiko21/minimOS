b3ea76201fbd642f66a06d040f361ab9a73da45f
Carlos Santisteban <zuiko21@gmail.com>
2022-11-27 16:06:19 +0100

    add some keys in map

--- OS/firmware/modules/durango-5x8key.s
+++ OS/firmware/modules/durango-5x8key.s
@@ -1,7 +1,7 @@
 ; Durango-X 5x8 keyboard driver
-; v0.1a1
+; v0.1a2
 ; (c) 2022 Carlos J. Santisteban
-; last modified 20221028-1957
+; last modified 20221127-1606
 
 ; usual definitions
 #ifndef	KEYBDRV
@@ -23,7 +23,7 @@ kb_rcnt	= $020F				; repeat counter
 ; *** ISR to be called via JSR (will return)
 kbd_isr:
 	LDX kb_type
-	JMP (kbd_drv, X)
+	JMP (kbd_drv, X)		; CMOS only
 
 ; *** drivers pointer list ***
 kdb_drv:
@@ -53,36 +53,35 @@ no_cs:
 	LDA IO9m5x8				; and read it
 	ASL						; only d7 is interesting (SYMBOL SHIFT)
 	ROR kb_mod				; insert SYMBOL bit at d7
-	LDY #4					; prepare to scan backwards
+	LDY #5					; prepare to scan backwards (note indices are 1...5)
 col_loop:
-		LDX #7				; row loop
-		LDA col_bit, Y		; get bit position for column
+		LDA col_bit-1, Y	; get bit position for column, note offset
 		STA IO9m5x8			; select column
 		LDA IO9m5x8			; and read it
 ;			STZ IO9m5x8		; deselect all, not necessary but lower power
-		AND k_mask, Y		; discard modifier bits
+		AND k_mask-1, Y		; discard modifier bits, note offset
 		BEQ kb_skip			; no keys from this column
+		LDX #7				; row loop
 row_loop:
 			ASL				; d7 goes first
 			BCS key_pr		; detected keypress!
 			DEX
 			BPL row_loop	; all 8 rows
 		DEY					; next column
-		BPL col_loop
-; if arrived here, no keys (beside SHIFT) were pressed
-	LDA #$FF				; invalid scancode
-	STA kb_scan
+		BNE col_loop
+; if arrived here, no keys (beside modifiers) were pressed
+	STY kb_scan				; Y is zero, which is now an invalid scancode
 	LDA #DELAY				; reset key repeat
 	STA kb_rcnt
 no_key:
-	STZ kb_asc				; no key pressed
+	STZ kb_asc				; no key pressed, CMOS only
 	RTS
 ; otherwise, a key was detected
 key_pr:
-	TYA						; get column index
+	TYA						; get column index (1...5)
 	ASL
 	ASL
-	ASL						; times 8
+	ASL						; times 8 (8...40)
 	STX kb_asc				; TEMPORARY STORAGE
 	ORA kb_asc				; ··YYYXXX
 	BIT kb_ctl				; is control-mode enabled?
@@ -98,10 +97,10 @@ key_pr:
 diff_k:
 	STA kb_scan				; in any case, update last scancode as new keystroke
 	TAX						; use scancode as index
-	LDA kb_map, X			; get ASCII from layout
-	CMP #$FF				; invalid ASCII, this will change into CONTROL mode
+	LDA kb_map-8, X			; get ASCII from layout, note offset
+	CMP #$FF				; invalid ASCII, this will change into CONTROL mode *** check
 	BNE no_ctl
-		STA kb_ctl			; set d7
+		STA kb_ctl			; set d7 *** beware of above
 		LDA #0				; no ASCII for now
 no_ctl:
 	STA kb_asc				; store detected ASCII
@@ -109,8 +108,8 @@ no_ctl:
 ; if arrived here, a key was pressed while in CONTROL mode (will not repeat)
 ctl_key:
 	TAX						; use scancode as index
-	LDA ctl_map, X			; get ASCII from CONTROL-mode layout
-	CMP #$FF				; invalid ASCII, this will change into CONTROL mode
+	LDA ctl_map-8, X		; get ASCII from CONTROL-mode layout, note offset
+	CMP #$FF				; invalid ASCII, this will stay into CONTROL mode *** check
 	BNE no_ctl
 		STA kb_ctl			; keep d7 set
 		LDA #0				; no ASCII for now
@@ -120,16 +119,47 @@ ctl_key:
 ; *** data tables ***
 ; *******************
 
-; bit positions for every column
+
+
+; *** standard keymap, first 8 bytes removed ***
+kb_map:
+; unshifted keys (d7d6=00)
+	.asc	"1qa0p", 0, $D, ' '		; column 1, note SHIFT disabled (scan = 8...$F)
+	.asc	"2ws9ozl", 0			; column 2, note ALT disabled (scan = $10...$17)
+	.asc	"3ed8ixkm"				; column 3 (scan = $18...$1F)
+	.asc	"4rf7ucjn"				; column 4 (scan = $20...$27)
+	.asc	"5tg6yvhb"				; column 5 (scan = $28...$2F)
+; * note 24-byte gap *
+; bit positions for every column (may place in gaps)
 col_bit:
 	.byt	1, 2, 4, 8, 16
-
 ; valid row bits minus shift keys
 k_mask:
 	.byt	%11011111, %01111111, %11111111, %11111111, %11111111
+; * filling after gap *
+	.dsb	14, 0
+; SHIFTed keys (d6=1)
+	.asc	$1B, "qa", 8, 'p', 0, $D, ' '	; column 1, note SHIFT disabled (scan = $48...$4F)
+	.asc	9, "WS", $FF, "OZL", 0			; column 2, note ALT disabled (scan = $50...$57)
+	.asc	$F, "ED", 6, "IXKM"				; column 3 (scan = $58...$5F)
+	.asc	$E, "RF", $B, "UCJN"			; column 4 (scan = $60...$67)
+	.asc	2, "TG", $A, "YVHB"				; column 5 (scan = $68...$6F)
+; note 24-byte gap
+	.dsb	24, 0
+; ALTed keys (d7=1)********** TBD
+	.asc	"1qa0p", 0, $0D, ' '	; column 1, note SHIFT disabled (scan = $88...$8F)
+	.asc	"2ws9ozl", 0			; column 2, note ALT disabled (scan = $90...$97)
+	.asc	"3ed8ixkm"				; column 3 (scan = $98...$9F)
+	.asc	"4rf7ucjn"				; column 4 (scan = $A0...$A7)
+	.asc	"5tg6yvhb"				; column 5 (scan = $A8...$AF)
+; note 24-byte gap
+	.dsb	24, 0
+; SHIFT+ALT (d7d6=11)
+	.asc	"1qa0p", 0, $0D, ' '	; column 1, note SHIFT disabled (scan = $C8...$CF)
+	.asc	"2ws9ozl", 0			; column 2, note ALT disabled (scan = $D0...$D7)
+	.asc	"3ed8ixkm"				; column 3 (scan = $D8...$DF)
+	.asc	"4rf7ucjn"				; column 4 (scan = $E0...$E7)
+	.asc	"5tg6yvhb"				; column 5 (scan = $E8...$EF)
 
-; standard keymap *** TBD
-kb_map:
-
-; control mode keymap *** TBD
+; *** control mode keymap, first 8 bytes removed *** TBD
 ctl_map:
