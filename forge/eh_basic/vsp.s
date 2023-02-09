37228e040dd7bcd49fc3464ca44b522e45fb4c8b
Carlos Santisteban <zuiko21@gmail.com>
2023-02-09 23:11:00 +0100

    temporary SAVE implementation

--- forge/eh_basic/vsp.s
+++ forge/eh_basic/vsp.s
@@ -1,8 +1,13 @@
 ; Virtual Serial Port driver module for EhBASIC (under perdita)
 ; (c) 2023 Carlos J. Santisteban
-; last modified 20230209-0008
+; last modified 20230209-2258
 
 #echo "Using Virtual Serial Port for LOAD/SAVE"
+
+#define	PSV_FOPEN	$11
+#define	PSV_FREAD	$12
+#define	PSV_FWRITE	$13
+#define	PSV_FCLOSE	$1F
 
 ; *** redefine placeholders for EhBASIC LOAD/SAVE routines ***
 -aux_in:					; *** device input (MUST restore devices upon EOF) ***
@@ -28,10 +33,25 @@ do_aux_out:
 	RTS
 
 -aux_save:					; *** prepare things for SAVE, Carry if not possible ***
-	LDA #$F1				; PSV_ASCII
-	STA $DF94				; set VSP mode
+	LDA #PSV_FOPEN
+	STA $DF94				; set VSP mode for setting filename
+; must emit filename, placeholder below
+	LDX #0
+name_l:
+		LDA filename, X		; get char
+		BEQ name_ok			; until termination
+		STA $DF93			; send name character to VSP
+		INX					; eeeeeek
+		BNE name_l
+name_ok:
+	LDA #PSV_FWRITE
+	STA $DF94				; will use open file for writing
 	CLC						; all OK this far!
 	RTS
+filename:
+	.asc	"test.bas", 0
 
 -aux_close:					; *** tidy up after SAVE ***
+	LDA #PSV_FCLOSE
+	STA $DF94				; tell VSP to close file
 	RTS						; nothing to do this far
