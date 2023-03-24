dbd1e2c2b6eb807135a16a578274840fc4cefa96
Carlos Santisteban <zuiko21@gmail.com>
2023-03-24 19:48:57 +0100

    yet another SSH test

--- forge/eh_basic/ehbasic_sa.s
+++ forge/eh_basic/ehbasic_sa.s
@@ -1,6 +1,6 @@
 ; *** adapted version of EhBASIC for Durango-X (standalone) ***
 ; (c) 2015-2023 Carlos J. Santisteban
-; last modified 20230324-1941
+; last modified 20230324-1948
 ; *************************************************************
 
 ; Enhanced BASIC, $ver 2.22 with Durango-X support!
