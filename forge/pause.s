cc4a6905d538dd1af56b01a7cbeb558c87bee6b0
Carlos Santisteban <zuiko21@users.noreply.github.com>
2023-06-27 12:59:58 +0200

    simplified streak generation

--- forge/pause.s
+++ forge/pause.s
@@ -1,13 +1,11 @@
 ; Skeumorphic pause for Durango-X
 ; (c) 2023 Carlos J. Santisteban
-; last modified 20230627-1220
+; last modified 20230627-1255
 
-; *** zeropage definitions ***
-total		= 			; from this point on, no need for zeropage
-
+; *** other addresses ***
 ROM_BASE	= $E000		; randomish ROM contents start address
 RASTER_CNT	= 112		; noise band start raster
-MAX_ITER	= $500		; average ~32 rasters
+MAX_ITER	= 20		; average ~32 rasters
 
 ; *******************
 ; *** code itself ***
@@ -19,15 +17,7 @@ pause:
 		AND #START		; check button
 		BNE pause
 ; *** now generate the noise band until START is pressed again ***
-; init some stuff
-	LDY #0					; fixed LSB
-	LDA #>ROM_BASE			; pointer for randomish content
-	STY base
-	STA base+1
 frame:
-	LDY #0					; fixed LSB
-	STY total				; no need for STZ
-	STY total+1
 ; first wait for VBLANK
 vb_in:
 		BIT IO8blnk
@@ -45,26 +35,22 @@ hb_out:
 		BMI hb_in		; wait until the end of HBLANK
 		DEY
 		BNE vb_in		; exit upon reaching the desired raster
+	LDY #MAX_ITER		; 20 toggles should take around 32 rasters, make it even and will restore initial status
 ; go for the streaks
 streak:
 		LDA IO8attr
   		EOR #%10000000	; toggle between HIRES and colour
 		ORA #%00001000	; enable RGB, just in case
 		STA IO8attr		; switch mode
-		LDA (base), Y	; get randomish content
-		INY
+		LDA ROM_BASE, Y	; get randomish content
 		LSR
 		LSR				; max. 63, or 315t ~3.2 rasters
   		TAX				; for the counter
 dropout:
 			DEX
    			BPL dropout	; delay, 5*X t
-		ADC total
-		STA total
-		LDA total+1
-		ADC #0			; propagate carry
-		CMP #5
-  		BCC streak
+		DEY
+		BNE streak
 
 	JMP frame
 exit:
