1181d5c814bc84e80b4e1cc89333e685a2033135
Carlos Santisteban <zuiko21@users.noreply.github.com>
2023-06-27 12:47:52 +0200

    more on skeuomorph

--- forge/pause.s
+++ forge/pause.s
@@ -1,24 +1,22 @@
 ; Skeumorphic pause for Durango-X
 ; (c) 2023 Carlos J. Santisteban
-; last modified 20230627-1109
+; last modified 20230627-1220
 
 ; *** zeropage definitions ***
-sptr	= 
-base	= sptr + 2
-total	= base + 2		; from this point on, no need for zeropage
+total		= 			; from this point on, no need for zeropage
 
-ROM_BASE	= $E000		; ransomish ROM contents start address
+ROM_BASE	= $E000		; randomish ROM contents start address
 RASTER_CNT	= 112		; noise band start raster
-BAND_ADR	= $6E00		; noise band start address
+MAX_ITER	= $500		; average ~32 rasters
 
-; ************************************************************************************************************************
-; *** code itself, main program must check START button and, if pressed, JSR pause and then somehow restore the screen ***
-; ************************************************************************************************************************
+; *******************
+; *** code itself ***
+; *******************
 pause:
 ; should wait for START button release
 		LDA gamepad0	; assume these are ready and masked by the ISR
 		ORA gamepad1
- 		AND #START		; check button
+		AND #START		; check button
 		BNE pause
 ; *** now generate the noise band until START is pressed again ***
 ; init some stuff
@@ -26,34 +24,54 @@ pause:
 	LDA #>ROM_BASE			; pointer for randomish content
 	STY base
 	STA base+1
-;	LDY #0					; fixed LSB
-	LDA #>BAND_ADR			; pointer for randomish content
-	STY sptr
-	STA sptr+1
+frame:
+	LDY #0					; fixed LSB
+	STY total				; no need for STZ
+	STY total+1
 ; first wait for VBLANK
 vb_in:
 		BIT IO8blnk
-  		BVC vb_in		; wait until within VBLANK
+		BVC vb_in		; wait until within VBLANK
 vb_out:
 		BIT IO8blnk
-  		BVS vb_out		; wait until the end of VBLANK
+		BVS vb_out		; wait until the end of VBLANK
+; now wait until desired raster
 	LDY #RASTER_CNT		; raster counter
 hb_in:
 		BIT IO8blnk
-  		BPL hb_in		; wait until next HBLANK
+		BPL hb_in		; wait until next HBLANK
 hb_out:
 		BIT IO8blnk
-  		BMI hb_in		; wait until the end of HBLANK
+		BMI hb_in		; wait until the end of HBLANK
 		DEY
-  		BNE vb_in		; exit upon reaching the desired raster
+		BNE vb_in		; exit upon reaching the desired raster
 ; go for the streaks
+streak:
+		LDA IO8attr
+  		EOR #%10000000	; toggle between HIRES and colour
+		ORA #%00001000	; enable RGB, just in case
+		STA IO8attr		; switch mode
+		LDA (base), Y	; get randomish content
+		INY
+		LSR
+		LSR				; max. 63, or 315t ~3.2 rasters
+  		TAX				; for the counter
+dropout:
+			DEX
+   			BPL dropout	; delay, 5*X t
+		ADC total
+		STA total
+		LDA total+1
+		ADC #0			; propagate carry
+		CMP #5
+  		BCC streak
 
-
+	JMP frame
 exit:
-; pause is over, wait until START is released again... but screen must be restored?
+; pause is over, wait until START is released again
 		LDA gamepad0	; assume these are ready and masked by the ISR
 		ORA gamepad1
- 		AND #START		; check button
+		AND #START		; check button
 		BNE exit
-  RTS
+	RTS
 	
