556e7f7a064ac17532a4392b0bf8b2c12817e72e
Carlos Santisteban <zuiko21@users.noreply.github.com>
2023-12-04 14:01:28 +0100

    FIX wrong memory allocation in imager EEEEEEEK

--- forge/imager.c
+++ forge/imager.c
@@ -1,6 +1,6 @@
 /* Durango Imager - CLI version
  * (C)2023 Carlos J. Santisteban
- * last modified 20231204-1310
+ * last modified 20231204-1334
  * */
 
 /* Libraries */
@@ -110,7 +110,7 @@ int main (void) {
 	int		opt =	OPT_NONE;
 
 	init();					// Init things
-	printf("\nDurango-X volume creator, v1.0 by @zuiko21\n");
+	printf("\nDurango-X volume creator, v1.0b2 by @zuiko21\n");
 // Do actual stuff
 	while (opt != OPT_EXIT) {
 		opt =	menu();		// choose task
@@ -205,7 +205,6 @@ void	open(void) {					// Open volume
 		return;
 	}
 	printf(" OK\nReading headers...");
-fflush(stdout);
 	bye();					// free up dynamic memory
 	while (!feof(file)) {
 		if (fread(buffer, HD_BYTES, 1, file) != 1)	break;		// get header into buffer
@@ -219,21 +218,17 @@ fflush(stdout);
 			continue;											// EEEEEEEK
 		}
 		printf("\nFound %s (%-5.2f KiB): Allocating", h.name, h.size/1024.0);
-fflush(stdout);
-		printf("[%d]",used);									// entry to be allocated
-fflush(stdout);
+		printf("[%x]",used);									// entry to be allocated
+		if (h.size & 511) {	// uneven size in header, must be rounded up as was padded in volume BEFORE ALLOCATING EEEEEEEEEEEEEEEEKKKKKKKK
+			h.size = (h.size + 512) & 0xFFFFFE00;				// sector-aligned size
+		}
 		if ((ptr[used] = malloc(h.size)) == NULL) {				// Allocate dynamic memory
 			printf("\n\t*** Out of memory! ***\n");
 			return;
 		}
-else printf("{malloc OK}");fflush(stdout);
 		printf(", Header");
-fflush(stdout);
 		memcpy(ptr[used], buffer, HD_BYTES);					// copy preloaded header
 //		for (i=0; i<HD_BYTES; i++)		ptr[used][i] = buffer[i];	// * * * B A D * * * EEEEK
-		if (h.size & 511) {	// uneven size in header, must be rounded up as was padded in volume
-			h.size = (h.size + 512) & 0xFFFFFE00;				// sector-aligned size
-		}
 		if ((signature(&h) == SIG_ROM) || (signature(&h) == SIG_POCKET))	printf(", Code");
 		else																printf(", Data");
 		if (fread(ptr[used]+HD_BYTES, h.size-HD_BYTES, 1, file) != 1) {		// read remaining bytes 
