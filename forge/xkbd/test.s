56d16b6aaa905fb810e93b44b2f0007052d0bc66
Carlos Santisteban <zuiko21@gmail.com>
2024-09-21 13:34:56 +0200

    improve extended keyboard test

--- forge/xkbd/test.s
+++ forge/xkbd/test.s
@@ -29,14 +29,42 @@ start:
 	JSR delay
 	JSR delay
 	JSR delay
-	LDA #%00000000			; reset
+	LDA #%00100000			; check legacy ID
+	STA IOkbd
+;	LDA IOkbd
+;	CMP #$2C
+	LDA #%00000001			; first legacy column
 loop:
 		STA IOkbd
 		JSR delay
-		INC
+		ASL
+		CMP #%00100000		; all five columns done?
 		BNE loop
 	STZ IOkbd
-	LDA #127
+	JSR delay				; pause at legacy condition
+	JSR delay
+	JSR delay
+	LDA #%01000000			; check extended ID
+	STA IOkbd
+;	LDA IOkbd
+;	CMP #$88
+	LDA #%10000000			; first extended column
+xloop:
+			STA IOkbd
+			JSR delay
+			INC
+			BIT #%00001000	; all 8 columns done?
+			BEQ xloop
+		BIT #%00010000		; will use this bit for Caps Lock flag
+		BNE exit			; will end with caps lock on
+			LDX #%11000000	; will turn on caps (was native)
+			STX IOkbd
+			LDX #%10000000	; back to standard native
+			STX IOkbd
+			LDA #%10010000	; set flag
+			BRA xloop		; and try again
+exit:
+	LDA #127				; end of test, beep
 beep:
 		STA IOBeep
 bdly:
@@ -44,6 +72,12 @@ bdly:
 			BNE bdly
 		DEC
 		BPL beep
+	LDA #%00000000			; select caps lock...
+	STA IOkbd
+	LDA #%01000000			; ...and disable it
+	STA IOkbd
+	LDA #%10000000			; stay native
+	STA IOkbd
 lock:
 	BRA lock
 
@@ -51,6 +85,7 @@ delay:
 	LDX #0
 	LDY #0
 dloop:
+			NOP:NOP
 			INX
 			BNE dloop
 		INY
