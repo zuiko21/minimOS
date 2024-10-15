1595db23c5270c66aa1c4f0d093e099603b0d556
Carlos Santisteban <zuiko21@users.noreply.github.com>
2024-10-15 11:09:02 +0200

    ad clock stretching with timeout

--- forge/i2c.s
+++ forge/i2c.s
@@ -1,6 +1,6 @@
 ; RTC test via the I2C inteface on FastSPI card
 ; (c) 2024 Carlos J. Santisteban
-; last modified 20241015-0934
+; last modified 20241015-1030
 
 ; send as binary blob via nanoBoot (-x 0x1000 option)
 
@@ -15,8 +15,13 @@
 ; *** hardware addresses ***
 IO9rtc	= $DF97			; I2C port on FastSPI card
 
-; *** memory allocation ***
+; *** memory allocation *** needs two bytes, not necessarily on ZP
+#ifdef	I2C_LOCAL
+i2str	= I2C_LOCAL
+#else
 i2str	= $FE			; global started condition
+#endif
+i2time	= i2str+1		; timeout counter
 
 ; ******************
 ; *** code start ***
@@ -37,25 +42,12 @@ i2send:				; *** send byte in A to address in X ***
 	ASL				; put 0 at d0 as is a write operation
 	JSR i2write		; send this byte in A
 	PLA				; get stored data
-;	BRA i2write		; and write data afterwards
+;	JSR i2write		; and write data afterwards
+;	RTS
+
 i2write:			; *** raw byte in A sent thru I2C ***
 	TAX				; save for later
-; set start condition ***
-	BIT i2str		; already started?
-	BPL no_str		; if so, do restart
-		LDA #SDA
-		TSB IO9rtc	; set SDA
-		LDA #SCL
-		TSB IO9rtc	; set SCL
-		JSR clk_str	; clock stretching!
-no_str:
-	JSR arbitr		; check if arbitration was lost
-	LDA #SDA
-	TRB IO9rtc		; high-to-low SDA is START condition
-	DEC i2str		; dangerously set as started... and some delay as requested
-	LDA #SCL
-	TRB IO9rtc		; clear SCL as well
-; end of start condition ***
+	JSR i2start		; set START condition
 	LDY #8			; number of bits per byte
 is_loop:
 		TXA			; get remaining bits
@@ -76,6 +68,23 @@ sda_set:
 		BNE is_loop
 ; check ACK * * * T B D * * *
 	RTS				; this does NOT send stop bit
+
+i2start:			; *** generic set START condition ***
+	BIT i2str		; already started?
+	BPL no_str		; if so, do restart
+		LDA #SDA
+		TSB IO9rtc	; set SDA
+		LDA #SCL
+		TSB IO9rtc	; set SCL
+		JSR clk_str	; clock stretching!
+no_str:
+	JSR arbitr		; check if arbitration was lost
+	LDA #SDA
+	TRB IO9rtc		; high-to-low SDA is START condition
+	DEC i2str		; dangerously set as started... and some delay as requested
+	LDA #SCL
+	TRB IO9rtc		; clear SCL as well
+	RTS
 
 i2receive:			; *** receive byte in A from address in X ***
 	TXA				; retrieve address
@@ -99,13 +108,27 @@ i2stop:				; *** generic send STOP condition ***
 ;	JMP arbitr		; after delay, check for arbitration and return
 ;	RTS
 
-; *** check if arbitration is lost ***
-arbitr:
+arbitr:				; *** check if arbitration is lost ***
 	BIT IO9rtc		; check SDA
 	BMI st_ok
 		BRK			; if zero, arbitration lost
 st_ok:
 	RTS
 
-; *** check for clock stretching with timeout ***
+; *** check for clock stretching with timeout (~25 ms, standard overhead is 22t) ***
 clk_str:
+	STZ i2time		; reset timeout counter (base loop is 342*256=87552t, 25.04 mS at least, up to 57)
+str_l:
+		BIT IO9rtc	; check I2C_C (4)
+	BVS clk_ok		; already high, all OK (2)
+		PHY			; best here (3)
+		LDY #64		; inner iterations (2)
+str_il:
+			DEY		; inner loop (5*64-1=319)
+			BNE str_il
+		PLY			; (4)
+		INC i2time	; otherwise increment counter (5 or 6)
+		BNE str_l	; (3)
+	BRK				; until timeout!
+clk_ok:
+	RTS
